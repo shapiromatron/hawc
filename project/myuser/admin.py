731b92186cf4c8e9be99fddfaaaa22cdd5b005a4
Andy Shapiro <shapiromatron@gmail.com>
2014-11-18 12:38:02 -0500

    create welcome-email when adding a new user to HAWC, including instructions
    to reset their password

--- project/myuser/admin.py
+++ project/myuser/admin.py
@@ -3,9 +3,20 @@ from django.contrib import admin
 from . import models
 
 
+def send_welcome_emails(modeladmin, request, queryset):
+    for user in queryset:
+        user.send_welcome_email()
+
+    modeladmin.message_user(request, "Welcome email(s) sent!")
+
+send_welcome_emails.short_description = "Send welcome email"
+
 class HAWCUserAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'email', 'is_active',  'is_staff', 'date_joined')
     list_filter = ('date_joined', )
     ordering = ('-date_joined', )
+
+    actions = [send_welcome_emails]
+
 
 admin.site.register(models.HAWCUser, HAWCUserAdmin)
