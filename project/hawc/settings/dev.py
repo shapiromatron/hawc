e1f1f815320b70481a40ec010d17cb29138c0880
Andy Shapiro <shapiromatron@gmail.com>
2015-01-06 12:59:26 -0500

    added-back dev file

---
+++ project/hawc/settings/dev.py
@@ -0,0 +1,60 @@
+from .base import *
+
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+INSTALLED_APPS += ('debug_toolbar', 'django_extensions', 'django_coverage')
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'project', 'static'),
+)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'django.utils.log.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        },
+        'django': {
+            'handlers': ['null'],
+            'propagate': True,
+            'level': 'INFO',
+        },
+        'django.request': {
+            'handlers': ['null'],
+            'level': 'ERROR',
+            'propagate': False,
+        }
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'TIMEOUT': 60*60*24  # seconds
+    }
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.db"
