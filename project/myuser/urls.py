5c077ed82f7b144686e66bca2d05694f42bfc062
Andy Shapiro <shapiromatron@gmail.com>
2015-06-10 10:10:44 -0400

    pep8 myuser urls

--- project/myuser/urls.py
+++ project/myuser/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.views import (login, logout, password_reset,
-                                       password_reset_done, password_reset_confirm)
+                                       password_reset_done,
+                                       password_reset_confirm)
 from django.core.urlresolvers import reverse_lazy
 
 from . import forms
@@ -8,29 +9,51 @@ from . import views
 
 
 urlpatterns = patterns('',
-    url(r'^login/$', login, {'authentication_form': forms.HAWCAuthenticationForm}, 'login'),
-    url(r'^logout/$', logout, {'next_page': '/'}, name='logout'),
-    url(r'^new/$', views.create_account, name='new'),
-    url(r'^profile/$', views.ProfileDetail.as_view(), name='settings'),
-    url(r'^profile/update/$', views.ProfileUpdate.as_view(), name='profile_update'),
-    url(r'^password-change/$', views.PasswordChange.as_view(), name='change_password'),
+    url(r'^login/$',
+        login,
+        {'authentication_form': forms.HAWCAuthenticationForm},
+        'login'),
+    url(r'^logout/$',
+        logout,
+        {'next_page': '/'},
+        name='logout'),
 
+    url(r'^new/$',
+        views.create_account,
+        name='new'),
+
+    url(r'^profile/$',
+        views.ProfileDetail.as_view(),
+        name='settings'),
+    url(r'^profile/update/$',
+        views.ProfileUpdate.as_view(),
+        name='profile_update'),
+
+    url(r'^password-change/$',
+        views.PasswordChange.as_view(),
+        name='change_password'),
     url(r'^password-reset/$', password_reset,
-        {
-            "post_reset_redirect": reverse_lazy("user:reset_password_sent"),
-            "password_reset_form": forms.HAWCPasswordResetForm
-        },
+        {"post_reset_redirect": reverse_lazy("user:reset_password_sent"),
+         "password_reset_form": forms.HAWCPasswordResetForm},
         name='reset_password'),
-    url(r'^password-reset/sent/$', views.PasswordResetSent.as_view(),
+    url(r'^password-reset/sent/$',
+        views.PasswordResetSent.as_view(),
         name='reset_password_sent'),
     url(r'^password-reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$',
-            password_reset_confirm,
-            {"post_reset_redirect": reverse_lazy("user:login")},
-            name='reset_password_confirm'),
-    url(r'^password-reset-done/$', password_reset_done, name='reset_password_done'),
-    url(r'^password-reset/complete/$', views.PasswordResetSent.as_view(), name='reset_password_sent'),
-    url(r'^(?P<pk>\d+)/set-password/$', views.SetUserPassword.as_view(), name='set_password'),
+        password_reset_confirm,
+        {"post_reset_redirect": reverse_lazy("user:password_reset_complete")},
+        name='reset_password_confirm'),
+    url(r'^password-reset-done/$',
+        password_reset_done,
+        name='reset_password_done'),
+    url(r'^password-reset/complete/$',
+        views.PasswordResetSent.as_view(),
+        name='reset_password_sent'),
     url(r'^password/changed/$',
         views.PasswordChanged.as_view(),
         name='password_reset_complete'),
+
+    url(r'^(?P<pk>\d+)/set-password/$',
+        views.SetUserPassword.as_view(),
+        name='set_password'),
 )
