3726001e111dbaa542cb0523d7f5006196ddeae8
Andy Shapiro <shapiromatron@gmail.com>
2015-07-01 16:36:04 -0400

    only use secure cookies on site with https

--- project/hawc/settings/production.py
+++ project/hawc/settings/production.py
@@ -4,3 +4,6 @@ SERVER_ROLE = "production"
 
 # There should be only minor differences from staging
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+
+# only pass via HTTPS connection
+SESSION_COOKIE_SECURE = True
--- project/hawc/settings/staging.py
+++ project/hawc/settings/staging.py
@@ -4,5 +4,3 @@ SERVER_ROLE = "staging"
 
 LOGGING['handlers']['file']['filename'] = os.path.join(os.getenv('LOGS_PATH'), 'hawc.log')
 LOGGING['loggers']['django']['handlers'] = ['file']
-
-SESSION_COOKIE_SECURE = True
