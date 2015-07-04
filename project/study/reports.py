d0bd3c923f975aac6a01f57aca49c9dee1a5a436
Andy Shapiro <shapiromatron@gmail.com>
2015-07-04 11:14:21 -0400

    use new report template to generate RoB reports

--- project/study/reports.py
+++ project/study/reports.py
@@ -1,93 +1,81 @@
 from django.utils.html import strip_tags
 
-from docx.shared import Inches
+from docxUtils.tables import TableMaker
 
-from utils.helper import DOCXReport
+from utils.helper import HAWCDOCXReport
 
 from . import models
 
 
-def build_header_cell(row, col, width, text, colspan=1):
-    return {
-        "row": row,
-        "col": col,
-        "width": width,
-        "colspan": colspan,
-        "runs": [{ "text": text, "bold": True, "italic": False }]}
-
-def build_caption(study):
-    cell = build_header_cell(0, 0, 7.5, "", colspan=3)
-    cell["runs"] = [
-        { "text": u'{0}: '.format(study["short_citation"]), "bold": True, "italic": False },
-        { "text": study["full_citation"], "bold": False, "italic": False }
-    ]
-    return cell
 
 
-class SQDOCXReport(DOCXReport):
+class SQDOCXReport(HAWCDOCXReport):
 
-    def build_ROB_table(self, study):
-
-        # build header rows
-        cells = [
-            build_caption(study),
-            build_header_cell(1, 0, 2.8, r'Risk of Bias'),
-            build_header_cell(1, 1, 0.7, r'Rating'),
-            build_header_cell(1, 2, 4.0, r'Rating Rationale'),
-        ]
-
-        # build RoB rows
-        rows = 2
-        for d in study["qualities"]:
-            cells.append({"row": rows, "col": 0, "text": d["metric"]["metric"]})
-            cells.append({"row": rows, "col": 1, "text": d["score_symbol"], "shade": d["score_shade"]})
-            cells.append({"row": rows, "col": 2, "text": strip_tags(d["notes"])})
-            rows += 1
-
-        self.build_table(rows, 3, cells, numHeaders=2)
-
-    def build_ROB_legend(self):
-        cells = [
-           {"row": 0, "col": 0, "width": 1.5, "runs": [
-            {"text": "Risk of Bias Key:", "bold": True, "italic": False}
-           ]},
-           {"row": 0, "col": 1, "width": 1.0, "text": "Definitely Low"},
-           {"row": 0, "col": 2, "width": 0.4, "text": "++",
-            "shade": models.StudyQuality.SCORE_SHADES[4]},
-           {"row": 0, "col": 3, "width": 1.0, "text": "Probably Low"},
-           {"row": 0, "col": 4, "width": 0.4, "text": "+",
-            "shade": models.StudyQuality.SCORE_SHADES[3]},
-           {"row": 0, "col": 5, "width": 1.0, "text": "Probably High"},
-           {"row": 0, "col": 6, "width": 0.4, "text": "-",
-            "shade": models.StudyQuality.SCORE_SHADES[2]},
-           {"row": 0, "col": 7, "width": 0.4, "text": "NR",
-            "shade": models.StudyQuality.SCORE_SHADES[0]},
-           {"row": 0, "col": 8, "width": 1.0, "text": "Definitely High"},
-           {"row": 0, "col": 9, "width": 0.4, "text": "--",
-            "shade": models.StudyQuality.SCORE_SHADES[1]},
-        ]
-        self.build_table(1, len(cells), cells)
-
-    def create_context(self):
-        doc = self.doc
+    def create_content(self):
         d = self.context
 
-        # make landscape
-        section = doc.sections[-1]
-        section.left_margin = Inches(0.5)
-        section.right_margin = Inches(0.5)
+        self.setMargins(left=0.5, right=0.5)
 
-        # title
         txt = "Risk-of-bias report: {0}".format(d['assessment']['name'])
-        doc.add_heading(txt, 0)
+        self.doc.add_heading(txt, 1)
 
-        # loop through each study
         for study in d['studies']:
-            doc.add_heading(study['short_citation'], 1)
-            if len(study["qualities"])>0:
+            self.doc.add_heading(study['short_citation'], 2)
+            if len(study["qualities"]) > 0:
                 self.build_ROB_table(study)
-                doc.add_paragraph()
+                self.doc.add_paragraph()
                 self.build_ROB_legend()
-                doc.add_page_break()
+                self.doc.add_page_break()
             else:
-                doc.add_paragraph("No risk-of-bias details available for this study.")
+                self.doc.add_paragraph("No risk-of-bias details available for this study.")
+
+    def getTableCaptionRuns(self, study):
+        return [
+            TableMaker.new_run(u'{0}: '.format(study["short_citation"]), b=True, newline=False),
+            TableMaker.new_run(study["full_citation"], newline=False)
+        ]
+
+    def build_ROB_table(self, study):
+        widths = [2.8, 0.7, 4.0]
+        tbl = TableMaker(widths, numHeaders=2, firstRowCaption=True, tblStyle="ntpTbl")
+
+        # add caption
+        tbl.new_td_run(0, 0, self.getTableCaptionRuns(study), colspan=3)
+
+        # add headers
+        cells = ['Risk of Bias', 'Rating', 'Rating rationale']
+        for i, cell in enumerate(cells):
+            tbl.new_th(1, i, cell)
+
+        # add body
+        row = 2
+        for d in study["qualities"]:
+            tbl.new_td_txt(row, 0, d["metric"]["metric"])
+            tbl.new_td_txt(row, 1, d["score_symbol"], shade=d["score_shade"])
+            tbl.new_td_txt(row, 2, strip_tags(d["notes"]))
+            row += 1
+
+        return tbl.render(self.doc)
+
+    def build_ROB_legend(self):
+        widths = [1.5, 1.0, 0.4, 1.0, 0.4, 1.0, 0.4, 0.4, 1.0, 0.4]
+        tbl = TableMaker(widths, numHeaders=0, firstRowCaption=False, tblStyle="ntpTbl")
+        cells = [
+           {"runs": [TableMaker.new_run("Risk of bias key:", b=True, newline=False)]},
+           {"text": "Definitely Low"},
+           {"text": "++", "shade": models.StudyQuality.SCORE_SHADES[4]},
+           {"text": "Probably Low"},
+           {"text": "+", "shade": models.StudyQuality.SCORE_SHADES[3]},
+           {"text": "Probably High"},
+           {"text": "-", "shade": models.StudyQuality.SCORE_SHADES[2]},
+           {"text": "NR", "shade": models.StudyQuality.SCORE_SHADES[0]},
+           {"text": "Definitely High"},
+           {"text": "--", "shade": models.StudyQuality.SCORE_SHADES[1]},
+        ]
+        for i, cell in enumerate(cells):
+            if "runs" in cell:
+                tbl.new_td_run(0, i, cell["runs"], shade=cell.get("shade"))
+            else:
+                tbl.new_td_txt(0, i, cell["text"], shade=cell.get("shade"))
+
+        return tbl.render(self.doc)
