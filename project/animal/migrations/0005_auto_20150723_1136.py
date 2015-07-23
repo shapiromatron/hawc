6adfe87939ba426275012532d47814fc71fb6f10
Andy Shapiro <shapiromatron@gmail.com>
2015-07-23 12:36:36 -0400

    update model states from prior changes (help-text and ordering)

---
+++ project/animal/migrations/0005_auto_20150723_1136.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.core.validators
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('animal', '0004_auto_20150713_1015'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='experiment',
+            name='purity',
+            field=models.FloatField(blank=True, help_text=b'Percentage (ex: 95%)', null=True, verbose_name=b'Chemical purity (%)', validators=[django.core.validators.MinValueValidator(0), django.core.validators.MaxValueValidator(100)]),
+        ),
+        migrations.AlterField(
+            model_name='experiment',
+            name='purity_qualifier',
+            field=models.CharField(default='', max_length=1, blank=True, choices=[('>', '>'), ('\u2265', '\u2265'), ('=', '='), ('', '')]),
+        ),
+    ]
---
+++ project/lit/migrations/0002_auto_20150723_1134.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lit', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='search',
+            options={'ordering': ['-last_updated'], 'get_latest_by': 'last_updated', 'verbose_name_plural': 'searches'},
+        ),
+    ]
