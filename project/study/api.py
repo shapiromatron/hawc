b49dbe058be07cdf7a25813074302b1c6c58670b
Andy Shapiro <shapiromatron@gmail.com>
2015-09-21 17:21:38 -0400

    add study filters by study_type

--- project/study/api.py
+++ project/study/api.py
@@ -1,15 +1,31 @@
 from __future__ import absolute_import
 
-from assessment.api.views import AssessmentViewset
+import django_filters
+from rest_framework import filters
+from rest_framework import viewsets
+
 from utils.api import DisabledPagination
+from assessment.api.views import AssessmentLevelPermissions, InAssessmentFilter
 
 from . import models, serializers
 
 
-class Study(AssessmentViewset):
+class StudyFilters(django_filters.FilterSet):
+    study_type = django_filters.ChoiceFilter(
+        choices=models.Study.STUDY_TYPE_CHOICES)
+
+    class Meta:
+        model = models.Study
+        fields = ('study_type', )
+
+
+class Study(viewsets.ReadOnlyModelViewSet):
     assessment_filter_args = "assessment"
     model = models.Study
     pagination_class = DisabledPagination
+    permission_classes = (AssessmentLevelPermissions, )
+    filter_backends = (InAssessmentFilter, filters.DjangoFilterBackend)
+    filter_class = StudyFilters
 
     def get_serializer_class(self):
         cls = serializers.VerboseStudySerializer
