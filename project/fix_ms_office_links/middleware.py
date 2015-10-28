14fa7aaadc3a2e79ec0810431dec449a8937ec21
Andy Shapiro <shapiromatron@gmail.com>
2015-10-28 13:09:52 -0400

    fix MS office middleware

---
+++ project/fix_ms_office_links/middleware.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+import re
+
+
+class MicrosoftOfficeLinkMiddleware(object):
+    # https://support.microsoft.com/en-us/kb/899927
+    # https://github.com/spilliton/fix_microsoft_links
+
+    OFFICE_AGENTS = re.compile(r"(Word|Excel|PowerPoint|ms-office)")
+    OUTLOOK_AGENTS = re.compile(r"(Microsoft Outlook)")
+    RESPONSE_TEXT = """<html><head><meta http-equiv="refresh" content="0"/></head><body></body></html>"""
+
+    def process_request(self, request):
+        agent = request.META.get("HTTP_USER_AGENT", "")
+        if (re.findall(self.OFFICE_AGENTS, agent) and
+           not re.findall(self.OUTLOOK_AGENTS, agent)):
+                return HttpResponse(self.RESPONSE_TEXT)
--- project/hawc/settings/base.py
+++ project/hawc/settings/base.py
@@ -66,6 +66,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'fix_ms_office_links.middleware.MicrosoftOfficeLinkMiddleware',
 )
 
 
