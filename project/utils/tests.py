2ff4430822996bb6e5189fd18df51fff7b896114
Andy Shapiro <shapiromatron@gmail.com>
2015-11-23 12:10:56 -0500

    fix URL validator to allow for absolute filepaths for smb protocol

--- project/utils/tests.py
+++ project/utils/tests.py
@@ -13,3 +13,47 @@ class FormTester(TestCase):
         form = self.createTestForm(inps, *args, **kwargs)
         form.full_clean()
         self.assertTrue(msg in form.errors[field])
+
+
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from . import validators
+
+
+class CustomURLValidtor(TestCase):
+
+    def test_urls(self):
+        # ensure that standard URL cases work as expected
+        validator = validators.CustomURLValidator()
+        for url in [
+            'http://www.example.com',
+            'http://example.com',
+            'http://www.example.com/abc/def/efg?x=1&y=2',
+        ]:
+            assert validator(url) is None
+
+        for url in [
+            'random://example.com',
+            'www.example.com',
+            'http://example',
+            'http://example/foo/bar?x=1&y=2',
+        ]:
+            with self.assertRaises(ValidationError):
+                validator(url)
+
+    def test_smbs(self):
+        # ensure that SMB work as expected
+        validator = validators.CustomURLValidator()
+        for path in [
+            'smb://path/to/stuff/',
+            'smb://path/to/stuff.pdf',
+            'smb://path/to/stuff%20with%20spaces.txt',
+        ]:
+            assert validator(path) is None
+
+        for path in [
+            'smb://path/to/stuff with spaces.txt',
+        ]:
+            with self.assertRaises(ValidationError):
+                validator(path)
--- project/utils/validators.py
+++ project/utils/validators.py
@@ -1,5 +1,23 @@
+from django.utils.encoding import force_text
+from django.utils import http
+from django.core.exceptions import ValidationError
 from django.core.validators import URLValidator
 
 
 class CustomURLValidator(URLValidator):
     schemes = ['http', 'https', 'ftp', 'ftps', 'smb']
+
+    def __call__(self, value):
+        value = force_text(value)
+        # Check first if the scheme is valid
+        scheme = value.split('://')[0].lower()
+        if scheme not in self.schemes:
+            raise ValidationError(self.message, code=self.code)
+
+        if scheme == "smb":
+            # assert valid URL, which is already URL quoted as needed
+            abspath = value.split(':/')[1]
+            if http.urlquote(http.urlunquote(abspath)) != abspath:
+                raise ValidationError(self.message, code=self.code)
+        else:
+            super(CustomURLValidator, self).__call__(value)
