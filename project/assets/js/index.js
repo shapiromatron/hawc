c4644b85eb4d1b25580baad89579da3da5fe6063
Josh Addington <josh.addington@gmail.com>
2016-01-11 14:59:14 -0500

    Add prod and dev Root component config

--- project/assets/js/index.js
+++ project/assets/js/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { App } from './containers/App';
+import Root from './containers/Root';
 import configureStore from './store/configureStore';
 
-const store = configureStore()
+const store = configureStore();
 
-render(<App store={store}/>, document.getElementById('root'));
+render(<Root store={store}/>, document.getElementById('root'));
