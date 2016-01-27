1f09bef43682d528989c7f6251b5295d39bd6b13
Josh Addington <josh.addington@gmail.com>
2016-01-27 13:01:34 -0500

    remove unused action types

--- project/assets/js/constants/ActionTypes.js
+++ project/assets/js/constants/ActionTypes.js
@@ -5,33 +5,18 @@ export const RESET_ERROR_MESSAGE =      'RESET_ERROR_MESSAGE';
 // assessments
 export const AS_REQUEST =               'AS_REQUEST';
 export const AS_RECEIVE_OBJECT =        'AS_RECEIVE_OBJECT';
-export const AS_DELETE_OBJECT =         'AS_DELETE_OBJECT';
 export const AS_RELEASE =               'AS_RELEASE';
 
 // endpoints
 export const EP_REQUEST =               'EP_REQUEST';
 export const EP_RECEIVE_OBJECTS =       'EP_RECEIVE_OBJECTS';
 export const EP_RECEIVE_OBJECT =        'EP_RECEIVE_OBJECT';
+export const EP_SET_TYPE =              'EP_SET_TYPE';
 export const EP_RECEIVE_MODEL =         'EP_RECEIVE_MODEL';
+export const EP_SET_FIELD =             'EP_SET_FIELD';
 export const EP_DELETE_OBJECT =         'EP_DELETE_OBJECT';
 export const EP_RESET_EDIT_OBJECT =     'EP_RESET_EDIT_OBJECT';
 export const EP_CREATE_EDIT_OBJECT =    'EP_CREATE_EDIT_OBJECT';
 export const EP_PATCH_OBJECTS =         'EP_PATCH_OBJECTS';
 export const EP_RECEIVE_EDIT_ERRORS =   'EP_RECEIVE_EDIT_ERRORS';
 export const EP_RELEASE =               'EP_RELEASE';
-
-// endpoint type
-export const ET_REQUEST =               'ET_REQUEST';
-export const ET_SUCCESS =               'ET_SUCCESS';
-export const ET_FAILURE =               'ET_FAILURE';
-export const ET_RECEIVE_OBJECTS =       'ET_RECEIVE_OBJECTS';
-export const ET_RECEIVE_OBJECT =        'ET_RECEIVE_OBJECT';
-export const ET_RELEASE =               'ET_RELEASE';
-
-//endpoint fields
-export const EF_REQUEST =               'EF_REQUEST';
-export const EF_SUCCESS =               'EF_SUCCESS';
-export const EF_FAILURE =               'EF_FAILURE';
-export const EF_RECEIVE_OBJECTS =       'EF_RECEIVE_OBJECTS';
-export const EF_RECEIVE_OBJECT =        'EF_RECEIVE_OBJECT';
-export const EF_RELEASE =               'EF_RELEASE';
