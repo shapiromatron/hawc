3f5c5dd9a333677fe9678132c024591f085ff8a5
Josh Addington <josh.addington@gmail.com>
2016-01-28 11:05:52 -0500

    add loadConfig to Root container component

--- project/assets/js/containers/Root.dev.js
+++ project/assets/js/containers/Root.dev.js
@@ -3,18 +3,24 @@ import { Provider } from 'react-redux';
 import { ReduxRouter } from 'redux-router';
 import DevTools from './DevTools';
 
+import { loadConfig } from '../actions/Config';
+
 export default class Root extends Component {
-  render() {
-      const { store } = this.props;
-      return (
-          <Provider store={store}>
-              <div>
-                  <ReduxRouter />
-                  <DevTools />
-              </div>
-          </Provider>
-      );
-  }
+    componentWillMount() {
+        this.props.store.dispatch(loadConfig());
+    }
+
+    render() {
+        const { store } = this.props;
+        return (
+            <Provider store={store}>
+                <div>
+                    <ReduxRouter />
+                    <DevTools />
+                </div>
+            </Provider>
+        );
+    }
 }
 
 Root.propTypes = {
--- project/assets/js/containers/Root.prod.js
+++ project/assets/js/containers/Root.prod.js
@@ -3,16 +3,20 @@ import { Provider } from 'react-redux';
 import { ReduxRouter } from 'redux-router';
 
 export default class Root extends Component {
-  render() {
-      const { store } = this.props;
-      return (
-          <Provider store={store}>
-            <ReduxRouter />
-          </Provider>
-      );
-  }
+    componentWillMount() {
+        store.dispatch(loadConfig());
+    }
+
+    render() {
+        const { store } = this.props;
+        return (
+            <Provider store={store}>
+                <ReduxRouter />
+            </Provider>
+        );
+    }
 }
 
 Root.propTypes = {
-    store: PropTypes.object.isRequired
+    store: PropTypes.object.isRequired,
 };
