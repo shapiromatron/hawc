4fdc05a36bc511d98d2b684c7f67ea35a5d46242
Andy Shapiro <shapiromatron@gmail.com>
2016-02-09 17:42:02 -0500

    remove css test-case

--- project/assets/js/containers/AssessmentApp.js
+++ project/assets/js/containers/AssessmentApp.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'underscore';
-import 'containers/Assessment.css';
 
 import Assessment from 'components/Assessment';
 import Loading from 'components/Loading';
