ae3880cf5ab469dc54f1779715ef44a6d037364c
Andy Shapiro <shapiromatron@gmail.com>
2016-02-09 17:42:17 -0500

    fix indentation

--- project/assets/js/containers/Root.js
+++ project/assets/js/containers/Root.js
@@ -12,7 +12,7 @@ export default class Root extends Component {
         const { store } = this.props;
         return (
             <Provider store={store}>
-                    <ReduxRouter />
+                <ReduxRouter />
             </Provider>
         );
     }
