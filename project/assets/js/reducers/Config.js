e4bbb4b41a189a4b5761942b0ed0d2f9b2cb91c7
Andy Shapiro <shapiromatron@gmail.com>
2016-02-19 15:58:31 -0500

    use token instead of full html field

--- project/assets/js/reducers/Config.js
+++ project/assets/js/reducers/Config.js
@@ -7,7 +7,6 @@ export default function (state=defaultState, action) {
     switch (action.type) {
     case types.CF_LOAD:
         let data = JSON.parse(document.getElementById('config').textContent);
-        data.csrf = data.csrf.match(/value='([\w]+)'/)[1];
         data.apiUrl = window ? window.location.origin : 'http://127.0.0.1:8000';
         return data;
     default:
