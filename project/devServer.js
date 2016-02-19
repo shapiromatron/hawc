d075fef09fc1c6d9830677b031b9ac3ca21a0623
Andy Shapiro <shapiromatron@gmail.com>
2016-02-19 16:57:13 -0500

    cleanup devServer commands

--- project/devServer.js
+++ project/devServer.js
@@ -1,10 +1,10 @@
-var path = require('path');
-var express = require('express');
-var webpack = require('webpack');
-var config = require('./webpack.config.dev');
+var path = require('path'),
+    express = require('express'),
+    webpack = require('webpack'),
+    config = require('./webpack.config.dev');
 
-var app = express();
-var compiler = webpack(config);
+var app = express(),
+    compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, {
     noInfo: true,
