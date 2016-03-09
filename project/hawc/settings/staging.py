babf697ea5c2f5bf49832696a546b2f263dc3291
Andy Shapiro <shapiromatron@gmail.com>
2016-03-09 16:54:24 -0500

    use mailgun http for production environment

--- project/hawc/settings/staging.py
+++ project/hawc/settings/staging.py
@@ -1,5 +1,8 @@
 from .base import *
 
+EMAIL_BACKEND = 'django_mailgun.MailgunBackend'
+MAILGUN_ACCESS_KEY = os.environ.get('MAILGUN_ACCESS_KEY')
+MAILGUN_SERVER_NAME = os.environ.get('MAILGUN_SERVER_NAME')
 
 SERVER_ROLE = "staging"
 
