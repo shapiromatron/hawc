14da9496043feafdd78aa1b3e509e1d3463c4995
Andy Shapiro <shapiromatron@gmail.com>
2016-03-09 17:08:42 -0500

    add secret key to staging instead of production

--- project/hawc/settings/production.py
+++ project/hawc/settings/production.py
@@ -1,10 +1,6 @@
 from .staging import *
 
-
 SERVER_ROLE = "production"
-
-# There should be only minor differences from staging
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # only pass via HTTPS connection
 SESSION_COOKIE_SECURE = True
