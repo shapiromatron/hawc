73df48b140e44fc06ea5e2944be95668c93569cb
Andy Shapiro <shapiromatron@gmail.com>
2016-05-04 11:12:52 -0400

    hotfix: fix store

--- project/assets/js/robVisual/store/configureStore.prod.js
+++ project/assets/js/robVisual/store/configureStore.prod.js
@@ -1,13 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import { reduxReactRouter } from 'redux-router';
-import createHistory from 'history/lib/createBrowserHistory';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
 
 const finalCreateStore = compose(
-  applyMiddleware(thunk),
-  reduxReactRouter({ routes, createHistory })
+  applyMiddleware(thunk)
 )(createStore);
 
 export default function configureStore(initialState) {
