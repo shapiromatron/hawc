fd5d94b76d87cf85ee12e72d86dd5159f5ec6194
Andy Shapiro <shapiromatron@gmail.com>
2016-05-05 11:42:05 -0400

    create js app startup methods

--- project/assets/index.js
+++ project/assets/index.js
@@ -1,19 +1,7 @@
-import React from 'react';
-import { render } from 'react-dom';
+import textCleanupStartup from 'textCleanup';
+import robVisualStartup from 'robVisual';
 
-import Root from 'textCleanup/containers/Root';
-import configureStore from 'textCleanup/store/configureStore';
-
-import RobRoot from 'robVisual/containers/Root';
-import configureRobStore from 'robVisual/store/configureStore';
-
-
-if (document.getElementById('root')){
-    const store = configureStore();
-    render(<Root store={store}/>, document.getElementById('root'));
-}
-
-if (document.getElementById('rootRobFilter')){
-    const store = configureRobStore();
-    render(<RobRoot store={store}/>, document.getElementById('rootRobFilter'));
-}
+window.app = {
+    textCleanupStartup,
+    robVisualStartup,
+};
