385fae1970a48b937e8f5990b037c1326104f34b
Andy Shapiro <shapiromatron@gmail.com>
2016-05-13 12:48:05 -0400

    remove unused Math extensions; fix log10 name

--- project/static/js/hawc.js
+++ project/static/js/hawc.js
@@ -186,83 +186,9 @@ _.extend(Math, {
                b[3]*Math.pow(df,3) + b[4]*Math.pow(df,2) + b[5]*Math.pow(df,1) +
                b[6];
     },
-    og10: function(val){
-      return Math.log(val) / Math.LN10;
-    },
-    ltqnorm: function(p){
-       /*
-        * Modified from the author's original python code (original comments t
-        * follow below) by shapiromatron@gmail.com.  February 18, 2013.
-        *
-        * Lower tail quantile for standard normal distribution function.
-        *
-        * This function returns an approximation of the inverse cumulative
-        * standard normal distribution function.  I.e., given P, it returns
-        * an approximation to the X satisfying P = Pr{Z <= X} where Z is a
-        * random variable from the standard normal distribution.
-        *
-        * The algorithm uses a minimax approximation by rational functions
-        * and the result has a relative error whose absolute value is less
-        * than 1.15e-9.
-        *
-        * Author:      Peter John Acklam
-        * Time-stamp:  2000-07-19 18:26:14
-        * E-mail:      pjacklam@online.no
-        * WWW URL:     http://home.online.no/~pjacklam
-        */
-
-        if ((p <= 0) || (p >= 1)){
-            // Alert console of error
-            console.log("Argument to ltqnorm  must be in open interval (0,1)");
-        }
-
-        // Coefficients in rational approximations.
-        a = [-3.969683028665376e+01,  2.209460984245205e+02, -2.759285104469687e+02,
-              1.383577518672690e+02, -3.066479806614716e+01,  2.506628277459239e+00];
-        b = [-5.447609879822406e+01,  1.615858368580409e+02, -1.556989798598866e+02,
-              6.680131188771972e+01, -1.328068155288572e+01];
-        c = [-7.784894002430293e-03, -3.223964580411365e-01, -2.400758277161838e+00,
-             -2.549732539343734e+00,  4.374664141464968e+00,  2.938163982698783e+00];
-        d = [7.784695709041462e-03,  3.224671290700398e-01, 2.445134137142996e+00,
-             3.754408661907416e+00];
-
-        // Define break-points.
-        plow  = 0.02425;
-        phigh = 1 - plow;
-
-        // Rational approximation for lower region:
-        if (p < plow){
-           q  = Math.sqrt(-2*Math.log(p));
-           return (((((c[0]*q+c[1])*q+c[2])*q+c[3])*q+c[4])*q+c[5]) /
-                  ((((d[0]*q+d[1])*q+d[2])*q+d[3])*q+1);
-        }
-
-        // Rational approximation for upper region:
-        if (phigh < p){
-           q  = Math.sqrt(-2*Math.log(1-p));
-           return -(((((c[0]*q+c[1])*q+c[2])*q+c[3])*q+c[4])*q+c[5]) /
-                   ((((d[0]*q+d[1])*q+d[2])*q+d[3])*q+1);
-        }
-
-        // Rational approximation for central region:
-        q = p - 0.5;
-        r = q*q;
-        return (((((a[0]*r+a[1])*r+a[2])*r+a[3])*r+a[4])*r+a[5])*q /
-               (((((b[0]*r+b[1])*r+b[2])*r+b[3])*r+b[4])*r+1);
-    },
-    normalcdf: function(mean, sigma, to){
-        // http://stackoverflow.com/questions/5259421/
-        var z = (to-mean)/Math.sqrt(2*sigma*sigma);
-        var t = 1/(1+0.3275911*Math.abs(z));
-        var a1 =  0.254829592;
-        var a2 = -0.284496736;
-        var a3 =  1.421413741;
-        var a4 = -1.453152027;
-        var a5 =  1.061405429;
-        var erf = 1-(((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*Math.exp(-z*z);
-        var sign = 1;
-        if(z<0){sign = -1;}
-        return (1/2)*(1+sign*erf);
+    log10: function(val){
+        // required for IE
+        return Math.log(val) / Math.LN10;
     },
     mean: function(array){
         switch(array.length){
@@ -280,15 +206,6 @@ _.extend(Math, {
             return null;
         }
     },
-    stdev: function(array){
-        // calculates sample standard-deviation (n-1)
-        var sum = 0,  mean = Math.mean(array);
-        if(mean === null) return null;
-        array.forEach(function(v){
-            sum += (v-mean)*(v-mean);
-        });
-        return Math.sqrt(sum/(array.length-1));
-    }
 });
 
 
