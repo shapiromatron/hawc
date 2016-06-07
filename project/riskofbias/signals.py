3916f97c630237a3d06ccf6ad26b954744cf4e18
Andy Shapiro <shapiromatron@gmail.com>
2016-06-07 16:34:55 -0400

    bust study-cache when RoB changes

--- project/riskofbias/signals.py
+++ project/riskofbias/signals.py
@@ -10,17 +10,17 @@ import models
 @receiver(post_save, sender=models.RiskOfBiasMetric)
 @receiver(pre_delete, sender=models.RiskOfBiasMetric)
 def invalidate_caches_rob_metrics(sender, instance, **kwargs):
-    study = apps.get_model('study', 'Study')
+    Study = apps.get_model('study', 'Study')
     if sender is models.RiskOfBiasDomain:
         assessment_id = instance.assessment_id
     elif sender is models.RiskOfBiasMetric:
         assessment_id = instance.domain.assessment_id
 
-    ids = study.objects\
+    ids = Study.objects\
         .filter(assessment_id=assessment_id)\
         .values_list('id', flat=True)
 
-    study.delete_caches(ids)
+    Study.delete_caches(ids)
 
 
 @receiver(post_save, sender=models.RiskOfBias)
@@ -28,9 +28,13 @@ def invalidate_caches_rob_metrics(sender, instance, **kwargs):
 @receiver(post_save, sender=models.RiskOfBiasScore)
 @receiver(pre_delete, sender=models.RiskOfBiasScore)
 def invalidate_caches_risk_of_bias(sender, instance, **kwargs):
+    Study = apps.get_model('study', 'Study')
     if sender is models.RiskOfBias:
-        ids = [instance.id]
+        rob_ids = [instance.id]
+        study_ids = [instance.study_id]
     elif sender is models.RiskOfBiasScore:
-        ids = [instance.riskofbias.id]
+        rob_ids = [instance.riskofbias.id]
+        study_ids = [instance.riskofbias.study_id]
 
-    models.RiskOfBias.delete_caches(ids)
+    models.RiskOfBias.delete_caches(rob_ids)
+    Study.delete_caches(study_ids)
