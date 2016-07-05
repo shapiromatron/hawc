0cb16d60492c6ca5ef1e2c5302676f697c9c405b
Andy Shapiro <shapiromatron@gmail.com>
2016-07-05 13:12:06 -0400

    make modal read only and editable

--- project/assets/bmd/components/BMROptionModal.js
+++ project/assets/bmd/components/BMROptionModal.js
@@ -15,10 +15,63 @@ class BMROptionModal extends React.Component {
         console.log('handled');
     }
 
+    renderReadOnlyTable(){
+        return (
+            <table className="table table-condensed table-striped">
+                <tbody>
+                    <tr>
+                        <th style={{width: '50%'}}>BMR type</th>
+                        <td style={{width: '50%'}}></td>
+                    </tr>
+                    <tr>
+                        <th>Value</th>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th>Confidence level</th>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        );
+    }
+
+    renderEditingForm(){
+        return (
+            <form className="form-horizontal">
+                <div className="control-group form-row">
+                    <label className="control-label"
+                           for="bmr_type">BMR type</label>
+                    <div className="controls">
+                        <select id="bmr_type">
+                        </select>
+                    </div>
+                </div>
+
+                <div className="control-group form-row">
+                    <label className="control-label"
+                           for="bmr_value">BMR value</label>
+                    <div className="controls">
+                        <input id="bmr_value"></input>
+                    </div>
+                </div>
+
+                <div className="control-group form-row">
+                    <label className="control-label"
+                           for="bmr_confidence_level">BMR confidence level</label>
+                    <div className="controls">
+                        <input id="bmr_confidence_level"></input>
+                    </div>
+                </div>
+            </form>
+        );
+    }
+
     render() {
 
         let editMode = this.props.editMode,
-            titlePrefix = (this.props.editMode)?'Edit ': '';
+            title = (this.props.editMode)?'Edit benchmark response': 'Benchmark response',
+            tableFunc = (editMode)? this.renderEditingForm: this.renderReadOnlyTable;
 
         return (
             <div className="modal hide fade" role="dialog" id={types.BMR_MODAL_ID}>
@@ -26,26 +79,11 @@ class BMROptionModal extends React.Component {
                 <div className="modal-header">
                     <button className="close" type="button"
                         data-dismiss="modal">×</button>
-                    <h3>{titlePrefix} Benchmark response</h3>
+                    <h3>{title}</h3>
                 </div>
 
                 <div className="modal-body">
-                    <table className="table table-condensed table-striped">
-                        <tbody>
-                            <tr>
-                                <th style={{width: '50%'}}>BMR type</th>
-                                <td style={{width: '50%'}}></td>
-                            </tr>
-                            <tr>
-                                <th>Value</th>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <th>Confidence level</th>
-                                <td></td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    {tableFunc()}
                 </div>
 
                 <EditableModalFooter
