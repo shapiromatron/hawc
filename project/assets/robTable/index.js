b6ab06bb99a73fd09fb3fdb072640e5e115844eb
Josh Addington <josh.addington@gmail.com>
2016-07-12 16:35:34 -0400

    pass mounting element to React startup functions

--- project/assets/robTable/index.js
+++ project/assets/robTable/index.js
@@ -4,9 +4,9 @@ import { render } from 'react-dom';
 import Root from 'robTable/containers/Root';
 import configureStore from 'robTable/store/configureStore';
 
-const startup = function(){
+const startup = function(element){
     const store = configureStore();
-    render(<Root store={store}/>, document.getElementById('robTable'));
+    render(<Root store={store}/>, element);
 };
 
 export default startup;
