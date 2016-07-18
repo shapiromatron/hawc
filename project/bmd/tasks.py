40e919ce23037bea5f61dd1e366324741e6c8852
Andy Shapiro <shapiromatron@gmail.com>
2016-07-18 16:56:01 -0400

    remove time sleep

--- project/bmd/tasks.py
+++ project/bmd/tasks.py
@@ -4,8 +4,6 @@ from celery import shared_task
 from celery.utils.log import get_task_logger
 from django.apps import apps
 
-import time
-
 logger = get_task_logger(__name__)
 
 
@@ -13,5 +11,4 @@ logger = get_task_logger(__name__)
 def execute(session_id):
     logger.info('BMD exeuction -> {}'.format(session_id))
     session = apps.get_model('bmd', 'BMDSession').objects.get(id=session_id)
-    time.sleep(5)
     session.execute()
