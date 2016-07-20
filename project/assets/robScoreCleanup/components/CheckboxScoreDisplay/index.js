d946a4e61f81e5c969b94a1dc68542bb829f47ff
Josh Addington <josh.addington@gmail.com>
2016-07-20 14:31:01 -0400

    update CheckboxScoreDisplay"

--- project/assets/robScoreCleanup/components/CheckboxScoreDisplay/index.js
+++ project/assets/robScoreCleanup/components/CheckboxScoreDisplay/index.js
@@ -1,39 +1,45 @@
 import React, { Component, PropTypes } from 'react';
 
 import ScoreDisplay from 'robTable/components/ScoreDisplay';
+import './CheckboxScoreDisplay.css';
 
 class CheckboxScoreDisplay extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {config: {
-            display: 'final',
-            isForm: false,
+        this.state = {
+            config: {
+                display: 'final',
+                isForm: false,
         }};
     }
 
     render() {
+        let { checked, item, config, handleCheck } = this.props;
         return (
-            <div>
-                <ScoreDisplay />
-                <input type="checkbox"
+            <div className='flexRow-container'>
+                <input className='score-display-checkbox'
+                       type="checkbox"
                        title="Include/exclude selected item in change"
                        checked={checked}
                        id={item.id}
-                       onChange={this.props.checkItem}/>
+                       onChange={handleCheck}/>
+                <ScoreDisplay key={item.id} score={item} config={config} />
             </div>
         );
     }
 }
 
 CheckboxScoreDisplay.propTypes = {
-    checkItem: PropTypes.func.isRequired,
+    handleCheck: PropTypes.func.isRequired,
     item: PropTypes.shape({
+        id: PropTypes.number.isRequired,
         notes: PropTypes.string,
         score_description: PropTypes.string.isRequired,
         score_symbol: PropTypes.string.isRequired,
         score_shade: PropTypes.string.isRequired,
     }),
+    config: PropTypes.object.isRequired,
 };
 
 export default CheckboxScoreDisplay;
