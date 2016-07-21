aeb51ede8e4e791a8c1db90a75d927a440e0814a
Andy Shapiro <shapiromatron@gmail.com>
2016-07-21 17:24:00 -0400

    don't show threshold field if value is None

--- project/bmd/forms.py
+++ project/bmd/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, HiddenInput
 
 from . import models
 
@@ -11,15 +11,19 @@ class AssessmentSettingsForm(ModelForm):
 
 
 class LogicFieldForm(ModelForm):
-    # TODO: if default threshold is blank, don't display field,
-    # otherwise make required
 
     class Meta:
         model = models.LogicField
         fields = '__all__'
 
+    def __init__(self, *args, **kwargs):
+        super(LogicFieldForm, self).__init__(*args, **kwargs)
+        if self.instance.threshold is None:
+            self.fields.pop('threshold')
+
 
 class BMDSessionForm(ModelForm):
+
     class Meta:
         model = models.BMDSession
         fields = '__all__'
