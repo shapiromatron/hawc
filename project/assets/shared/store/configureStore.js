ec23c33c5ff1811e3e71696f45b334b25fc185c9
Andy Shapiro <shapiromatron@gmail.com>
2016-08-01 11:36:22 -0400

    remove duplicate code; use one file

--- project/assets/shared/store/configureStore.js
+++ project/assets/shared/store/configureStore.js
@@ -1,5 +1,37 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import createLogger from 'redux-logger';
+
+import addPromiseSupportToDispatch from './promisedDispatch';
+
+
+let finalCreateStore;
+
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./configureStore.prod');
+
+    finalCreateStore = compose(
+      applyMiddleware(thunk)
+    )(createStore);
+
 } else {
-    module.exports = require('./configureStore.dev');
+
+    // development
+    const logger = createLogger({
+        level: 'info',
+        collapsed: false,
+        logger: console,
+        predicate: (getState, action) => true,
+    });
+
+    finalCreateStore = compose(
+      applyMiddleware(thunk, logger),
+      window.devToolsExtension ? window.devToolsExtension() : (f) => f
+    )(createStore);
+
+}
+
+export default function configureStore(reducer, initialState) {
+    const store = finalCreateStore(reducer, initialState);
+    store.dispatch = addPromiseSupportToDispatch(store);
+    return store;
 }
