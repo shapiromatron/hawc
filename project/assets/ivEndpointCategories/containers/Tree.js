d447103555c631a076ad460db364f96a03bdb449
Andy Shapiro <shapiromatron@gmail.com>
2016-08-02 13:56:36 -0400

    enable draggable interior

--- project/assets/ivEndpointCategories/containers/Tree.js
+++ project/assets/ivEndpointCategories/containers/Tree.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import Loading from 'shared/components/Loading';
 import Node from 'ivEndpointCategories/components/Node';
 import EditNode from 'ivEndpointCategories/components/EditNode';
+import Sortable from 'sortablejs';
 
 import {
     createTag,
@@ -39,7 +40,9 @@ class Tree extends React.Component {
             parent={NO_PARENT}
             parentOptions={this.props.parentOptions}
             handleUpdate={this.handleUpdate.bind(this)}
-            handleDelete={this.handleDelete.bind(this)} />;
+            handleDelete={this.handleDelete.bind(this)}
+            sortableGroupDecorator={this.sortableGroupDecorator.bind(this)}
+            />;
     }
 
     handleCreateClick(){
@@ -79,6 +82,26 @@ class Tree extends React.Component {
         return <p className='help-block'><i>No content is available - create some!</i></p>;
     }
 
+    handleMoveNode(item, oldIndex, newIndex){
+        console.log(item, oldIndex, newIndex);
+    }
+
+    sortableGroupDecorator = (componentBackingInstance) => {
+        // check if backing instance not null
+        if (!componentBackingInstance){
+            return;
+        }
+        Sortable.create(componentBackingInstance, {
+            draggable: '.draggable',
+            ghostClass: 'draggable-ghost',
+            chosenClass: 'draggable-chosen',
+            animation: 200,
+            onUpdate: (e) => {
+                this.handleMoveNode(parseInt(e.item.dataset.id), e.oldIndex, e.newIndex);
+            }
+        });
+    };
+
     render() {
         if (!this.props.tagsLoaded){
             return <Loading />;
@@ -93,11 +116,14 @@ class Tree extends React.Component {
             </h1>
             <div>
                 {(this.state.showCreate)?this.renderCreateNode():null}
-                {this.props.tags.map(this.renderNode.bind(this))}
+                <div ref={this.sortableGroupDecorator}>
+                    {this.props.tags.map(this.renderNode.bind(this))}
+                </div>
                 {this.renderNoTags()}
             </div>
         </div>;
     }
+
 }
 
 function mapStateToProps(state) {
