1445c3ff603f648aa4c6327af043e70cf15ce843
Andy Shapiro <shapiromatron@gmail.com>
2016-08-03 15:52:28 -0400

    add edit endpoint

--- project/assets/animal/index.js
+++ project/assets/animal/index.js
@@ -1,5 +1,9 @@
 import DosingRegimeForm from './DosingRegimeForm';
+import SampleSizeWidget from './SampleSizeWidget';
+import EditEndpoint from './EditEndpoint';
 
 export default {
     DosingRegimeForm,
+    SampleSizeWidget,
+    EditEndpoint,
 };
