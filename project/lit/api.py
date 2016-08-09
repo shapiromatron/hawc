3df124fd91bac6c29423ee7405937294799111e7
Josh Addington <josh.addington@gmail.com>
2016-08-09 13:37:39 -0400

    fix lit.api import

--- project/lit/api.py
+++ project/lit/api.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-from assessment.api.views import AssessmentRootedTagTreeViewset
+from assessment.api import AssessmentRootedTagTreeViewset
 
 from . import models, serializers
 
