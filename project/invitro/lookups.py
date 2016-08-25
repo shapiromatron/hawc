1efe9a1e805b6abc79867735e6c7fd5f15f1ba1c
Josh Addington <josh.addington@gmail.com>
2016-08-25 16:42:36 -0400

    add AutoCompleteWidget to invitro fields that would benefit

--- project/invitro/lookups.py
+++ project/invitro/lookups.py
@@ -28,9 +28,69 @@ class IVChemicalPurityLookup(RelatedDistinctStringLookup):
     related_filter = 'study__assessment_id'
 
 
+class IVCellTypeSpeciesLookup(RelatedDistinctStringLookup):
+    model = models.IVCellType
+    distinct_field = 'species'
+    related_filter = 'study__assessment_id'
+
+
+class IVCellTypeStrainLookup(RelatedDistinctStringLookup):
+    model = models.IVCellType
+    distinct_field = 'strain'
+    related_filter = 'study__assessment_id'
+
+
+class IVCellTypeCellTypeLookup(RelatedDistinctStringLookup):
+    model = models.IVCellType
+    distinct_field = 'cell_type'
+    related_filter = 'study__assessment_id'
+
+
+class IVCellTypeTissueLookup(RelatedDistinctStringLookup):
+    model = models.IVCellType
+    distinct_field = 'tissue'
+    related_filter = 'study__assessment_id'
+
+
+class IVCellTypeSourceLookup(RelatedDistinctStringLookup):
+    model = models.IVCellType
+    distinct_field = 'source'
+    related_filter = 'study__assessment_id'
+
+
+class IVExperimentTransfectionLookup(RelatedDistinctStringLookup):
+    model = models.IVExperiment
+    distinct_field = 'transfection'
+    related_filter = 'study__assessment_id'
+
+
+class IVExperimentPositiveControlLookup(RelatedDistinctStringLookup):
+    model = models.IVExperiment
+    distinct_field = 'positive_control'
+    related_filter = 'study__assessment_id'
+
+
+class IVExperimentNegativeControlLookup(RelatedDistinctStringLookup):
+    model = models.IVExperiment
+    distinct_field = 'negative_control'
+    related_filter = 'study__assessment_id'
+
+
+class IVExperimentVehicleControlLookup(RelatedDistinctStringLookup):
+    model = models.IVExperiment
+    distinct_field = 'vehicle_control'
+    related_filter = 'study__assessment_id'
+
+
 class IVEndpointEffectLookup(RelatedDistinctStringLookup):
     model = models.IVEndpoint
     distinct_field = 'effect'
+    related_filter = 'assessment_id'
+
+
+class IVEndpointAssayTypeLookup(RelatedDistinctStringLookup):
+    model = models.IVEndpoint
+    distinct_field = 'assay_type'
     related_filter = 'assessment_id'
 
 
@@ -54,6 +114,16 @@ registry.register(IVChemicalNameLookup)
 registry.register(IVChemicalCASLookup)
 registry.register(IVChemicalSourceLookup)
 registry.register(IVChemicalPurityLookup)
+registry.register(IVCellTypeSpeciesLookup)
+registry.register(IVCellTypeStrainLookup)
+registry.register(IVCellTypeCellTypeLookup)
+registry.register(IVCellTypeTissueLookup)
+registry.register(IVCellTypeSourceLookup)
+registry.register(IVExperimentTransfectionLookup)
+registry.register(IVExperimentPositiveControlLookup)
+registry.register(IVExperimentNegativeControlLookup)
+registry.register(IVExperimentVehicleControlLookup)
 registry.register(IVEndpointEffectLookup)
+registry.register(IVEndpointAssayTypeLookup)
 registry.register(IVEndpointResponseUnitsLookup)
 registry.register(IVEndpointByAssessmentTextLookup)
