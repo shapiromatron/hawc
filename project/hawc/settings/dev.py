4b187b5c1f081a302cdd3a2ebaa54233d6894026
Andy Shapiro <shapiromatron@gmail.com>
2016-11-03 10:06:12 -0400

    add jquery url for offline mode

--- project/hawc/settings/dev.py
+++ project/hawc/settings/dev.py
@@ -18,6 +18,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 CELERY_ALWAYS_EAGER = True
 CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
 
+DEBUG_TOOLBAR_CONFIG = dict(
+    JQUERY_URL='/static/debug/jquery/1.9.1/jquery.js',
+)
 
 CACHES = {
     'default': {
