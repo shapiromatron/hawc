e6e56c63851c7d02b199c261f7155c9edd04960c
JoshAddington <josh.addington@gmail.com>
2017-08-03 10:53:16 -0400

    hotfix: remove required option for dataPivot caption

--- project/summary/forms.py
+++ project/summary/forms.py
@@ -541,6 +541,7 @@ class DataPivotForm(forms.ModelForm):
             self.instance.assessment = assessment
         self.helper = self.setHelper()
         self.fields['settings'].widget.attrs['rows'] = 2
+        self.fields['caption'].required = False
 
     def setHelper(self):
 
