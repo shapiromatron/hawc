513feb8f385f83726eaa3a9feaad99652feea170
JoshAddington <josh.addington@gmail.com>
2017-09-29 12:13:24 -0400

    only show unpublished visuals to admin and assessment memebers

--- project/summary/api.py
+++ project/summary/api.py
@@ -1,6 +1,23 @@
-from assessment.api import AssessmentViewset, DisabledPagination
+from rest_framework.filters import BaseFilterBackend
+from assessment.api import AssessmentViewset, DisabledPagination, InAssessmentFilter
 
 from . import models, serializers
+
+
+class UnpublishedFilter(BaseFilterBackend):
+    """
+        Only show unpublished visuals to admin and assessment members.
+    """
+    def get_unpublished_perms(self, user, view):
+        return ((user.is_superuser) or
+                (user in view.assessment.project_manager.all()) or
+                (user in view.assessment.team_members.all()) or
+                (user in view.assessment.reviewers.all()))
+
+    def filter_queryset(self, request, queryset, view):
+        if not self.get_unpublished_perms(request.user, view):
+            queryset = queryset.filter(published=True)
+        return queryset
 
 
 class DataPivot(AssessmentViewset):
@@ -12,6 +29,7 @@ class DataPivot(AssessmentViewset):
     assessment_filter_args = "assessment"
     model = models.DataPivot
     pagination_class = DisabledPagination
+    filter_backends = (InAssessmentFilter, UnpublishedFilter)
 
     def get_serializer_class(self):
         cls = serializers.DataPivotSerializer
@@ -31,6 +49,7 @@ class Visual(AssessmentViewset):
     assessment_filter_args = "assessment"
     model = models.Visual
     pagination_class = DisabledPagination
+    filter_backends = (InAssessmentFilter, UnpublishedFilter)
 
     def get_serializer_class(self):
         cls = serializers.VisualSerializer
