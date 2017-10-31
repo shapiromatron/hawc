50a0cd962c973749ffd786fcc4f3c4780bbd2639
Comeaux, Justin M (CGI Federal) <justin.comeaux@cgifederal.com>
2017-10-30 20:16:56 -0500

    APPDEV-3805: Define RoB Answers per Metric Admin Interface
    This lets an Admin of HAWC enter data in the new model for Metric Answers.

--- project/riskofbias/admin.py
+++ project/riskofbias/admin.py
@@ -6,4 +6,8 @@ from . import models
 class RiskOfBiasMetricAdmin(admin.ModelAdmin):
     list_display = ('domain', 'name', 'created', 'last_updated')
 
+class RiskOfBiasMetricAnswerAdmin(admin.ModelAdmin):
+    list_display = ('choice_one', 'choice_two', 'choice_three', 'choice_four', 'choice_five', 'choice_six')
+
 admin.site.register(models.RiskOfBiasMetric, RiskOfBiasMetricAdmin)
+admin.site.register(models.RiskOfBiasMetricAnswers, RiskOfBiasMetricAnswerAdmin)
