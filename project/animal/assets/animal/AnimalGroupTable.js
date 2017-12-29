2329715aec4048698687968bc7d9c2773f23cca9
JoshAddington <josh.addington@gmail.com>
2017-12-28 20:43:00 -0500

    hotfix for AnimalGroupTable endpoint display

--- project/animal/assets/animal/AnimalGroupTable.js
+++ project/animal/assets/animal/AnimalGroupTable.js
@@ -55,7 +55,8 @@ class AnimalGroupTable {
                 .each(function(v, i) {
                     if (i === 0) tbl.addRow(v._build_ag_n_row());
                     tbl.addRow(v._build_ag_response_row(tbl.footnotes));
-                });
+                })
+                .value();
         });
     }
 
