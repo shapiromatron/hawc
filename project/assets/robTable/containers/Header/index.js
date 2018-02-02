277ee2c3cb79c4d249ac7bec9147455018f82097
akbennet2 <adam.bennett@cgifederal.com>
2018-02-02 12:55:30 -0500

    APPDEV-3847 - First pass

--- project/assets/robTable/containers/Header/index.js
+++ project/assets/robTable/containers/Header/index.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import $ from '$';
+import Study from 'study/Study';
+import { fetchFullStudyIfNeeded } from 'robTable/actions';
 
 class Header extends Component {
 
     render() {
-        let headerText, smallText,
+        let headerText, smallText, heroID,
             { isForm, display } = this.props.config;
+            console.log("hello");
+            console.log(this.props.config.study.id);
+            this.props.dispatch(fetchFullStudyIfNeeded());
+            console.log(this.props.config.study);
         if(isForm){
             headerText = `${display == 'final' ? 'Final ' : ''}Review edit`;
             smallText = 'Justification for risk of bias assessment of selected study. Each row contains the selected domain, a description of the question to be answered, and an area for the user to detail the bias selection and notes for justification.';
+            heroID = 12345;
+            
         } else if(display == 'all'){
             headerText = 'Show all active reviews';
         }
@@ -19,6 +27,7 @@ class Header extends Component {
                     {headerText}
                 </h3>
                 <span className='help-text'>{smallText}</span>
+                <p><a className='btn btn-mini btn-primary' target='_blank' href={'https://hero.epa.gov/hero/index.cfm/reference/downloads/reference_id/' + heroID}>Full text link <i className='fa fa-fw fa-file-pdf-o'></i></a><span>&nbsp;</span></p>
             </div>
         );
     }
