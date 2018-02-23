2ec3d9ec5530527e9a3652721e4ce37fb031decf
Andy Shapiro <shapiromatron@gmail.com>
2018-02-23 15:58:31 -0500

    add bmds settings to admin

---
+++ project/utils/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+
+class AllListFieldAdmin(admin.ModelAdmin):
+
+    def __init__(self, model, admin_site):
+        self.list_display = [field.name for field in model._meta.fields]
+        super().__init__(model, admin_site)
