222c183fb9c1510d896163c7800503cf907852f0
Andy Shapiro <shapiromatron@gmail.com>
2018-06-06 00:16:29 -0400

    rewrite chemspider using new api https://developer.rsc.org/apis

--- project/utils/chemspider.py
+++ project/utils/chemspider.py
@@ -1,9 +1,6 @@
-import re
+import json
 import logging
-import xml.etree.ElementTree as ET
-
 from django.conf import settings
-
 import requests
 
 
@@ -13,43 +10,43 @@ logger = logging.getLogger(__name__)
 def fetch_chemspider(cas_number):
     d = {}
     try:
-        # get chemspider chem id
-        url = 'http://www.chemspider.com/Search.asmx/SimpleSearch'
-        payload = {
-            'query': cas_number,
-            'token': settings.CHEMSPIDER_TOKEN,
-        }
-        r = requests.post(url, data=payload)
-        id_val = re.search(r'\<int\>(\d+)\</int\>', r.text).group(1)
+        with requests.Session() as session:
+            session.headers.update({
+                'apikey': settings.CHEMSPIDER_TOKEN,
+                'Content-type': 'application/json',
+                'Accept': 'application/json'
+            })
 
-        # get details
-        url = 'http://www.chemspider.com/MassSpecAPI.asmx/GetExtendedCompoundInfo'  # noqa
-        payload = {
-            'CSID': id_val,
-            'token': settings.CHEMSPIDER_TOKEN,
-        }
-        r = requests.post(url, data=payload)
-        xml = ET.fromstring(r.text)
-        namespace = '{http://www.chemspider.com/}'
-        d['CommonName'] = xml.find('{}CommonName'.format(namespace)).text
-        d['SMILES'] = xml.find('{}SMILES'.format(namespace)).text
-        d['MW'] = xml.find('{}MolecularWeight'.format(namespace)).text
+            # submit query
+            url = 'https://api.rsc.org/compounds/v1/filter/name'
+            payload = {"name": cas_number}
+            response = session.post(url, data=json.dumps(payload))
+            query_id = response.json()['queryId']
 
-        # get image
-        url = 'http://www.chemspider.com/Search.asmx/GetCompoundThumbnail'
-        payload = {
-            'id': id_val,
-            'token': settings.CHEMSPIDER_TOKEN,
-        }
-        r = requests.post(url, data=payload)
-        xml = ET.fromstring(r.text)
-        d['image'] = xml.text
+            # get chemspider id
+            url = f'https://api.rsc.org/compounds/v1/filter/{query_id}/results'
+            params = {'start': 0, 'count': 1}
+            response = session.get(url, params=params)
+            chemspider_id = response.json()['results'][0]
 
-        # call it a success if we made it here
-        d['status'] = 'success'
+            # get details
+            url = f'https://api.rsc.org/compounds/v1/records/{chemspider_id}/details'
+            params = {'fields': 'SMILES,MolecularWeight,CommonName'}
+            response = session.get(url, params=params).json()
+            d['CommonName'] = response['commonName']
+            d['SMILES'] = response['smiles']
+            d['MW'] = response['molecularWeight']
+
+            # get image
+            url = f'https://api.rsc.org/compounds/v1/records/{chemspider_id}/image'
+            response = session.get(url)
+            d['image'] = response.json()['image']
+
+            # call it a success if we made it here
+            d['status'] = 'success'
 
     except AttributeError as e:
-        logger.error(f"Request failed: {r.text}", exc_info=True)
+        logger.error(f"Request failed: {response.text}", exc_info=True)
 
     except Exception as e:
         logger.error(str(e), exc_info=True)
