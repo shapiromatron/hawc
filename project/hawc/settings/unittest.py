26baa27a9dad293bfd0d13c3d4ebafb6fe4f19b4
Andy Shapiro <shapiromatron@gmail.com>
2018-06-05 23:36:12 -0400

    fix py.test and ensure unit tests work

--- project/hawc/settings/unittest.py
+++ project/hawc/settings/unittest.py
@@ -25,15 +25,3 @@ PASSWORD_HASHERS = (
 
 
 logging.disable(logging.CRITICAL)
-
-
-class DisableMigrations(object):
-
-    def __contains__(self, item):
-        return True
-
-    def __getitem__(self, item):
-        return 'notmigrations'
-
-
-MIGRATION_MODULES = DisableMigrations()
