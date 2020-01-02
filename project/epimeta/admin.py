c446723f758c41ed42f60d9edf2d894bb8be2944
Andy Shapiro <shapiromatron@gmail.com>
2020-01-02 15:38:13 -0500

    add basic admin pages for epimeta and invitro

---
+++ project/epimeta/admin.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+
+from . import models
+
+
+@admin.register(models.MetaProtocol)
+class MetaProtocolAdmin(admin.ModelAdmin):
+    search_fields = (
+        'name',
+        'study__short_citation',
+    )
+    list_display = (
+        "name",
+        "study",
+        "protocol_type",
+    )
+
+
+@admin.register(models.MetaResult)
+class MetaResultAdmin(admin.ModelAdmin):
+    list_display = (
+        'protocol',
+        'label',
+        'data_location',
+        'health_outcome',
+        'exposure_name',
+        'estimate',
+        'lower_ci',
+        'upper_ci',
+    )
+
+
+@admin.register(models.SingleResult)
+class SingleResultAdmin(admin.ModelAdmin):
+    list_display = (
+        'meta_result',
+        'study',
+        'exposure_name',
+        'weight',
+        'n',
+        'estimate',
+        'lower_ci',
+        'upper_ci',
+    )
