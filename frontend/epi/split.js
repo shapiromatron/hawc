d2aeb3835bb5bce44d0066d6051b1b6270022857
Andy Shapiro <shapiromatron@gmail.com>
2020-01-30 17:47:17 -0500

    Hero search (#166)
    - hotfix - repair export of epi.js code for template use
    - update litter_getter==0.2.6
    - Use new PUBMED_API_KEY where needed
    - Use new HERO API

--- frontend/epi/split.js
+++ frontend/epi/split.js
@@ -1,7 +1,17 @@
-const startup = function(cb) {
-    import("./index.js").then(epi => {
-        cb(epi.default);
-    });
-};
+import ComparisonSet from "./ComparisonSet";
+import Exposure from "./Exposure";
+import Group from "./Group";
+import Outcome from "./Outcome";
+import OutcomeListTable from "./OutcomeListTable";
+import Result from "./Result";
+import StudyPopulation from "./StudyPopulation";
 
-export default startup;
+export default {
+    ComparisonSet,
+    Exposure,
+    Group,
+    Outcome,
+    OutcomeListTable,
+    Result,
+    StudyPopulation,
+};
