2d03ebd214eddbfd4f4c0f328509a45fa0e22ac1
Andy Shapiro <shapiromatron@gmail.com>
2020-03-20 15:53:06 -0400

    histogram literature by year (#183)
    * create new histogram plot with fake data
    
    * create histogram
    
    * handle edge cases w/ few references or many years
    
    * more edge-case refinement; test plotly api with really simple case
    
    Co-authored-by: Rice <RICE.RICHARD@EPA.GOV>

--- hawc/apps/lit/api.py
+++ hawc/apps/lit/api.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import plotly.express as px
 from rest_framework import decorators, mixins, viewsets
 from rest_framework.response import Response
 
@@ -55,6 +57,43 @@ class LiteratureAssessmentViewset(LegacyAssessmentAdapterMixin, viewsets.Generic
 
         df = models.ReferenceTags.objects.as_dataframe(instance.id)
         return Response(df)
+
+    @decorators.detail_route(methods=("get",))
+    def reference_year_histogram(self, request, pk):
+        instance = self.get_object()
+        # get all the years for a given assessment
+        years = list(
+            models.Reference.objects.filter(assessment_id=instance.id, year__gt=0).values_list(
+                "year", flat=True
+            )
+        )
+        payload = {}
+        if len(years) > 0:
+
+            df = pd.DataFrame(years, columns=["Year"])
+            nbins = min(max(df.Year.max() - df.Year.min() + 1, 4), 30)
+
+            try:
+                fig = px.histogram(df, x="Year", nbins=nbins)
+            except ValueError:
+                # in some cases a bad nbins can be provided; just use default bins instead
+                # Invalid value of type 'numpy.int64' received for the 'nbinsx' property of histogram
+                # [2005, 2013, 1995, 2001, 2017, 1991, 1991, 2009, 2006, 2005]; nbins=27
+                fig = px.histogram(df, x="Year")
+
+            fig.update_yaxes(title_text="# References")
+            fig.update_xaxes(title_text="Year")
+            fig.update_traces(marker=dict(color="#003d7b"))
+
+            fig.update_layout(
+                bargap=0.1,
+                plot_bgcolor="white",
+                autosize=True,
+                margin=dict(l=0, r=0, t=30, b=0),  # noqa: E741
+            )
+            payload = fig.to_dict()
+
+        return Response(payload)
 
     @decorators.detail_route(
         methods=("get",), url_path="references-download", renderer_classes=PandasRenderers
