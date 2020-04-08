8374fd5240c25606adadc2f1c3da31539c92743b
Andy Shapiro <shapiromatron@gmail.com>
2020-04-07 22:32:35 -0400

    - use standard django class-based views for registration - update all emails to be lowercased in db save - update profile to set hero access based on email domain

--- hawc/apps/myuser/models.py
+++ hawc/apps/myuser/models.py
@@ -71,6 +71,10 @@ class HAWCUser(AbstractBaseUser, PermissionsMixin):
         msg.attach_alternative(html, "text/html")
         msg.send()
 
+    def create_profile(self) -> "UserProfile":
+        HERO_access = True if "@epa.gov" in self.email else False
+        return UserProfile.objects.create(user=self, HERO_access=HERO_access)
+
 
 class UserProfile(models.Model):
     objects = managers.UserProfileManager()
