23612c11d20584ef3d42f9bcd0d25a63dd97d797
Andy Shapiro <shapiromatron@gmail.com>
2020-04-26 07:36:27 -0400

    add more celery settings

--- compose/django/celery.sh
+++ compose/django/celery.sh
@@ -8,6 +8,4 @@ sleep 10
 exec /usr/local/bin/celery worker \
     --app=hawc.main.celery \
     --loglevel=INFO \
-    --logfile=$LOGFILE \
-    --soft-time-limit=90 \
-    --time-limit=120
+    --logfile=$LOGFILE
