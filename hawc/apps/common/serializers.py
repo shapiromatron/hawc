ca559bc030bfd876ea25e09b35d5f4a0e3827d4f
Daniel Rabstejnek <djrabste@ncsu.edu>
2020-05-07 09:33:14 -0400

    Wrapped class docstring.

--- hawc/apps/common/serializers.py
+++ hawc/apps/common/serializers.py
@@ -3,7 +3,8 @@ from rest_framework import serializers
 
 class UnusedSerializer(serializers.Serializer):
     """
-    Nondescript serializer. Used in instances where a view does not use a serializer but one needs to be set, such as for OpenAPI generation.
+    Nondescript serializer. Used in instances where a view does not use a serializer
+    but one needs to be set, such as for OpenAPI generation.
     """
 
     pass
