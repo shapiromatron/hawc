d06b30201461012a30604676136b44866590cf75
Andy Shapiro <shapiromatron@gmail.com>
2020-06-08 22:06:30 -0400

    capture get data handle messages

--- frontend/summary/summary/ExploreHeatmap.js
+++ frontend/summary/summary/ExploreHeatmap.js
@@ -4,6 +4,8 @@ import HAWCModal from "utils/HAWCModal";
 import ExploreHeatmapPlot from "./ExploreHeatmapPlot";
 import $ from "$";
 
+import h from "shared/utils/helpers";
+
 class ExploreHeatmap extends BaseVisual {
     constructor(data, dataset) {
         super(data);
@@ -29,17 +31,21 @@ class ExploreHeatmap extends BaseVisual {
         };
     }
 
-    getDataset() {
+    getDataset(callback) {
         const url = this.data.settings.data_url;
-        if (!this.dataset) {
-            this.dataset = JSON.parse(
-                $.ajax(url, {
-                    async: false,
-                    dataType: "json",
-                }).responseText
-            );
+        if (this.dataset) {
+            callback({dataset: this.dataset});
+        } else {
+            fetch(url, h.fetchGet)
+                .then(response => response.json())
+                .then(json => {
+                    this.dataset = json;
+                    callback({dataset: json});
+                })
+                .catch(error => {
+                    callback({error});
+                });
         }
-        return this.dataset;
     }
 
     displayAsPage($el, options) {
@@ -47,20 +53,35 @@ class ExploreHeatmap extends BaseVisual {
             captionDiv = $("<div>").html(this.data.caption),
             caption = new SmartTagContainer(captionDiv),
             $plotDiv = $("<div>"),
-            data = {
-                settings: this.getSettings(),
-                dataset: this.getDataset(),
+            callback = resp => {
+                if (window.isEditable) {
+                    title.append(this.addActionsMenu());
+                }
+                if (resp.dataset) {
+                    const data = {
+                        settings: this.getSettings(),
+                        dataset: resp.dataset,
+                    };
+
+                    $el.empty().append($plotDiv);
+                    if (!options.visualOnly) {
+                        $el.prepend(title).append(captionDiv);
+                    }
+
+                    new ExploreHeatmapPlot(this, data, options).render($plotDiv);
+
+                    caption.renderAndEnable();
+                } else if (resp.error) {
+                    $el.empty()
+                        .prepend(title)
+                        .append(this.getErrorDiv());
+                } else {
+                    throw "Unknown status.";
+                }
             };
+
         options = options || {};
-
-        if (window.isEditable) title.append(this.addActionsMenu());
-
-        $el.empty().append($plotDiv);
-        if (!options.visualOnly) $el.prepend(title).append(captionDiv);
-
-        new ExploreHeatmapPlot(this, data, options).render($plotDiv);
-        caption.renderAndEnable();
-        return this;
+        this.getDataset(callback);
     }
 
     displayAsModal(options) {
@@ -71,21 +92,42 @@ class ExploreHeatmap extends BaseVisual {
             caption = new SmartTagContainer(captionDiv),
             $plotDiv = $("<div>"),
             modal = new HAWCModal(),
-            data = {
-                settings: this.getSettings(),
-                dataset: this.getDataset(),
+            callback = resp => {
+                if (resp.error) {
+                    const data = {
+                        settings: this.getSettings(),
+                        dataset: resp.dataset,
+                    };
+
+                    modal.getModal().on("shown", function() {
+                        new ExploreHeatmapPlot(self, data, options).render($plotDiv);
+                        caption.renderAndEnable();
+                    });
+
+                    modal
+                        .addHeader($("<h4>").text(this.data.title))
+                        .addBody([$plotDiv, captionDiv])
+                        .addFooter("")
+                        .show({maxWidth: 1200});
+                } else if (resp.error) {
+                    modal
+                        .addHeader($("<h4>").text(this.data.title))
+                        .addBody(this.getErrorDiv())
+                        .addFooter("")
+                        .show({maxWidth: 1200});
+                } else {
+                    throw "Unknown status.";
+                }
             };
 
-        modal.getModal().on("shown", function() {
-            new ExploreHeatmapPlot(self, data, options).render($plotDiv);
-            caption.renderAndEnable();
-        });
+        options = options || {};
+        this.getDataset(callback);
+    }
 
-        modal
-            .addHeader($("<h4>").text(this.data.title))
-            .addBody([$plotDiv, captionDiv])
-            .addFooter("")
-            .show({maxWidth: 1200});
+    getErrorDiv() {
+        return `<div class="alert alert-danger" role="alert">
+            <i class="fa fa-exclamation-circle"></i>&nbsp;An error occurred; please modify settings...
+        </div>`;
     }
 }
 
