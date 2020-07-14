51cccc281b6879179f1a7c674a9763e942a54f38
Daniel Rabstejnek <rabstejnek@gmail.com>
2020-07-14 10:47:24 -0400

    Animal bioassay create API (#222)
    * Add REST API for creating bioassay endpoints
    * Update HAWC client to use new API
    * Add example notebook for using API

--- hawc/apps/common/serializers.py
+++ hawc/apps/common/serializers.py
@@ -1,3 +1,6 @@
+from typing import Dict, List
+
+from django.db import models
 from rest_framework import serializers
 
 
@@ -12,3 +15,65 @@ class UnusedSerializer(serializers.Serializer):
 
 class HeatmapQuerySerializer(serializers.Serializer):
     unpublished = serializers.BooleanField(default=False)
+
+
+def get_matching_instance(Model: models.Model, data: Dict, field_name: str) -> models.Model:
+    """
+    Return a matching django model object or throw ValidationError if not found.
+
+    Args:
+        model: (models.Model): The model to fetch an instance of
+        data (Dict): the data dictionary
+        field_name (str): the ID field name
+
+    Returns:
+        models.Model: A matching model instance
+    """
+    err = {}
+    id_ = data.get(field_name)
+
+    if id_ is None:
+        err[field_name] = f"{field_name} is required."
+        raise serializers.ValidationError(err)
+
+    try:
+        return Model.objects.get(id=id_)
+    except ValueError:
+        err[field_name] = f"`{field_name} must be a number; got {id_}."
+        raise serializers.ValidationError(err)
+    except models.ObjectDoesNotExist:
+        err[field_name] = f"{Model.__name__} {id_} does not exist."
+        raise serializers.ValidationError(err)
+
+
+def get_matching_instances(Model: models.Model, data: Dict, field_name: str) -> List[models.Model]:
+    """
+    Return a matching django models list or throw ValidationError if not found.
+
+    Args:
+        model: (models.Model): The model to fetch an instance of
+        data (Dict): the data dictionary
+        field_name (str): the IDs field name
+
+    Returns:
+        models.Model: A list of matching model instances
+    """
+    err = {}
+    ids = data.get(field_name)
+
+    if ids is None:
+        err[field_name] = f"{field_name} is required."
+        raise serializers.ValidationError(err)
+
+    instances = []
+    for id_ in ids:
+        try:
+            instances.append(Model.objects.get(id=id_))
+        except ValueError:
+            err[field_name] = f"`{field_name} must be a number; got {id_}."
+            raise serializers.ValidationError(err)
+        except models.ObjectDoesNotExist:
+            err[field_name] = f"{Model.__name__} {id_} does not exist."
+            raise serializers.ValidationError(err)
+
+    return instances
