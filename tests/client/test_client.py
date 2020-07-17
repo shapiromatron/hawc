1ae9ea553fd174be4b65eff710dacc910a8d5059
Andy Shapiro <shapiromatron@gmail.com>
2020-07-17 12:04:46 -0400

    add tests to ensure our client create methods for animal data works

--- tests/client/test_client.py
+++ tests/client/test_client.py
@@ -3,6 +3,8 @@ import pytest
 from django.core.cache import cache
 from django.test import LiveServerTestCase, TestCase
 
+from hawc.apps.animal.models import Experiment
+from hawc.apps.assessment.models import Strain
 from hawc_client import BaseClient, HawcClient, HawcClientException
 
 
@@ -56,6 +58,140 @@ class TestClient(LiveServerTestCase, TestCase):
         client = HawcClient(self.live_server_url)
         response = client.animal.endpoints(self.db_keys.assessment_client)
         assert isinstance(response, list)
+
+    def test_animal_create(self):
+        """
+        Test all the create methods.  This only checks the happy-path and confirms that the client
+        methods work as intended.
+        """
+        client = HawcClient(self.live_server_url)
+        client.authenticate("pm@pm.com", "pw")
+
+        # experiment
+        experiment_name = "30 day oral"
+        data = dict(
+            study_id=self.db_keys.study_working,
+            name=experiment_name,
+            type="St",
+            has_multiple_generations=False,
+            chemical="2,3,7,8-Tetrachlorodibenzo-P-dioxin",
+            cas="1746-01-6",
+            chemical_source="ABC Inc.",
+            purity_available=True,
+            purity_qualifier="≥",
+            purity=99.9,
+            vehicle="DMSO",
+            guideline_compliance="not reported",
+            description="Deatils here.",
+        )
+        experiment = client.animal.create_experiment(data)
+        assert isinstance(experiment, dict) and experiment["name"] == experiment_name
+
+        # animal group + dosing regime + doses
+        animal_group_name = "Female C57BL/6 Mice"
+        strain = Strain.objects.first()
+        data = dict(
+            experiment_id=experiment["id"],
+            name=animal_group_name,
+            species=strain.species_id,
+            strain=strain.id,
+            sex="F",
+            animal_source="Charles River",
+            lifestage_exposed="Adult",
+            lifestage_assessed="Adult",
+            generation="",
+            comments="Detailed comments here",
+            diet="...",
+            dosing_regime=dict(
+                route_of_exposure="OR",
+                duration_exposure=30,
+                duration_exposure_text="30 days",
+                duration_observation=180,
+                num_dose_groups=3,
+                positive_control=True,
+                negative_control="VT",
+                description="...",
+                doses=[
+                    {"dose_group_id": 0, "dose": 0, "dose_units_id": 1},
+                    {"dose_group_id": 1, "dose": 50, "dose_units_id": 1},
+                    {"dose_group_id": 2, "dose": 100, "dose_units_id": 1},
+                    {"dose_group_id": 0, "dose": 0, "dose_units_id": 2},
+                    {"dose_group_id": 1, "dose": 3.7, "dose_units_id": 2},
+                    {"dose_group_id": 2, "dose": 11.4, "dose_units_id": 2},
+                ],
+            ),
+        )
+        animal_group = client.animal.create_animal_group(data)
+        assert isinstance(experiment, dict) and animal_group["name"] == animal_group_name
+
+        # endpoint + endpoint group
+        data = dict(
+            animal_group_id=animal_group["id"],
+            name="Relative liver weight",
+            system="Hepatic",
+            organ="Liver",
+            effect="Organ weight",
+            effect_subtype="Relative weight",
+            litter_effects="NA",
+            litter_effect_notes="",
+            observation_time=104,
+            observation_time_units=5,
+            observation_time_text="104 weeks",
+            data_location="Figure 2B",
+            expected_adversity_direction=3,
+            response_units="g/100g BW",
+            data_type="C",
+            variance_type=1,
+            confidence_interval=0.95,
+            NOEL=1,  # should be the corresponding dose_group_id below or -999
+            LOEL=2,  # should be the corresponding dose_group_id below or -999
+            FEL=-999,  # should be the corresponding dose_group_id below or -999
+            data_reported=True,
+            data_extracted=True,
+            values_estimated=False,
+            monotonicity=8,
+            statistical_test="ANOVA + Dunnett's test",
+            trend_value=0.0123,
+            trend_result=2,
+            diagnostic="...",
+            power_notes="...",
+            results_notes="...",
+            endpoint_notes="...",
+            groups=[
+                dict(
+                    dose_group_id=0,
+                    n=10,
+                    incidence=None,
+                    response=4.35,
+                    variance=0.29,
+                    significant=False,
+                    significance_level=None,
+                ),
+                dict(
+                    dose_group_id=1,
+                    n=10,
+                    incidence=None,
+                    response=5.81,
+                    variance=0.47,
+                    significant=False,
+                    significance_level=None,
+                ),
+                dict(
+                    dose_group_id=2,
+                    n=10,
+                    incidence=None,
+                    response=7.72,
+                    variance=0.63,
+                    significant=True,
+                    significance_level=0.035,
+                ),
+            ],
+        )
+        endpoint = client.animal.create_endpoint(data)
+        assert isinstance(endpoint, dict) and endpoint["id"] > 0
+
+        # test cleanup; remove what we just created
+        Experiment.objects.filter(id=experiment["id"]).delete()
 
     ##########################
     # AssessmentClient tests #
