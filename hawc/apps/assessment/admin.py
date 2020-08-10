4e4bdf320b980cc3dc5fa62432dc1d58aaa6582e
Daniel Rabstejnek <rabstejnek@gmail.com>
2020-08-10 10:57:53 -0400

    Made basic admin view for DSSTox

--- hawc/apps/assessment/admin.py
+++ hawc/apps/assessment/admin.py
@@ -145,3 +145,59 @@ class TimeSpentEditingAdmin(admin.ModelAdmin):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.list_display_links = []
+
+
+@admin.register(models.DSSTox)
+class DSSXToxAdmin(admin.ModelAdmin):
+    list_display = (
+        "dtxsid",
+        "get_casrns",
+        "get_assessments",
+        "get_experiments",
+        "get_ivchemicals",
+    )
+    search_fields = ("dtxsid", "assessments__cas", "experiments__cas", "ivchemicals__cas")
+
+    def queryset(self, request):
+        qs = super().queryset(request)
+        return qs.prefetch_related("assessments", "experiments", "ivchemicals")
+
+    def get_ul(self, iterable, func):
+        ul = ["<ul>"]
+        for obj in iterable:
+            ul.append(f"<li>{func(obj)}</li>")
+        ul.append("</ul>")
+        return format_html(" ".join(ul))
+
+    def linked_name(self, obj):
+        return f"<a href='{obj.get_absolute_url()}'>{obj.name}</a>"
+
+    def get_assessments(self, obj):
+        return self.get_ul(obj.assessments.all(), self.linked_name)
+
+    def get_experiments(self, obj):
+        return self.get_ul(obj.experiments.all(), self.linked_name)
+
+    def get_ivchemicals(self, obj):
+        return self.get_ul(obj.ivchemicals.all(), self.linked_name)
+
+    def get_casrns(self, obj):
+        casrns = set()
+        casrns.update(obj.assessments.all().values_list("cas", flat=True))
+        casrns.update(obj.experiments.all().values_list("cas", flat=True))
+        casrns.update(obj.ivchemicals.all().values_list("cas", flat=True))
+
+        return self.get_ul(casrns, lambda casrn: casrn)
+
+    get_casrns.short_description = "CASRN"
+    get_casrns.allow_tags = True
+
+    get_assessments.short_description = "Assessments"
+    get_assessments.allow_tags = True
+
+    get_experiments.short_description = "Experiments"
+    get_experiments.allow_tags = True
+
+    get_ivchemicals.short_description = "IVChemicals"
+    get_ivchemicals.allow_tags = True
+
