42199129b276890b7027ecb5a0071ac15414651e
Daniel Rabstejnek <rabstejnek@gmail.com>
2020-08-19 12:21:20 -0400

    Literature tagtree: hide no-reference tags (#276)
    * Added tagtree function that prunes no-reference nodes.
    
    * Removed unnecessary file.
    
    * Added checkbox to toggle pruning no reference nodes.
    
    * Added option to hide empty nodes / show checkbox on lit tagtree viz
    
    * Renamed 'hide_empty' to 'hide_empty_tag_nodes', and removed 'show_checkbox'.
    
    * Added migration to add 'hide_empty_tag_nodes' setting.
    
    * Commented in explanation of pruning functionality.
    
    * Updated lit review viz to use right setting.
    
    * updates
    
    * updates
    
    Co-authored-by: Andy Shapiro <shapiromatron@gmail.com>

---
+++ hawc/apps/summary/migrations/0027_tagtree_hide_empty_tag_nodes.py
@@ -0,0 +1,44 @@
+import json
+
+from django.db import migrations
+
+literature_tagtree = 4
+
+
+def add_hide_empty_tag_nodes(apps, schema_editor):
+    for visual in apps.get_model("summary", "Visual").objects.filter(
+        visual_type=literature_tagtree
+    ):
+        try:
+            settings = json.loads(visual.settings)
+        except json.JSONDecodeError:
+            continue
+
+        settings["hide_empty_tag_nodes"] = False
+        visual.settings = json.dumps(settings)
+        visual.save()
+
+
+def remove_hide_empty_tag_nodes(apps, schema_editor):
+    for visual in apps.get_model("summary", "Visual").objects.filter(
+        visual_type=literature_tagtree
+    ):
+        try:
+            settings = json.loads(visual.settings)
+        except json.JSONDecodeError:
+            continue
+
+        settings.pop("hide_empty_tag_nodes", None)
+        visual.settings = json.dumps(settings)
+        visual.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("summary", "0026_rob_heatmap_excludes"),
+    ]
+
+    operations = [
+        migrations.RunPython(add_hide_empty_tag_nodes, reverse_code=remove_hide_empty_tag_nodes),
+    ]
