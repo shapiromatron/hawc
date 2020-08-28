2032a158a1d6ea0eb36b33112c3e203ccd6f5fb6
Daniel Rabstejnek <rabstejnek@gmail.com>
2020-08-28 12:19:21 -0400

    Renamed and commented job permission class.

--- hawc/apps/assessment/api.py
+++ hawc/apps/assessment/api.py
@@ -52,7 +52,13 @@ def get_assessment_from_query(request) -> Optional[models.Assessment]:
     return models.Assessment.objects.filter(pk=assessment_id).first()
 
 
-class AssessmentForeignKeyPermissions(permissions.BasePermission):
+class JobPermissions(permissions.BasePermission):
+    """
+    Requires admin permissions where jobs have no associated assessment
+    or when part of a list, and assessment level permissions when jobs
+    have an associated assessment.
+    """
+
     def has_object_permission(self, request, view, obj):
         if obj.assessment is None:
             return bool(request.user and request.user.is_staff)
@@ -73,6 +79,8 @@ class AssessmentForeignKeyPermissions(permissions.BasePermission):
             else:
                 return assessment.user_can_edit_object(request.user)
         else:
+            # other actions are object specific,
+            # and will be caught by object permissions
             return True
 
 
@@ -496,7 +504,7 @@ class JobViewset(
 ):
     model = models.Job
     serializer_class = serializers.JobSerializer
-    permission_classes = (AssessmentForeignKeyPermissions,)
+    permission_classes = (JobPermissions,)
     pagination_class = None
 
     def get_queryset(self):
