eb7272eb259999fd35f2bc16c1faab811e4a7e5d
Andy Shapiro <shapiromatron@gmail.com>
2020-08-31 12:23:18 -0400

    remove toolbar; add credentials

--- hawc/main/settings/unittest.py
+++ hawc/main/settings/unittest.py
@@ -6,6 +6,10 @@ from .dev import *  # noqa
 
 DEBUG = True
 
+# remove toolbar for selenium tests
+INSTALLED_APPS = [app for app in INSTALLED_APPS if app != "debug_toolbar"]
+MIDDLEWARE = [middleware for middleware in MIDDLEWARE if "debug_toolbar" not in middleware]
+
 HAWC_FLAVOR = "PRIME"
 
 DATABASES["default"]["NAME"] = "hawc-fixture-test"
