147f59442b31f2fdd63735ab434326762db78efe
Daniel Rabstejnek <rabstejnek@gmail.com>
2020-08-31 21:05:30 -0400

    ''

--- tests/integration/test_hello_helium.py
+++ tests/integration/test_hello_helium.py
@@ -23,3 +23,22 @@ def test_hello_helium(chrome_driver):
     h.write("not my password", into="Password*")
     h.click("Login")
     assert h.Text(msg).exists() is True
+
+
+@pytest.mark.skipif(SKIP_INTEGRATION, reason="integration test")
+def test_local(chrome_driver):
+    # set test to use our session-level driver
+    h.set_driver(chrome_driver)
+
+    # go to website
+    h.go_to("http://web-server")
+    h.click("Login")
+    assert "/user/login/" in chrome_driver.current_url
+
+    # invalid password check
+    msg = "Please enter a correct email and password."
+    assert h.Text(msg).exists() is False
+    h.write("webmaster@hawcproject.org", into="Email*")
+    h.write("not my password", into="Password*")
+    h.click("Login")
+    assert h.Text(msg).exists() is True
