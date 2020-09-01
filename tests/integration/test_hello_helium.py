95c0df01824b896cead7d271fa4bf71cc2d0f53d
Daniel Rabstejnek <rabstejnek@gmail.com>
2020-08-31 22:11:42 -0400

    ''

--- tests/integration/test_hello_helium.py
+++ tests/integration/test_hello_helium.py
@@ -2,43 +2,44 @@ import os
 
 import helium as h
 import pytest
+from django.test import LiveServerTestCase
 
 SKIP_INTEGRATION = os.environ.get("HAWC_INTEGRATION_TESTS") is None
 
 
-@pytest.mark.skipif(SKIP_INTEGRATION, reason="integration test")
-def test_hello_helium(chrome_driver):
-    # set test to use our session-level driver
-    h.set_driver(chrome_driver)
+class SeleniumTest(LiveServerTestCase):
+    @pytest.mark.skipif(SKIP_INTEGRATION, reason="integration test")
+    def test_hello_helium(chrome_driver):
+        # set test to use our session-level driver
+        h.set_driver(chrome_driver)
 
-    # go to website
-    h.go_to("https://hawcproject.org")
-    h.click("Login")
-    assert "/user/login/" in chrome_driver.current_url
+        # go to website
+        h.go_to("https://hawcproject.org")
+        h.click("Login")
+        assert "/user/login/" in chrome_driver.current_url
 
-    # invalid password check
-    msg = "Please enter a correct email and password."
-    assert h.Text(msg).exists() is False
-    h.write("webmaster@hawcproject.org", into="Email*")
-    h.write("not my password", into="Password*")
-    h.click("Login")
-    assert h.Text(msg).exists() is True
+        # invalid password check
+        msg = "Please enter a correct email and password."
+        assert h.Text(msg).exists() is False
+        h.write("webmaster@hawcproject.org", into="Email*")
+        h.write("not my password", into="Password*")
+        h.click("Login")
+        assert h.Text(msg).exists() is True
 
+    @pytest.mark.skipif(SKIP_INTEGRATION, reason="integration test")
+    def test_local(chrome_driver):
+        # set test to use our session-level driver
+        h.set_driver(chrome_driver)
 
-@pytest.mark.skipif(SKIP_INTEGRATION, reason="integration test")
-def test_local(chrome_driver):
-    # set test to use our session-level driver
-    h.set_driver(chrome_driver)
+        # go to website
+        h.go_to("http://web-server")
+        h.click("Login")
+        assert "/user/login/" in chrome_driver.current_url
 
-    # go to website
-    h.go_to("http://web-server")
-    h.click("Login")
-    assert "/user/login/" in chrome_driver.current_url
-
-    # invalid password check
-    msg = "Please enter a correct email and password."
-    assert h.Text(msg).exists() is False
-    h.write("webmaster@hawcproject.org", into="Email*")
-    h.write("not my password", into="Password*")
-    h.click("Login")
-    assert h.Text(msg).exists() is True
+        # invalid password check
+        msg = "Please enter a correct email and password."
+        assert h.Text(msg).exists() is False
+        h.write("webmaster@hawcproject.org", into="Email*")
+        h.write("not my password", into="Password*")
+        h.click("Login")
+        assert h.Text(msg).exists() is True
