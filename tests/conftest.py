b87892a0c46c884990c4a9f4ac42d20cdf2426de
Daniel Rabstejnek <rabstejnek@gmail.com>
2020-08-31 20:45:03 -0400

    ''

--- tests/conftest.py
+++ tests/conftest.py
@@ -107,7 +107,7 @@ def chrome_driver():
     options = webdriver.FirefoxOptions()
     options.add_argument("--window-size=1920,1080")
     driver = webdriver.Remote(
-        command_executor="http://127.0.0.1:4444/wd/hub",
+        command_executor="http://selenium-server:4444/wd/hub",
         desired_capabilities=DesiredCapabilities.FIREFOX,
         options=options,
     )
