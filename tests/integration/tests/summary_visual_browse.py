15b799042197c2fd6f1d0f66ac362726be791232
Daniel Rabstejnek <rabstejnek@gmail.com>
2020-09-08 22:58:19 -0400

    Print href of link.

--- tests/integration/tests/summary_visual_browse.py
+++ tests/integration/tests/summary_visual_browse.py
@@ -41,6 +41,7 @@ def summary_visual_browse(driver, live_server_url):
 
     # click the heatmap example
     h.wait_until(h.Link("heatmap").exists, 60)
+    print(h.Link("heatmap").href)
     h.click(h.Link("heatmap"))
 
     assert "/summary/visual/3/" in driver.current_url
