9289db7883647a3dfdba627f2691e7685352f5b5
Andy Shapiro <shapiromatron@gmail.com>
2020-09-10 11:13:20 -0400

    selenium integration tests (#261)
    Expand frontend HAWC test suite to include integration tests, to ensure that javascript rich pages are properly being rendered and are communicating correctly with the backend API.

---
+++ tests/integration/tests/__init__.py
@@ -0,0 +1,4 @@
+from .login import login  # noqa: F401
+from .rob_browse import rob_browse  # noqa: F401
+from .summary_visual_browse import summary_visual_browse  # noqa: F401
+from .user_permissions import user_permissions  # noqa: F401
