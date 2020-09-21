1626f01561c5af7d34fdc1dff585081e014e0df3
Daniel Rabstejnek <rabstejnek@gmail.com>
2020-09-20 22:03:18 -0400

    Renumbered assessment migrations (#291)

--- hawc/apps/assessment/migrations/0023_dataset_published.py
+++ hawc/apps/assessment/migrations/0023_dataset_published.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("assessment", "0021_endpoint_ordering"),
+        ("assessment", "0022_add_vocabulary"),
     ]
 
     operations = [
