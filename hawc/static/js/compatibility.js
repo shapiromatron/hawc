dceb013a06ff15414fff26abd148b3ad8503aba4
Andy Shapiro <shapiromatron@gmail.com>
2020-10-05 12:22:48 -0400

    remove admin/js/urlify

--- hawc/static/js/compatibility.js
+++ hawc/static/js/compatibility.js
@@ -7,9 +7,8 @@ var supportedBrowserCheck = function() {
     Tested using github gist:
         https://gist.github.com/shapiromatron/bae66a34e30c8d6c518676f39592fed4
 
-    Validated that the error is shown on IE11 but not on the chrome, firefox, or edge.
+    Validated that the error is shown on IE11 but not on the chrome, firefox, safari, or edge.
     */
-
     var isSupportedBrowser = function () {
         // not ideal; but <IE12 isn't supported which is primary-goal:
         // http://webaim.org/blog/user-agent-string-history/
@@ -20,17 +19,17 @@ var supportedBrowserCheck = function() {
         return isChrome || isFirefox || isSafari;
     }
 
-    if (isSupportedBrowser()) {
+    if (!isSupportedBrowser()) {
         $("#container").prepend(
             '<div class="alert">\
-                    <p><b>Warning:</b> Your current browser has not been tested extensively with this website, which may result in some some errors with functionality. The following browsers are fully supported:</p>\
-                    <ul>\
-                        <li><a href="https://www.google.com/chrome/" target="_blank">Google Chrome</a> (preferred)</li>\
-                        <li><a href="https://www.mozilla.org/firefox/" target="_blank">Mozilla Firefox</a></li>\
-                        <li><a href="https://www.apple.com/safari/" target="_blank">Apple Safari</a></li>\
-                    </ul>\
-                    <p>Please use a different browser for an optimal experience.</p>\
-                </div>'
+                <p><b>Warning:</b> Your current browser has not been tested extensively with this website, which may result in some some errors with functionality. The following browsers are fully supported:</p>\
+                <ul>\
+                    <li><a href="https://www.google.com/chrome/" target="_blank">Google Chrome</a> (preferred)</li>\
+                    <li><a href="https://www.mozilla.org/firefox/" target="_blank">Mozilla Firefox</a></li>\
+                    <li><a href="https://www.apple.com/safari/" target="_blank">Apple Safari</a></li>\
+                </ul>\
+                <p>Please use a different browser for an optimal experience.</p>\
+            </div>'
         );
     }
 }
