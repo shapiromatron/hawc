147e50a3666f0244febd4d8b56f63dc6cceedd87
unknown <rabstejnek@gmail.com>
2020-10-22 14:39:44 -0400

    Moved user_can_edit_object to permissions module

--- hawc/apps/common/api/__init__.py
+++ hawc/apps/common/api/__init__.py
@@ -1,30 +1,7 @@
 # flake8: noqa
 
-from django.db import models
-from rest_framework import exceptions
-
 from .filters import *
 from .mixins import *
 from .permissions import *
 from .throttling import *
 from .viewsets import *
-
-
-def user_can_edit_object(
-    instance: models.Model, user: models.Model, raise_exception: bool = False
-) -> bool:
-    """Permissions check to ensure that user can edit assessment objects
-
-    Args:
-        instance (models.Model): The instance to check
-        user (models.Model): The user instance
-        raise_exception (bool, optional): Throw an Exception; defaults to False.
-
-    Raises:
-        exceptions.PermissionDenied: If raise_exc is True and user doesn't have permission
-
-    """
-    can_edit = instance.get_assessment().user_can_edit_object(user)
-    if raise_exception and not can_edit:
-        raise exceptions.PermissionDenied("Invalid permission to edit assessment.")
-    return can_edit
--- hawc/apps/common/api/permissions.py
+++ hawc/apps/common/api/permissions.py
@@ -1,6 +1,27 @@
-from rest_framework import permissions
+from django.db import models
+from rest_framework import exceptions, permissions
 
 from ...assessment.api import get_assessment_from_query
+
+
+def user_can_edit_object(
+    instance: models.Model, user: models.Model, raise_exception: bool = False
+) -> bool:
+    """Permissions check to ensure that user can edit assessment objects
+
+    Args:
+        instance (models.Model): The instance to check
+        user (models.Model): The user instance
+        raise_exception (bool, optional): Throw an Exception; defaults to False.
+
+    Raises:
+        exceptions.PermissionDenied: If raise_exc is True and user doesn't have permission
+
+    """
+    can_edit = instance.get_assessment().user_can_edit_object(user)
+    if raise_exception and not can_edit:
+        raise exceptions.PermissionDenied("Invalid permission to edit assessment.")
+    return can_edit
 
 
 class CleanupFieldsPermissions(permissions.BasePermission):
