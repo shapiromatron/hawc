2dc260c1f2329fc80ac7c44283797bc2fb0d5e9b
unknown <rabstejnek@gmail.com>
2020-10-22 12:47:04 -0400

    Refactored common api

---
+++ hawc/apps/common/api/permissions.py
@@ -0,0 +1,20 @@
+from rest_framework import permissions
+
+from ...assessment.api import get_assessment_from_query
+
+
+class CleanupFieldsPermissions(permissions.BasePermission):
+    """
+    Custom permissions for bulk-cleanup view. No object-level permissions. Here we check that
+    the user has permission to edit content for this assessment, but not necessarily that they
+    can edit the specific ids selected.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # no object-specific permissions
+        return False
+
+    def has_permission(self, request, view):
+        # must be team-member or higher to bulk-edit
+        view.assessment = get_assessment_from_query(request)
+        return view.assessment.user_can_edit_object(request.user)
