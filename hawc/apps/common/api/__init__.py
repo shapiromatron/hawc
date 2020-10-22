bbf04ad9c10843f0dea8f5ac836a3a7195e0117f
unknown <rabstejnek@gmail.com>
2020-10-22 14:50:05 -0400

    Made flake8 ignores error code specific

--- hawc/apps/common/api/__init__.py
+++ hawc/apps/common/api/__init__.py
@@ -1,7 +1,5 @@
-# flake8: noqa
-
-from .filters import *
-from .mixins import *
-from .permissions import *
-from .throttling import *
-from .viewsets import *
+from .filters import *  # noqa: F401,F403
+from .mixins import *  # noqa: F401,F403
+from .permissions import *  # noqa: F401,F403
+from .throttling import *  # noqa: F401,F403
+from .viewsets import *  # noqa: F401,F403
