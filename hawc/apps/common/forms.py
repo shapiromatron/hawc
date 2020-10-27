54e557aa12737b13101ac408cf7b802eac701e61
unknown <rabstejnek@gmail.com>
2020-10-27 09:45:56 -0400

    Fixed up forms

--- hawc/apps/common/forms.py
+++ hawc/apps/common/forms.py
@@ -11,7 +11,6 @@ from . import validators
 
 class BaseFormHelper(cf.FormHelper):
 
-    form_class = "form-horizontal"
     error_text_inline = False
 
     def __init__(self, form=None, **kwargs):
@@ -73,7 +72,7 @@ class BaseFormHelper(cf.FormHelper):
         for i, v in enumerate(wrapperClasses):
             self[first + i].wrap(cfl.Field, wrapper_class=v)
         self[first : first + numFields].wrap_together(
-            cfl.Div, css_class="row", id=f"fluid_id_{firstField}_{numFields}"
+            cfl.Div, css_class="form-row", id=f"fluid_id_{firstField}_{numFields}"
         )
 
     def add_td(self, firstField, numFields):
