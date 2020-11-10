d7596260599c27f7918a6754d8305bd7ac124314
unknown <rabstejnek@gmail.com>
2020-11-10 00:42:38 -0500

    Added permission test for API endpoint

--- tests/hawc/apps/assessment/test_assessment_api.py
+++ tests/hawc/apps/assessment/test_assessment_api.py
@@ -282,3 +282,28 @@ class TestDssToxViewset:
         resp = client.get(url)
         assert resp.status_code == 200
         assert resp.json()["dtxsid"] == dtxsid
+
+
+@pytest.mark.django_db
+class TestAssessment:
+    def test_rob_bulk_copy(self, db_keys):
+        client = APIClient()
+
+        # only admin can perform this action
+        url = reverse("assessment:api:assessment-rob-bulk-copy")
+        data = {
+            "src_assessment_id": 1,
+            "dst_assessment_id": 2,
+            "src_dst_study_ids": [(1, 5)],
+            "src_dst_metric_ids": [(1, 14), (2, 15)],
+            "copy_mode": 1,
+            "author_mode": 1,
+        }
+
+        assert client.login(username="pm@pm.com", password="pw") is True
+        resp = client.post(url, data, format="json")
+        assert resp.status_code == 403
+
+        assert client.login(username="sudo@sudo.com", password="pw") is True
+        resp = client.post(url, data, format="json")
+        assert resp.status_code == 200
