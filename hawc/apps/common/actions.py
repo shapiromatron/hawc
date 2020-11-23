7573ecde40a59219100a6e37c1b666ff7a8e513c
Daniel Rabstejnek <rabstejnek@gmail.com>
2020-11-23 14:57:22 -0500

    RoB copy (#332)
    - Allow admins to copy RoB between assessments.
    - Can either copy the entire RoB object, including draft and final reviews, or copy just a final review to an initial review. - Added to hawc client and created example notebook

---
+++ hawc/apps/common/actions.py
@@ -0,0 +1,122 @@
+import json
+from collections import defaultdict
+from typing import Any, ClassVar, Dict, List, Tuple, Type, Union
+
+import pandas as pd
+import pydantic
+from django.db import transaction
+from rest_framework.exceptions import PermissionDenied, ValidationError
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+DataModel = Type[pydantic.BaseModel]
+
+
+class BaseApiAction:
+    """
+    An API action that's not tied to a database model schema. Mapping to a drf serializer is not
+    ideal, so instead we bind to a pydantic data model.
+
+    The main entrypoint is `ApiActionRequest.handle_request`; which takes a request and returns
+    a response; however a standard class initialization can be used with data in the form of a
+    python dictionary.
+    """
+
+    input_model: ClassVar[DataModel]  # should be defined
+
+    def __init__(self, data: Dict):
+        self.data = data
+        self.errors: Dict[str, List] = defaultdict(list)
+        self.inputs = None
+
+    @classmethod
+    def format_request_data(cls, request: Request) -> Dict:
+        """Convert a request into a dictionary of data for the action request
+
+        Args:
+            request (Request): The incoming request
+
+        Returns:
+            Dict: A valid dictionary ready for the action
+        """
+        return request.data
+
+    @classmethod
+    def handle_request(cls, request: Request, atomic: bool = False) -> Response:
+        """Handle the API request and response; throws standard django rest framework lifecycle
+        errors if the response is invalid.
+
+        Args:
+            request (Request): The incoming request
+            atomic (bool, optional): Should `execute` be wrapped in a transaction. Defaults to False.
+
+        Raises:
+            drf.ValidationError: If request data is invalid
+            drf.PermissionDenied: If permission is denied
+
+        Returns:
+            Response: A drf.Response
+        """
+        instance = cls(data=cls.format_request_data(request))
+
+        instance.validate(raise_exception=True)
+
+        # check permissions
+        has_permission, reason = instance.has_permission()
+        if not has_permission:
+            raise PermissionDenied(reason)
+
+        # perform action
+        if atomic:
+            with transaction.atomic():
+                response = instance.evaluate()
+        else:
+            response = instance.evaluate()
+
+        # return response
+        return Response(response)
+
+    def validate(self, raise_exception: bool = False):
+        # parse primitive data types
+        try:
+            self.inputs = self.input_model.parse_obj(self.data)
+        except pydantic.ValidationError as err:
+            self.errors = json.loads(err.json())
+
+        # validate business logic
+        if len(self.errors) == 0:
+            self.validate_business_logic()
+
+        if raise_exception and self.errors:
+            raise ValidationError(self.errors)
+
+    @property
+    def is_valid(self):
+        return self.inputs is not None and len(self.errors) == 0
+
+    def validate_business_logic(self):
+        """Validate input data beyond the primitive data types.
+
+        This method also frequently sets additional class attributes.
+
+        An example check:
+
+        if self.inputs.swallow == "brazil":
+            self.errors["swallow"].append("Only african or european are allowed")
+        """
+        pass
+
+    def has_permission(self) -> Tuple[bool, str]:
+        """Any additional permission checks after business logic has been validated.
+
+        Returns:
+            Tuple[bool, str]: has_permission, reason (if permission denied)
+        """
+        return True, ""
+
+    def evaluate(self) -> Union[Dict[str, Any], pd.DataFrame]:
+        """
+        Perform the desired action of the request action. Returns a response type compatible
+        with the desired action
+        """
+        return {}
---
+++ tests/hawc/apps/riskofbias/test_riskofbias_actions_bulk_copy.py
@@ -0,0 +1,133 @@
+import json
+from typing import Dict
+
+import pytest
+
+from hawc.apps.myuser.models import HAWCUser
+from hawc.apps.riskofbias.actions.rob_clone import BulkCopyAuthor, BulkCopyMode, BulkRobCopyAction
+from hawc.apps.riskofbias.models import RiskOfBias, RiskOfBiasScore
+
+
+@pytest.mark.django_db
+class TestBulkRobCopyAction:
+    def _valid_arguments(self) -> Dict:
+        return {
+            "src_assessment_id": 1,
+            "dst_assessment_id": 2,
+            "dst_author_id": HAWCUser.objects.get(email="team@team.com").id,
+            "src_dst_study_ids": [(1, 5)],
+            "src_dst_metric_ids": [(1, 14), (2, 15)],
+            "copy_mode": BulkCopyMode.ALL_ACTIVE,
+            "author_mode": BulkCopyAuthor.PRESERVE_ORIGINAL,
+        }
+
+    def test_validation(self):
+        invalid_datasets = [
+            # data structure
+            (dict(src_dst_study_ids="not a list"), "value is not a valid list"),
+            (dict(src_dst_study_ids=[(-1,)]), "wrong tuple length 1, expected 2"),
+            (dict(src_dst_study_ids=[(-1, -1, -1)]), "wrong tuple length 3, expected 2"),
+            # business logic
+            (dict(dst_assessment_id=1), "Source and destination assessments must be different"),
+            (dict(src_dst_study_ids=[(-1, 5)]), "Invalid source study"),
+            (dict(src_dst_study_ids=[(1, -1)]), "Invalid destination study"),
+            (dict(src_dst_metric_ids=[(2, 14), (2, 15)]), "Source metric ids must be unique"),
+            (dict(src_dst_metric_ids=[(14, 14)]), "Invalid source metric"),
+            (dict(src_dst_metric_ids=[(1, 14)]), "Need mapping"),
+            (dict(src_dst_metric_ids=[(1, 1)]), "Invalid destination metric"),
+            (
+                dict(dst_author_id=None, author_mode=BulkCopyAuthor.OVERWRITE),
+                "dst_author_id required when author_mode overwrite.",
+            ),
+            (dict(dst_author_id=-1, author_mode=BulkCopyAuthor.OVERWRITE), "Author not found.",),
+        ]
+        for overrides, error_msg in invalid_datasets:
+            data = self._valid_arguments()
+            data.update(overrides)
+            action = BulkRobCopyAction(data)
+            action.validate()
+            assert action.is_valid is False
+            assert error_msg in json.dumps(action.errors)
+
+    def test_copy_all(self):
+        data = self._valid_arguments()
+        data.update(copy_mode=BulkCopyMode.ALL_ACTIVE, author_mod=BulkCopyAuthor.PRESERVE_ORIGINAL)
+
+        action = BulkRobCopyAction(data)
+        action.validate()
+        assert action.is_valid is True
+        res = action.evaluate()
+        mapping = res["mapping"]
+
+        # all scores from src studies should be in mapping
+        src_study_ids = mapping["study"].keys()
+        src_score_ids = mapping["score"].keys()
+        assert (
+            len(src_score_ids)
+            == RiskOfBiasScore.objects.filter(
+                riskofbias__active=True, riskofbias__study__in=src_study_ids
+            ).count()
+        )
+
+        # all riskofbiases from src studies should be in mapping
+        src_riskofbias_ids = mapping["riskofbias"].keys()
+        assert (
+            len(src_riskofbias_ids)
+            == RiskOfBias.objects.filter(active=True, study__in=src_study_ids).count()
+        )
+
+        # all src riskofbiases should be copied
+        riskofbias_ids = [item for pair in mapping["riskofbias"].items() for item in pair]
+        riskofbias_id_to_instance = RiskOfBias.objects.in_bulk(riskofbias_ids)
+        for src_riskofbias_id, dst_riskofbias_id in mapping["riskofbias"].items():
+            src_riskofbias = riskofbias_id_to_instance[src_riskofbias_id]
+            dst_riskofbias = riskofbias_id_to_instance[dst_riskofbias_id]
+            # check study changed but original author is preserved
+            assert dst_riskofbias.study_id == mapping["study"][src_riskofbias.study_id]
+            assert dst_riskofbias.author_id == src_riskofbias.author_id
+
+        # all src scores should be copied
+        score_ids = [item for pair in mapping["score"].items() for item in pair]
+        score_id_to_instance = RiskOfBiasScore.objects.in_bulk(score_ids)
+        for src_score_id, dst_score_id in mapping["score"].items():
+            src_score = score_id_to_instance[src_score_id]
+            dst_score = score_id_to_instance[dst_score_id]
+            assert dst_score.score == src_score.score
+            assert dst_score.metric_id == mapping["metric"][src_score.metric_id]
+            assert dst_score.riskofbias_id == mapping["riskofbias"][src_score.riskofbias_id]
+
+    def test_copy_final_to_initial(self):
+        data = self._valid_arguments()
+        data.update(copy_mode=BulkCopyMode.FINAL_TO_INITIAL, author_mode=BulkCopyAuthor.OVERWRITE)
+
+        action = BulkRobCopyAction(data)
+        action.validate()
+        assert action.is_valid is True
+        res = action.evaluate()
+        mapping = res["mapping"]
+
+        # all src riskofbiases should be copied
+        rob_ids = [item for pair in mapping["riskofbias"].items() for item in pair]  # flatten
+        rob_map = RiskOfBias.objects.in_bulk(rob_ids)
+        for src_rob_id, dst_rob_id in mapping["riskofbias"].items():
+            src_riskofbias = rob_map[src_rob_id]
+            dst_riskofbias = rob_map[dst_rob_id]
+            assert dst_riskofbias.study_id == mapping["study"][src_riskofbias.study_id]
+
+            # ensure final is changed
+            assert src_riskofbias.final is True and dst_riskofbias.active is True
+            assert dst_riskofbias.final is False and dst_riskofbias.active is True
+
+            # ensure author is overridden
+            assert src_riskofbias.author_id != dst_riskofbias.author_id
+            assert dst_riskofbias.author_id == data["dst_author_id"]
+
+        # all src scores should be copied
+        score_ids = [item for pair in mapping["score"].items() for item in pair]
+        score_map = RiskOfBiasScore.objects.in_bulk(score_ids)
+        for src_score_id, dst_score_id in mapping["score"].items():
+            src_score = score_map[src_score_id]
+            dst_score = score_map[dst_score_id]
+            assert dst_score.score == src_score.score
+            assert dst_score.metric_id == mapping["metric"][src_score.metric_id]
+            assert dst_score.riskofbias_id == mapping["riskofbias"][src_score.riskofbias_id]
