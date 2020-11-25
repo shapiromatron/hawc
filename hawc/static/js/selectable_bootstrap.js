9ff7c18b9fd2fed3d5943951d83f75deb8f39c9e
unknown <rabstejnek@gmail.com>
2020-11-25 15:11:10 -0500

    Fixed selectable dropdown button

--- hawc/static/js/selectable_bootstrap.js
+++ hawc/static/js/selectable_bootstrap.js
@@ -2,6 +2,22 @@ $.ui.djselectable.prototype.options.defaultClasses = {
   text: "form-control mb-2",
   combobox: "form-control mb-2",
 };
+
+$.ui.djselectable.prototype._comboButtonTemplate = function (input) {
+  // Wrap input with input-group
+  $(input)
+    .wrap('<div class="input-group-append" />')
+    .wrap('<div class="input-group" />');
+  // Return button
+  return $("<button>").addClass("btn btn-primary btn-sm dropdown-toggle mb-2");
+};
+
+$.ui.djselectable.prototype._removeButtonTemplate = function (item) {
+  var icon = $("<i>").addClass("icon-remove-sign");
+  // Return button link with the chosen icon
+  return $("<a>").append(icon).addClass("btn btn-small pull-right");
+};
+
 (function () {
   let _create = $.ui.menu.prototype._create;
   $.ui.menu.prototype._create = function () {
