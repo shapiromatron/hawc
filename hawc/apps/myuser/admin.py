c0906f8e4abeee3d2dfc894bed591f424f3bd61b
Andy Shapiro <shapiromatron@gmail.com>
2021-03-10 16:42:43 -0500

    add diagnostic test email operation

--- hawc/apps/myuser/admin.py
+++ hawc/apps/myuser/admin.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.contrib import admin, messages
 from django.core.cache import cache
+from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
@@ -64,6 +66,19 @@ class HAWCUserAdmin(admin.ModelAdmin):
         message = "Cache test executed successfully"
         modeladmin.message_user(request, message)
 
+    def diagnostic_email(modeladmin, request, queryset):
+        to_email = request.user.email
+        send_mail(
+            "Test email",
+            "Test message",
+            settings.DEFAULT_FROM_EMAIL,
+            [to_email],
+            fail_silently=False,
+        )
+
+        message = f"Attempted to send email to {to_email}"
+        modeladmin.message_user(request, message)
+
     def save_model(self, request, obj, form, change):
         form.save(commit=True)
 
@@ -72,6 +87,7 @@ class HAWCUserAdmin(admin.ModelAdmin):
     throw_500.short_description = "Intentionally throw a server error (500)"
     diagnostic_celery_task.short_description = "Diagnostic celery task test"
     diagnostic_cache.short_description = "Diagnostic cache test"
+    diagnostic_email.short_description = "Diagnostic email test"
 
     actions = (
         send_welcome_emails,
@@ -79,4 +95,5 @@ class HAWCUserAdmin(admin.ModelAdmin):
         throw_500,
         diagnostic_celery_task,
         diagnostic_cache,
+        diagnostic_email,
     )
