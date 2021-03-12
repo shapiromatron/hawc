e8a521fde3ee563e3023ef4ce9c2a9217f5314df
ricerb <58782045+ricerb@users.noreply.github.com>
2021-03-12 14:19:59 -0500

    preliminary eco model draft (#355)
    * Add eco models
    
    * Make field choices easier to match to test data
    
    * Modify TBD field choices for test data display
    
    * small model tweaks - change order of fields
    
    * cleanup
    
    Co-authored-by: Andy Shapiro <shapiromatron@gmail.com>

---
+++ hawc/apps/eco/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from . import models
+
+admin.site.register(models.Reference)
+admin.site.register(models.Metadata)
+admin.site.register(models.Cause)
+admin.site.register(models.Effect)
+admin.site.register(models.Quantitative)
+admin.site.register(models.Country)
+admin.site.register(models.State)
+admin.site.register(models.Ecoregion)
+admin.site.register(models.Climate)
