b2cd0baebbf3ab9d1d0917261588d507e2285cab
Andy Shapiro <shapiromatron@gmail.com>
2021-03-14 22:19:31 -0400

    use new bulk_update in migration

--- hawc/apps/summary/migrations/0029_tagtree_size.py
+++ hawc/apps/summary/migrations/0029_tagtree_size.py
@@ -6,32 +6,44 @@ literature_tagtree = 4
 
 
 def add_tagtree_size(apps, schema_editor):
-    for visual in apps.get_model("summary", "Visual").objects.filter(
-        visual_type=literature_tagtree
-    ):
+    Visual = apps.get_model("summary", "Visual")
+    updates = []
+    failures = []
+
+    for visual in Visual.objects.filter(visual_type=literature_tagtree):
         try:
             settings = json.loads(visual.settings)
         except json.JSONDecodeError:
+            failures.append(visual.id)
             continue
 
         settings.update(width=1280, height=800)
         visual.settings = json.dumps(settings)
-        visual.save()
+        updates.append(visual)
+
+    Visual.objects.bulk_update(updates, ["settings"])
+    print(f"{len(updates)} objects updated; {len(failures)} not updated: {failures}")
 
 
 def remove_tagtree_size(apps, schema_editor):
-    for visual in apps.get_model("summary", "Visual").objects.filter(
-        visual_type=literature_tagtree
-    ):
+    Visual = apps.get_model("summary", "Visual")
+    updates = []
+    failures = []
+
+    for visual in Visual.objects.filter(visual_type=literature_tagtree):
         try:
             settings = json.loads(visual.settings)
         except json.JSONDecodeError:
+            failures.append(visual.id)
             continue
 
         settings.pop("width")
         settings.pop("height")
         visual.settings = json.dumps(settings)
-        visual.save()
+        updates.append(visual)
+
+    Visual.objects.bulk_update(updates, ["settings"])
+    print(f"{len(updates)} objects updated; {len(failures)} not updated: {failures}")
 
 
 class Migration(migrations.Migration):
