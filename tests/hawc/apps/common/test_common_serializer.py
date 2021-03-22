aea722fa659d063f878bb903cefdfc02106e1d72
Tom Feiler <tfeiler@gmail.com>
2021-03-22 15:11:55 -0500

    add FlexibleChoiceField tests to common serializer tests

--- tests/hawc/apps/common/test_common_serializer.py
+++ tests/hawc/apps/common/test_common_serializer.py
@@ -1,7 +1,11 @@
 import pytest
 from rest_framework.exceptions import ValidationError
 
-from hawc.apps.common.serializers import get_matching_instance, get_matching_instances
+from hawc.apps.common.serializers import (
+    FlexibleChoiceField,
+    get_matching_instance,
+    get_matching_instances,
+)
 from hawc.apps.study.models import Study
 
 
@@ -47,3 +51,42 @@ def test_get_matching_instances(db_keys):
     ]:
         with pytest.raises(ValidationError):
             get_matching_instances(Study, data, "study_id")
+
+
+@pytest.mark.django_db
+def test_flexible_choice_field(db_keys):
+    # working = Study.objects.get(id=db_keys.study_working)
+    SAMPLE_NUMERIC_CHOICES = ((0, "example"), (1, "test"))
+
+    SAMPLE_TEXT_CHOICES = (("name", "nom de plume"), ("job", "occupation"))
+
+    numeric_choice = FlexibleChoiceField(choices=SAMPLE_NUMERIC_CHOICES)
+    text_choice = FlexibleChoiceField(choices=SAMPLE_TEXT_CHOICES)
+
+    # either the key or a case insensitive version will resolve to the same internal value
+    for valid_input in [1, "test", "TeSt"]:
+        assert numeric_choice.to_internal_value(valid_input) == 1
+
+    # works for text or numeric keys
+    for valid_input in ["job", "occupation", "OcCuPaTiOn"]:
+        assert text_choice.to_internal_value(valid_input) == "job"
+
+    for invalid_input in [99, "bad input"]:
+        try:
+            numeric_choice.to_internal_value(invalid_input)
+            assert False
+        except ValidationError:
+            # this is correct behavior
+            pass
+
+    # should convert raw values to readable ones
+    assert numeric_choice.to_representation(0) == "example"
+    assert numeric_choice.to_representation(1) == "test"
+
+    # should throw KeyError if given a bad value to convert
+    try:
+        numeric_choice.to_representation(2)
+        assert False
+    except KeyError:
+        # this is correct behavior
+        pass
