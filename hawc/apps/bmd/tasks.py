cada9513ae9f08824baeec4db202d548c03247ea
Andy Shapiro <shapiromatron@gmail.com>
2021-03-23 17:08:19 -0400

    bmds2 healthcheck (#406)
    * move diagnostic tasks to common path
    
    * update requests; ignore ssl; add diagnostic test and cronjob for simple bmds 2 model execution
    
    * reorganize; add test
    
    * few updates; add back ssl checking
    
    * remove unused exception definition

--- hawc/apps/bmd/tasks.py
+++ hawc/apps/bmd/tasks.py
@@ -2,6 +2,8 @@ from celery import shared_task
 from celery.utils.log import get_task_logger
 from django.apps import apps
 
+from .diagnostics import bmds2_service_healthcheck
+
 logger = get_task_logger(__name__)
 
 
@@ -10,3 +12,12 @@ def execute(session_id):
     logger.info(f"BMD execution -> {session_id}")
     session = apps.get_model("bmd", "Session").objects.get(id=session_id)
     session.execute()
+
+
+@shared_task
+def bmds2_healthcheck():
+    """
+    Run a simple dataset just to make sure the external webservice is operational.
+    """
+    status = bmds2_service_healthcheck()
+    logger.info(f"BMDS healthcheck successful: {status}")
