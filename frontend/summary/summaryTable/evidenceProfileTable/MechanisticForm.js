930097d635d748ae879bf09024c6eead29280604
Andy Shapiro <shapiromatron@gmail.com>
2021-03-23 17:59:07 -0400

    EPT revisions (#403)
    * change ? to info circle
    
    * Added hide_content to model, and to frontend form/render
    
    * Added no_content_text to form, html render, and report
    
    * Updated word report to work with hide_content
    
    * Propagate all model cleaning errors to frontend
    
    * Made requested PR change
    
    * help-text updates
    
    Co-authored-by: unknown <rabstejnek@gmail.com>

--- frontend/summary/summaryTable/evidenceProfileTable/MechanisticForm.js
+++ frontend/summary/summaryTable/evidenceProfileTable/MechanisticForm.js
@@ -8,83 +8,115 @@ import QuillTextInput from "shared/components/QuillTextInput";
 import CheckboxInput from "shared/components/CheckboxInput";
 import {ActionsTh, MoveRowTd} from "shared/components/EditableRowData";
 
-import {JUDGEMENT_HELP_TEXT} from "./common";
+import {JUDGEMENT_HELP_TEXT, HIDE_CONTENT_HELP_TEXT, NO_CONTENT_HELP_TEXT} from "./common";
 
 const MechanisticForm = observer(props => {
         const {store} = props,
             {mechanistic} = props.store.settings;
         return (
-            <div>
-                <TextInput
-                    name="title"
-                    label="Subheading"
-                    value={mechanistic.title}
-                    onChange={e => store.updateValue("mechanistic.title", e.target.value)}
-                    required
-                />
-                <TextInput
-                    name="col_header"
-                    label="Evidence header"
-                    value={mechanistic.col_header_1}
-                    onChange={e => store.updateValue("mechanistic.col_header_1", e.target.value)}
-                    required
-                />
+            <>
                 <CheckboxInput
-                    label="Merge judgement?"
-                    checked={mechanistic.merge_judgement}
-                    onChange={e =>
-                        store.updateValue("mechanistic.merge_judgement", e.target.checked)
-                    }
-                    helpText={JUDGEMENT_HELP_TEXT}
+                    label="Hide section?"
+                    checked={mechanistic.hide_content}
+                    onChange={e => {
+                        store.updateValue("mechanistic.hide_content", e.target.checked);
+                    }}
+                    helpText={HIDE_CONTENT_HELP_TEXT}
                     required
                 />
-                <table className="table table-sm table-bordered">
-                    <colgroup>
-                        <col width="30%" />
-                        <col width="30%" />
-                        <col width="30%" />
-                        <col width="10%" />
-                    </colgroup>
-                    <thead>
-                        <tr>
-                            <th>
-                                {mechanistic.col_header_1}
-                                <HelpTextPopup
-                                    title="Help-text"
-                                    content="Briefly describe the evidence or information analyzed, which may be subdivided as described in the “adding rows” instruction. Generally, cite or link evidence synthesis (e.g., for references; for detailed analysis). Does not have to be chemical-specific (e.g., read-across)."
-                                />
-                            </th>
-                            <th>
-                                Summary of key findings and interpretation
-                                <HelpTextPopup
-                                    title="Help-text"
-                                    content="Summary of expert interpretation of the body of evidence or information and supporting rationale. "
-                                />
-                            </th>
-                            <th>
-                                Judgment(s) and rationale
-                                <HelpTextPopup
-                                    title="Help-text"
-                                    content="Summary of findings across the body of evidence (may focus on or emphasize highly informative designs or findings), including key sources of uncertainty or identified limitations of the study designs tested (e.g., regarding the biological event or pathway being examined)."
-                                />
-                            </th>
-                            <ActionsTh onClickNew={() => store.createMechanisticRow()} />
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {mechanistic.rows.map((row, index) => {
-                            return (
-                                <MechanisticFormRow
-                                    store={store}
-                                    row={row}
-                                    index={index}
-                                    key={index}
-                                />
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </div>
+                <div className={mechanistic.hide_content ? "hidden" : null}>
+                    <TextInput
+                        name="title"
+                        label="Section subheading"
+                        value={mechanistic.title}
+                        onChange={e => store.updateValue("mechanistic.title", e.target.value)}
+                        required
+                    />
+                    <TextInput
+                        name="col_header"
+                        label="Evidence header"
+                        value={mechanistic.col_header_1}
+                        onChange={e =>
+                            store.updateValue("mechanistic.col_header_1", e.target.value)
+                        }
+                        required
+                    />
+                    <CheckboxInput
+                        label="Merge judgement?"
+                        checked={mechanistic.merge_judgement}
+                        onChange={e =>
+                            store.updateValue("mechanistic.merge_judgement", e.target.checked)
+                        }
+                        helpText={JUDGEMENT_HELP_TEXT}
+                        required
+                    />
+                    <table className="table table-sm table-bordered">
+                        <colgroup>
+                            <col width="30%" />
+                            <col width="30%" />
+                            <col width="30%" />
+                            <col width="10%" />
+                        </colgroup>
+                        <thead>
+                            <tr>
+                                <th>
+                                    {mechanistic.col_header_1}
+                                    <HelpTextPopup
+                                        title="Help-text"
+                                        content="Briefly describe the evidence or information analyzed, which may be subdivided as described in the “adding rows” instruction. Generally, cite or link evidence synthesis (e.g., for references; for detailed analysis). Does not have to be chemical-specific (e.g., read-across)."
+                                    />
+                                </th>
+                                <th>
+                                    Summary of key findings and interpretation
+                                    <HelpTextPopup
+                                        title="Help-text"
+                                        content="Summary of expert interpretation of the body of evidence or information and supporting rationale. "
+                                    />
+                                </th>
+                                <th>
+                                    Judgment(s) and rationale
+                                    <HelpTextPopup
+                                        title="Help-text"
+                                        content="Summary of findings across the body of evidence (may focus on or emphasize highly informative designs or findings), including key sources of uncertainty or identified limitations of the study designs tested (e.g., regarding the biological event or pathway being examined)."
+                                    />
+                                </th>
+                                <ActionsTh onClickNew={() => store.createMechanisticRow()} />
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {mechanistic.rows.length ? (
+                                mechanistic.rows.map((row, index) => {
+                                    return (
+                                        <MechanisticFormRow
+                                            store={store}
+                                            row={row}
+                                            index={index}
+                                            key={index}
+                                        />
+                                    );
+                                })
+                            ) : (
+                                <tr>
+                                    <td colSpan={4}>
+                                        <TextInput
+                                            name="no_content_text"
+                                            value={mechanistic.no_content_text}
+                                            onChange={e =>
+                                                store.updateValue(
+                                                    "mechanistic.no_content_text",
+                                                    e.target.value
+                                                )
+                                            }
+                                            helpText={NO_CONTENT_HELP_TEXT}
+                                            required
+                                        />
+                                    </td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            </>
         );
     }),
     MechanisticFormRow = observer(props => {
