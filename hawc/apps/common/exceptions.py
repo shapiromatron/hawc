9c357c20d568acb423f10fa8ec75d7e2682f2f8e
Andy Shapiro <shapiromatron@gmail.com>
2021-03-30 22:48:50 -0400

    lint

--- hawc/apps/common/exceptions.py
+++ hawc/apps/common/exceptions.py
@@ -2,4 +2,5 @@ class ClassConfigurationException(Exception):
     """
     Raised when a programmer has misconfigured something; should not occur in normal application lifecycle.
     """
+
     pass
