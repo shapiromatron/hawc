fa574eb4bdc51bdc5ae508747188647f05caf8af
Daniel Rabstejnek <rabstejnek@gmail.com>
2021-04-15 10:42:45 -0400

    Documented new client methods

--- client/hawc_client/__init__.py
+++ client/hawc_client/__init__.py
@@ -781,14 +781,38 @@ class VocabClient(BaseClient):
     """
 
     def bulk_create(self, terms: List[Dict]) -> List[Dict]:
+        """
+        Bulk creates a list of terms.
+
+        Args:
+            terms (List[Dict]): List of serialized terms.
+
+        Returns:
+            List[Dict]: List of created, serialized terms.
+        """
         url = f"{self.session.root_url}/vocab/api/term/bulk-create/"
         return self.session.post(url, terms).json()
 
     def bulk_update(self, terms: List[Dict]) -> List[Dict]:
+        """
+        Bulk updates a list of terms.
+
+        Args:
+            terms (List[Dict]): List of serialized terms.
+
+        Returns:
+            List[Dict]: List of updated, serialized terms.
+        """
         url = f"{self.session.root_url}/vocab/api/term/bulk-update/"
         return self.session.patch(url, terms).json()
 
-    def uids(self):
+    def uids(self) -> List[Tuple[int, int]]:
+        """
+        Get all term ids and uids.
+
+        Returns:
+            List[Tuple[int,int]]: List of id, uid tuples for all terms.
+        """
         url = f"{self.session.root_url}/vocab/api/term/uids/"
         return self.session.get(url).json()
 
