68959c0956dca10948cf56fcb54cd08b0f4e122f
Andy Shapiro <shapiromatron@gmail.com>
2021-05-12 16:26:01 -0400

    Add new diagnostic apis for migrating to new EHV (#439)
    * Added new field for synaptica uid
    
    * Added migration for new term field
    
    * Removed the comment model
    
    * Create BulkSerializer for bulk create/update
    
    * Made BulkSerializer more customizable
    
    * Added test for bulk create/update on term serializer
    
    * Ran formatter
    
    * Added api endpoints and tests
    
    * Updated client and api to work with synaptica import workflow
    
    * Fixed RoB override test
    
    * Fix test data
    
    * Undo unnecessary changes
    
    * Removed schema from serializer
    
    * Documented new client methods
    
    * Removed old load migration and csv, made uid required, and fixed vocab serializer to use django timezone util.
    
    * Updated terms in fixture with uid
    
    * Fixed tests
    
    * revert uid requirement on this branch
    
    * fix url
    
    * add uid to admin
    
    * use timezone aware datetime; use same update datetime for all
    
    * add api endpoint to view tabular ehv
    
    * pydantic security update
    
    * add new term-checking action
    
    * add terms to full export
    
    * convert from action to just simple function
    
    * Made requested serializer changes, fixed test
    
    * Fixed serializer errors, made requested api changes
    
    * Fixed tests
    
    * add api checks
    
    * shorten docstring
    
    Co-authored-by: unknown <rabstejnek@gmail.com>

---
+++ hawc/apps/animal/actions/term_check.py
@@ -0,0 +1,105 @@
+import pandas as pd
+
+from ..models import Endpoint
+
+
+def term_check(assessment_id: int) -> pd.DataFrame:
+    """
+    Generate a report for potential issues identified in a given assessment and
+    the current EHV vocabulary.
+    """
+    fields = ["system", "organ", "effect", "effect_subtype", "name"]
+    related = [f"{field}_term" for field in fields]
+    data = []
+    for endpoint in Endpoint.objects.filter(assessment_id=assessment_id).select_related(*related):
+        row1 = [endpoint.assessment_id, endpoint.id, endpoint.get_absolute_url()]
+        for idx, field in enumerate(fields):
+            text = getattr(endpoint, field)
+            term = getattr(endpoint, f"{field}_term")
+
+            # only continue checks if a term exists
+            if term is None:
+                continue
+
+            # Check if term is deprecated
+            if term.deprecated_on is not None:
+                row2 = list(row1)
+                row2.extend(
+                    [
+                        field,
+                        text,
+                        term.name,
+                        term.id,
+                        "Term is deprecated",
+                        f'Term "{term.name}" ({term.id}) is deprecated',
+                    ]
+                )
+                data.append(row2)
+
+            # check if endpoint free text matches term text
+            if text != term.name:
+                row2 = list(row1)
+                row2.extend(
+                    [
+                        field,
+                        text,
+                        term.name,
+                        term.id,
+                        "Endpoint free text does not match Term text",
+                        f'{field} text "{text}" != Term "{term.name}" ({term.id})',
+                    ]
+                )
+                data.append(row2)
+
+            if idx > 0:
+                parent_term = getattr(endpoint, f"{fields[idx-1]}_term")
+
+                # check if a child term exists, but a parent does not
+                if parent_term is None:
+                    row2 = list(row1)
+                    row2.extend(
+                        [
+                            field,
+                            text,
+                            term.name,
+                            term.id,
+                            "Parent term not specified",
+                            f'"{field}" term exists but "{fields[idx-1]}" term not set - use {term.parent.name} ({term.parent.id})?',
+                        ]
+                    )
+                    data.append(row2)
+
+                # check if the parent term in the vocabulary matches the term on the endpoint
+                if parent_term and parent_term.id != term.parent_id:
+                    row2 = list(row1)
+                    row2.extend(
+                        [
+                            field,
+                            text,
+                            term.name,
+                            term.id,
+                            "Parent term on endpoint does not match parent term in vocabulary",
+                            f"The {field} parent vocabulary term is {term.parent.name} ({term.parent.id}); this endpoint specified {parent_term.name} ({parent_term.id})",
+                        ]
+                    )
+                    data.append(row2)
+
+    df = (
+        pd.DataFrame(
+            columns=[
+                "assessment_id",
+                "endpoint_id",
+                "endpoint_url",
+                "term_type",
+                "endpoint_term_free_text",
+                "vocabulary_term_text",
+                "vocabulary_term_id",
+                "issue_class",
+                "issue_description",
+            ],
+            data=data,
+        )
+        .sort_values(["assessment_id", "endpoint_id"])
+        .reset_index(drop=True)
+    )
+    return df
