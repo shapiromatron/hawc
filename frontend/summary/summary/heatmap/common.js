b7698edaf32aee3704ff04653cce18cf8af36e3e
Andy Shapiro <shapiromatron@gmail.com>
2021-06-09 12:27:30 -0400

    filter out multiple falsy cases

--- frontend/summary/summary/heatmap/common.js
+++ frontend/summary/summary/heatmap/common.js
@@ -2,7 +2,8 @@ import _ from "lodash";
 import h from "shared/utils/helpers";
 
 export const getColumnValues = function(dataset, column, delimiter) {
-    // Get column values for a column in a dataset.
+    // Get column values for a column in a dataset.  Returns unique values, and removes
+    // cases where multiple falsy-like values are represented.
     return _.chain(dataset)
         .map(d => d[column])
         .map(d => (delimiter && d ? d.split(delimiter) : [d]))
@@ -10,7 +11,8 @@ export const getColumnValues = function(dataset, column, delimiter) {
         .uniq()
         .sort()
         .map(d => {
-            return {id: d, label: d || h.nullString};
+            return d ? {id: d, label: d} : {id: "", label: h.nullString};
         })
+        .uniqBy("id")
         .value();
 };
