76e990d41fc8fafc4bc282c80575aabc734dcec3
Daniel Rabstejnek <rabstejnek@gmail.com>
2021-06-21 08:17:47 -0400

    Fixed frontend linting issues

--- frontend/riskofbias/robStudyForm/Domain.js
+++ frontend/riskofbias/robStudyForm/Domain.js
@@ -1,7 +1,6 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
 import {observer, inject} from "mobx-react";
-import _ from "lodash";
 
 import Metric from "./Metric";
 
