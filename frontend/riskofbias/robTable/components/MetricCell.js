b5724be57c6b4633ec77e7f88268da688f2143a6
Andy Shapiro <shapiromatron@gmail.com>
2021-06-24 15:46:42 -0400

    lint

--- frontend/riskofbias/robTable/components/MetricCell.js
+++ frontend/riskofbias/robTable/components/MetricCell.js
@@ -1,8 +1,6 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
 
-import h from "shared/utils/helpers";
-
 import {getMultiScoreDisplaySettings} from "../../constants";
 import "./MetricCell.css";
 
