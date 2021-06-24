5c1e59b9c0b0fcb0b183e74879d3d8ec8e604fcc
Daniel Rabstejnek <rabstejnek@gmail.com>
2021-06-24 13:53:13 -0400

    Sort test jsons on write

--- tests/hawc/apps/invitro/test_invitro_api.py
+++ tests/hawc/apps/invitro/test_invitro_api.py
@@ -36,6 +36,6 @@ class TestIVAssessmentViewset:
         data = resp.json()
 
         if rewrite_data_files:
-            Path(fn).write_text(json.dumps(data, indent=2))
+            Path(fn).write_text(json.dumps(data, indent=2, sort_keys=True))
 
         assert data == json.loads(fn.read_text())
