41789bf6eb88059fd0db6acd2de11cc4ffcc5ef1
Daniel Rabstejnek <rabstejnek@gmail.com>
2021-06-30 12:24:45 -0400

    Made rob stores subclass off common store (#470)

--- frontend/riskofbias/stores/StudyRobStore.js
+++ frontend/riskofbias/stores/StudyRobStore.js
@@ -1,26 +1,11 @@
 import _ from "lodash";
 import {observable, computed, action} from "mobx";
 
-import {fetchRobSettings, fetchRobStudy, hideScore} from "../constants";
+import {fetchRobSettings, fetchRobStudy} from "../constants";
 
 class StudyRobStore {
     @observable settings = null;
     @observable study = null;
-
-    @computed get final() {
-        return _.find(this.study.riskofbiases, {
-            final: true,
-            active: true,
-        });
-    }
-
-    @computed get hasLoaded() {
-        return _.isObject(this.settings) && _.isObject(this.study);
-    }
-
-    @computed get hasFinalData() {
-        return this.hasLoaded && this.study.assessment.enable_risk_of_bias && this.final;
-    }
 
     @computed get domains() {
         return _.keyBy(this.settings.domains, domain => domain.id);
@@ -28,14 +13,6 @@ class StudyRobStore {
 
     @computed get metrics() {
         return _.keyBy(this.settings.metrics, metric => metric.id);
-    }
-
-    @computed get canShowScoreVisualization() {
-        if (this.hasFinalData) {
-            // confusing; if all hidden -> don't show; if any not hidden -> show
-            return !_.every(this.final.scores.map(d => hideScore(d.score)));
-        }
-        return true;
     }
 
     @computed get metricDomains() {
