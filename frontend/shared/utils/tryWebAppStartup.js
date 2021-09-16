b3cca04f721c0e5b5c24fbf7a9776542642ebe30
Andy Shapiro <shapiromatron@gmail.com>
2021-09-15 23:57:17 -0400

    docs

--- frontend/shared/utils/tryWebAppStartup.js
+++ frontend/shared/utils/tryWebAppStartup.js
@@ -1,17 +1,19 @@
 export default function tryWebAppStartup() {
-    /* This method runs on each page in HAWC. It checks if a #config div is present, and if so,
-     * parses the configuration and attempts to start the selected javascript frontend application.
+    /*
+    Attempts to start webpack application startup if current page has requirements.
+
+    If both a #config and #main are present, parse #config as JSON and then start selected
+    application, inserting content into the #main element.
+
+    If requirements are not present, noop.
      */
     const el = document.getElementById("main"),
         config = document.getElementById("config");
     if (el && config) {
         const content = JSON.parse(config.textContent);
-        window.app.startup(content.app, function(app) {
-            if (content.page) {
-                app[content.page](el, content.data);
-            } else {
-                app(el, content.data);
-            }
+        window.app.startup(content.app, app => {
+            const func = content.page ? app[content.page] : app;
+            func(el, content.data);
         });
     }
 }
