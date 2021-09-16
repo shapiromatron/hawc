126f24e5186a05e7d6b928c859b05d9888fdfb99
Andy Shapiro <shapiromatron@gmail.com>
2021-09-15 20:59:36 -0400

    hello vocab

--- hawc/apps/vocab/views.py
+++ hawc/apps/vocab/views.py
@@ -21,18 +21,24 @@ def build_ehv_breadcrumbs(user, name: str) -> List[Breadcrumb]:
 class EhvBrowse(TemplateView):
     template_name = "vocab/ehv_browse.html"
 
-    def _get_ehv_json(self) -> str:
+    def _get_config(self) -> str:
         # get EHV in json; use cache if possible
         key = "ehv-dataframe-json"
         data = cache.get(key)
         if data is None:
-            data = json.dumps({"data": models.Term.ehv_dataframe().to_csv(index=False)})
+            data = json.dumps(
+                dict(
+                    app="animalStartup",
+                    page="ehvBrowserStartup",
+                    data=dict(data=models.Term.ehv_dataframe().to_csv(index=False)),
+                )
+            )
             cache.set(key, data, settings.CACHE_10_MIN)
         return data
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["config"] = self._get_ehv_json()
+        context["config"] = self._get_config()
         context["breadcrumbs"] = Breadcrumb.build_crumbs(
             self.request.user, "Environmental Health Vocabulary"
         )
