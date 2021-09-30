ace03d53626340b25d05d3484cd06591eaa07c78
Andy Shapiro <shapiromatron@gmail.com>
2021-09-29 23:02:15 -0400

    add tests

--- tests/hawc/apps/lit/test_lit_constants.py
+++ tests/hawc/apps/lit/test_lit_constants.py
@@ -5,10 +5,10 @@ def test_doi_regex():
     # valid
     assert DOI_EXACT.fullmatch("10.1234/1234") is not None
     assert DOI_EXACT.fullmatch("10.1234/fff") is not None
-    assert DOI_EXACT.fullmatch("10.1234/fff-1234-._;()") is not None
+    assert DOI_EXACT.fullmatch("10.1234/fff-1234/-._;()") is not None
 
     # invalid
-    assert DOI_EXACT.fullmatch(" 10.1234/fff ") is None
+    assert DOI_EXACT.fullmatch(" 10.1234/fff") is None
     assert DOI_EXACT.fullmatch("10.1234/fff ") is None
     assert DOI_EXACT.fullmatch("10.abcd") is None
     assert DOI_EXACT.fullmatch("10.abcd/") is None
