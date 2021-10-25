d48339098cbf8a5453d06ab75c984278589b63ca
Andy Shapiro <shapiromatron@gmail.com>
2021-10-25 18:39:19 -0400

    add basic permission and api viewset tests

---
+++ tests/hawc/apps/bmd/test_bmd_api.py
@@ -0,0 +1,35 @@
+import pytest
+from rest_framework.test import APIClient
+
+from hawc.apps.assessment.models import Assessment
+from hawc.apps.bmd.models import Session
+
+
+@pytest.mark.django_db
+class TestSessionViewset:
+    def test_selected_model(self):
+        anon = APIClient()
+        client = APIClient()
+        assert client.login(username="team@hawcproject.org", password="pw") is True
+
+        # setup - make assessment editable; fetch required metadata
+        sess_id = 2
+        Assessment.objects.filter(id=sess_id).update(editable=True)
+        session = Session.objects.get(id=sess_id)
+        model = session.models.first()
+        url = session.get_selected_model_url()
+
+        payload = {"model": model.id, "notes": "example notes"}
+
+        # anon forbidden
+        resp = anon.post(url, payload, format="json")
+        assert resp.status_code == 403
+
+        # team member, with model
+        resp = client.post(url, payload, format="json")
+        assert resp.status_code == 200
+
+        # team member, no model
+        payload["model"] = None
+        resp = client.post(url, payload, format="json")
+        assert resp.status_code == 200
