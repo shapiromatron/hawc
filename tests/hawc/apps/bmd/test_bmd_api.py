a210b8b879cc4ca1faf6f71067c3dda576347ba7
Andy Shapiro <shapiromatron@gmail.com>
2021-10-25 19:04:17 -0400

    fix tests

--- tests/hawc/apps/bmd/test_bmd_api.py
+++ tests/hawc/apps/bmd/test_bmd_api.py
@@ -5,16 +5,17 @@ from hawc.apps.assessment.models import Assessment
 from hawc.apps.bmd.models import Session
 
 
-@pytest.mark.django_db
+@pytest.mark.django_db(transaction=True)
 class TestSessionViewset:
     def test_selected_model(self):
-        anon = APIClient()
+        assess_id = 2
+        Assessment.objects.filter(id=assess_id).update(editable=True)
+
+        anon_client = APIClient()
         client = APIClient()
         assert client.login(username="team@hawcproject.org", password="pw") is True
 
         # setup - make assessment editable; fetch required metadata
-        assess_id = 2
-        Assessment.objects.filter(id=assess_id).update(editable=True)
         session = Session.objects.filter(endpoint__assessment=assess_id).first()
         model = session.models.first()
         url = session.get_selected_model_url()
@@ -22,7 +23,7 @@ class TestSessionViewset:
         payload = {"model": model.id, "notes": "example notes"}
 
         # anon forbidden
-        resp = anon.post(url, payload, format="json")
+        resp = anon_client.post(url, payload, format="json")
         assert resp.status_code == 403
 
         # team member, with model
