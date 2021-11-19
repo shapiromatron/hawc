15ca14fa19973534b1b12483e2269013dd6bb14b
Andy Shapiro <shapiromatron@gmail.com>
2021-11-18 23:02:37 -0500

    remove media from debug

--- hawc/main/urls.py
+++ hawc/main/urls.py
@@ -100,7 +100,5 @@ if settings.INCLUDE_ADMIN:
 # only for DEBUG, want to use static server otherwise
 if settings.DEBUG:
     import debug_toolbar
-    from django.conf.urls.static import static
 
     urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
