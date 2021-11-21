b9630c36936d175cef54b9ca09933c8617f96970
Andy Shapiro <shapiromatron@gmail.com>
2021-11-20 23:51:29 -0500

    updates

--- frontend/bmd/common/BmdLine.js
+++ frontend/bmd/common/BmdLine.js
@@ -1,6 +1,5 @@
 import _ from "lodash";
 
-import {normalCDF, gammaCDF} from "shared/utils/math";  // eslint-disable-line
 import {buildModelFormula} from "./constants";
 
 class BmdLine {
@@ -25,36 +24,29 @@ class BmdLine {
     _getPlotData() {
         let bmd = this.model.output.BMD,
             bmdl = this.model.output.BMDL,
-            bmd_line,
-            bmdl_line,
-            bmd_y;
-
-        if (bmd && bmd > 0) {
-            x = bmd;
-            bmd_y = eval(model);
-            bmd_line = {x: bmd, y: bmd_y};
-        }
-
-        if (bmdl && bmdl > 0) {
-            bmdl_line = {x: bmdl, y: bmd_y};
-        }
+            {params, formula} = buildModelFormula(
+                this.model.name,
+                this.model.output.parameters,
+                this.model.output.fit_estimated
+            ),
+            func = xs => {
+                return _.chain(xs)
+                    .filter(d => d >= 0)
+                    .map(d => (d === 0 ? 1e-8 : d))
+                    .map(x => {
+                        return {x, y: formula(x, params)};
+                    })
+                    .value();
+            };
 
         return {
             id: this.model.id,
             dose_units_id: this.model.dose_units,
             name: this.model.name,
             stroke: this.color,
-            getData(xs) {
-                return _.chain(xs)
-                    .filter(d => d >= 0)
-                    .map(d => (d === 0 ? 1e-8 : d))
-                    .map(x => {
-                        return {x, y: eval(model)};
-                    })
-                    .value();
-            },
-            bmd_line,
-            bmdl_line,
+            getData: func,
+            bmd_line: bmd && bmd > 0 ? {x: bmd, y: formula(bmd, params)} : undefined,
+            bmdl_line: bmdl && bmdl > 0 ? {x: bmdl, y: formula(bmdl, params)} : undefined,
         };
     }
 }
--- frontend/bmd/common/constants.js
+++ frontend/bmd/common/constants.js
@@ -1,6 +1,6 @@
 import _ from "lodash";
 
-import { normalCDF, gammaCDF } from "shared/utils/math";
+import {normalCDF, gammaCDF} from "shared/utils/math";
 
 const BmrHeaderMap = {
         Extra: "%",
@@ -10,48 +10,90 @@ const BmrHeaderMap = {
         "Rel. Dev.": "% RD",
         Point: "Pt",
     },
-    // todo - get rid of this and the evals?
     formulas = {
-        Polynomial: (x, p) => p.beta_0 + (p.beta_1*x) + (p.beta_2*Math.pow(x,2)) + (p.beta_3*Math.pow(x,3)) + (p.beta_4*Math.pow(x,4)) + (p.beta_5*Math.pow(x,5)) + (p.beta_6*Math.pow(x,6)) + (p.beta_7*Math.pow(x,7)) + (p.beta_8*Math.pow(x,8)),
-        Linear: (x, p) =>p.beta_0 + (p.beta_1*x),
-        "Exponential-M2": (x, p) =>p.a * Math.exp(p.isIncreasing*p.b*x),
-        "Exponential-M3": (x, p) =>p.a * Math.exp(p.isIncreasing*Math.pow(p.b*x,p.d)),
-        "Exponential-M4": (x, p) =>p.a * (p.c-(p.c-1) * Math.exp(-1.*p.b*x)),
-        "Exponential-M5": (x, p) =>p.a * (p.c-(p.c-1) *  Math.exp(-1.*Math.pow(p.b*x,p.d))),
+        Polynomial: (x, p) =>
+            p.beta_0 + p.beta_1 * x + p.beta_2 ||
+            0 * Math.pow(x, 2) + p.beta_3 ||
+            0 * Math.pow(x, 3) + p.beta_4 ||
+            0 * Math.pow(x, 4) + p.beta_5 ||
+            0 * Math.pow(x, 5) + p.beta_6 ||
+            0 * Math.pow(x, 6) + p.beta_7 ||
+            0 * Math.pow(x, 7) + p.beta_8 ||
+            0 * Math.pow(x, 8),
+        Linear: (x, p) => p.beta_0 + p.beta_1 * x,
+        "Exponential-M2": (x, p) => p.a * Math.exp(p.isIncreasing * p.b * x),
+        "Exponential-M3": (x, p) => p.a * Math.exp(p.isIncreasing * Math.pow(p.b * x, p.d)),
+        "Exponential-M4": (x, p) => p.a * (p.c - (p.c - 1) * Math.exp(-1 * p.b * x)),
+        "Exponential-M5": (x, p) => p.a * (p.c - (p.c - 1) * Math.exp(-1 * Math.pow(p.b * x, p.d))),
         Power: (x, p) => p.control + p.slope * Math.pow(x, p.power),
-        Hill: (x, p) => p.intercept + (p.v * Math.pow(x, p.n)) / (Math.pow(p.k, p.n) + Math.pow(x, p.n)),
-        // Multistage: (x, p) =>p.Background + (1. - p.Background) * (1. - Math.exp( -1. * {Beta(1)}*x - {Beta(2)}*Math.pow(x,2) - {Beta(3)}*Math.pow(x,3) - {Beta(4)}*Math.pow(x,4) - {Beta(5)}*Math.pow(x,5) - {Beta(6)}*Math.pow(x,6) - {Beta(7)}*Math.pow(x,7) - {Beta(8)}*Math.pow(x,8))),
-        // "Multistage-Cancer": (x, p) =>p.Background + (1. - p.Background) * (1. - Math.exp( -1. * {Beta(1)}*x - {Beta(2)}*Math.pow(x,2) - {Beta(3)}*Math.pow(x,3) - {Beta(4)}*Math.pow(x,4) - {Beta(5)}*Math.pow(x,5) - {Beta(6)}*Math.pow(x,6) - {Beta(7)}*Math.pow(x,7) - {Beta(8)}*Math.pow(x,8))),
-        Weibull:
-            (x, p) =>p.Background + (1-p.Background) * (1 - Math.exp( -1.*p.Slope * Math.pow(x,p.Power) )),
-        LogProbit: (x, p) =>p.background + (1-p.background) * normalCDF(p.intercept + p.slope*Math.log(x)),
-        Probit: (x, p) =>normalCDF(p.intercept + p.slope*x),
-        Gamma: (x, p) =>p.Background + (1 - p.Background) * gammaCDF(x*p.Slope,p.Power),
-        LogLogistic:
-            (x, p) =>p.background + (1-p.background)/( 1 + Math.exp(-1.*p.intercept-1.*p.slope*Math.log(x) ) ),
-        Logistic: (x, p) => 1 / (1 + Math.exp(-1 * p.intercept - p.slope * x )),
-        "Dichotomous-Hill":
-            (x, p) =>p.v * p.g + (p.v - p.v * p.g) / (1 + Math.exp(-1 * p.intercept - p.slope * Math.log(x))),
+        Hill: (x, p) =>
+            p.intercept + (p.v * Math.pow(x, p.n)) / (Math.pow(p.k, p.n) + Math.pow(x, p.n)),
+        Multistage: (x, p) =>
+            p.background +
+            (1 - p.background) *
+                (1 -
+                    Math.exp(
+                        -1 * p.beta_1 * x - p.beta_2 ||
+                            0 * Math.pow(x, 2) - p.beta_3 ||
+                            0 * Math.pow(x, 3) - p.beta_4 ||
+                            0 * Math.pow(x, 4) - p.beta_5 ||
+                            0 * Math.pow(x, 5) - p.beta_6 ||
+                            0 * Math.pow(x, 6) - p.beta_7 ||
+                            0 * Math.pow(x, 7) - p.beta_8 ||
+                            0 * Math.pow(x, 8)
+                    )),
+        "Multistage-Cancer": (x, p) =>
+            p.background +
+            (1 - p.background) *
+                (1 -
+                    Math.exp(
+                        -1 * p.beta_1 * x - p.beta_2 ||
+                            0 * Math.pow(x, 2) - p.beta_3 ||
+                            0 * Math.pow(x, 3) - p.beta_4 ||
+                            0 * Math.pow(x, 4) - p.beta_5 ||
+                            0 * Math.pow(x, 5) - p.beta_6 ||
+                            0 * Math.pow(x, 6) - p.beta_7 ||
+                            0 * Math.pow(x, 7) - p.beta_8 ||
+                            0 * Math.pow(x, 8)
+                    )),
+        Weibull: (x, p) =>
+            p.background + (1 - p.background) * (1 - Math.exp(-1 * p.slope * Math.pow(x, p.power))),
+        LogProbit: (x, p) =>
+            p.background + (1 - p.background) * normalCDF(p.intercept + p.slope * Math.log(x)),
+        Probit: (x, p) => normalCDF(p.intercept + p.slope * x),
+        Gamma: (x, p) => p.background + (1 - p.background) * gammaCDF(x * p.slope, p.power),
+        LogLogistic: (x, p) =>
+            p.background +
+            (1 - p.background) / (1 + Math.exp(-1 * p.intercept - 1 * p.slope * Math.log(x))),
+        Logistic: (x, p) => 1 / (1 + Math.exp(-1 * p.intercept - p.slope * x)),
+        "Dichotomous-Hill": (x, p) =>
+            p.v * p.g +
+            (p.v - p.v * p.g) / (1 + Math.exp(-1 * p.intercept - p.slope * Math.log(x))),
+    },
+    cleanParameters = function(parameters, estimates) {
+        const cleanNames = _.chain(parameters)
+                .keys()
+                .map(d =>
+                    d
+                        .toLowerCase()
+                        .replace("(", "_")
+                        .replace(")", "")
+                )
+                .value(),
+            paramValues = _.chain(parameters)
+                .values()
+                .map(d => d.estimate)
+                .value(),
+            mapping = _.zipObject(cleanNames, paramValues);
+
+        mapping["isIncreasing"] = estimates[0] < estimates[estimates.length - 1] ? 1 : -1;
+        return mapping;
     };
 
-export const buildModelFormula = function(model_name, estimates, parameters) {
-        let params = {},
-            formula = formulas[model_name],
-            params_in_formula = formula.match(/\{[\w()]+\}/g);
-
-        // get parameter values for models
-        parameters.map((v, k) => params[k] = v.estimate);
-        params["isIncreasing"] = estimates[0] < estimates[estimates.length - 1] ? 1 : -1;
-        // TODO - replace (6) w/ _6
-
-        _.each(params_in_formula, function(param) {
-            let unbracketed = param.slice(1, param.length - 1),
-                v = params[unbracketed] !== undefined ? params[unbracketed] : 0,
-                regex = param.replace("(", "\\(").replace(")", "\\)"), // escape ()
-                re = new RegExp(regex, "g");
-            formula = formula.replace(re, v);
-        });
-        return formula;
+export const buildModelFormula = function(name, parameters, estimates) {
+        const params = cleanParameters(parameters, estimates),
+            formula = formulas[name];
+        return {params, formula};
     },
     bmdLabelText = function(bmr) {
         let str, val;
