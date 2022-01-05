7ff971712441d01b56b5d11a7619e1df5da0d231
Andy Shapiro <shapiromatron@gmail.com>
2022-01-05 11:06:57 -0500

    fix validation

--- hawc/apps/lit/management/commands/doi.py
+++ hawc/apps/lit/management/commands/doi.py
@@ -2,9 +2,10 @@ from collections import defaultdict
 
 from django.core.management.base import BaseCommand
 from django.db import transaction
+from django.db.models import Prefetch
 
 from hawc.apps.assessment.models import Assessment
-from hawc.apps.lit.constants import ReferenceDatabase, DOI_EXACT
+from hawc.apps.lit.constants import DOI_EXACT, ReferenceDatabase
 from hawc.apps.lit.models import Identifiers, Reference
 from hawc.services.utils.doi import get_doi_from_identifier, try_get_doi
 
@@ -42,82 +43,88 @@ class Command(BaseCommand):
         else:
             assessments = Assessment.objects.all().order_by("id")
         if options["validate_existing"]:
-            for assessment in assessments:
-                validate_existing(self.stdout, assessment)
+            validate_existing(self.stdout)
         if options["create_from_existing"]:
             for assessment in assessments:
                 create_from_existing(self.stdout, assessment, options["full_text"])
 
 
-def validate_existing(logger, assessment):
+def validate_existing(logger):
     """Checks all existing doi IDs for valid doi ids, which are stored as a unique_id for doi identifiers
     Attempts to clean all the failing dois, merges duplicate doi IDs, and deletes invalid doi IDs that cannot be cleaned
     """
-    doi_identifiers = Identifiers.objects.filter(
-        database=ReferenceDatabase.DOI, references__assessment=assessment
-    )
+    doi_identifiers = Identifiers.objects.filter(database=ReferenceDatabase.DOI)
     n = doi_identifiers.count()
-    logger.write(f"Assessment {assessment.id}; validating {n} DOIs")
+    logger.write(f"Validating {n} DOIs")
     if n == 0:
         return
 
-    existing_dois = {}
-    doi_idents_updated = []
-    # duplicate_doi_ids: stores unclean DOIs that have a clean duplicate for updating later
-    duplicate_doi_ids = defaultdict(list)
-    # invalid DOI IDs cannot be cleaned/validated (ex: '10')
+    existing_dois = 0
     invalid_doi_idents = []
+    new_candidates_ids = []
+    new_candidates_dois = []
 
-    # loop through once and add all valid dois to saved list to check for dupes as we go
-    for ident in doi_identifiers.prefetch_related("references"):
+    # loop through once and add all valid DOIs to saved list to check for dupes as we go
+    for ident in doi_identifiers.iterator():
         if DOI_EXACT.fullmatch(ident.unique_id):
-            existing_dois[ident.unique_id] = ident
+            existing_dois += 1
         else:
             doi = try_get_doi(ident.unique_id, full_text=True)
             if doi:
-                if doi in existing_dois:
-                    # DOI exists already; save identifier as a duplicate
-                    duplicate_doi_ids[doi].append(ident)
-                else:
-                    # TODO - logic fails here; we need to query to check again to see if an existing doi exits. And, this gets more complicated, because we dont want to duplicates w/ the bulk_update
-                    # DOI validation resulted in a new DOI
-                    ident.unique_id = doi
-                    doi_idents_updated.append(ident)
-                    existing_dois[doi] = ident
+                new_candidates_dois.append(doi)
+                new_candidates_ids.append(ident.id)
             else:
                 # DOI is invalid, save to be deleted below
-                invalid_doi_idents.append(ident.id)
+                invalid_doi_idents.append(ident)
 
-    logger.write(f"{len(existing_dois):8} DOI unchanged")
+    # unchanged
+    logger.write(f"{existing_dois:8} DOI unchanged")
 
     # deleted
-    logger.write(
-        f"{len(doi_idents_updated):8} DOI deleted; invalid IDs -> {','.join(str(id) for id in invalid_doi_idents)}"
-    )
-    Identifiers.objects.filter(id__in=invalid_doi_idents).delete()
+    ids = [ident.id for ident in invalid_doi_idents]
+    dois = "\n".join([f"\t\t{ident.unique_id}" for ident in invalid_doi_idents])
+    logger.write(f"{len(invalid_doi_idents):8} DOI deleted; invalid IDs:\n{dois}")
+    Identifiers.objects.filter(id__in=ids).delete()
 
-    # bulk-update new DOIs terms which need to be created
-    Identifiers.objects.bulk_update(doi_idents_updated, ["unique_id"])
-    logger.write(
-        f"{len(doi_idents_updated):8} DOI updated; changed to valid DOI-> {','.join(str(doi_idents_updated.id) for id in doi_idents_updated)}"
-    )
+    # update or merge
+    logger.write(f"{len(new_candidates_dois):8} new DOI candidates extracted from bad DOIs")
 
-    # pull references with incorrect DOI id and replace that ID with the correct duplicate doi ID
-    logger.write(f"{len(duplicate_doi_ids):8} DOI merged; duplicate DOI")
-    creates = []
-    deletes = []
+    # get existing dois that may already match
+    existing_dois = {
+        k: v
+        for (k, v) in Identifiers.objects.filter(
+            database=ReferenceDatabase.DOI, unique_id__in=new_candidates_dois
+        ).values_list("unique_id", "id")
+    }
+    new_candidates_idents = Identifiers.objects.filter(id__in=new_candidates_ids).prefetch_related(
+        Prefetch("references", queryset=Reference.objects.all().only("id"))
+    )
+    ident_deletes = []
+    ident_updates = []
+    m2m_creates = []
     RefIdentM2M = Reference.identifiers.through
-    for doi, old_identifiers in duplicate_doi_ids.items():
-        new_identifier_id = existing_dois[doi].id
-        for identifier in old_identifiers:
-            for reference in identifier.references.all():
-                creates.append(
-                    RefIdentM2M(reference_id=reference.id, identifier_id=new_identifier_id)
-                )
-            deletes.append(identifier.id)
+    for ident in new_candidates_idents:
+        doi = try_get_doi(ident.unique_id, full_text=True)
+        if doi in existing_dois:
+            # mark this identifier for deletion
+            ident_deletes.append(ident.id)
+            # update m2m relations to existing identifier
+            new_ident_id = existing_dois[doi]
+            for ref in ident.references.all():
+                m2m_creates.append(RefIdentM2M(identifiers_id=new_ident_id, reference_id=ref.id))
+        else:
+            # update this identifier
+            ident.unique_id = doi
+            existing_dois[doi] = ident.id
+            ident_updates.append(ident)
 
-    RefIdentM2M.objects.bulk_create(creates)
-    Identifiers.objects.filter(id__in=deletes).delete()
+    logger.write(f"{len(ident_deletes):8} DOIs deleted from candidates")
+    logger.write(f"{len(ident_updates):8} DOIs updated from candidates")
+    logger.write(f"{len(m2m_creates):8} new Identifier-Reference relations created")
+
+    Identifiers.objects.filter(id__in=ident_deletes).delete()
+    Identifiers.objects.bulk_update(ident_updates, ["unique_id"])
+    RefIdentM2M.objects.bulk_create(m2m_creates)
 
 
 def create_from_existing(logger, full_text: bool):
