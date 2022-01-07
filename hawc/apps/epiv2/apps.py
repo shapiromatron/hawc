8be3ca87f07df7605e509823b5fdabd685acdc26
Matthew Munns <mmunns16@gmail.com>
2022-01-07 10:52:06 -0500

    Add epi v2 to installed apps

--- hawc/apps/epiv2/apps.py
+++ hawc/apps/epiv2/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class Epiv2Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'epiv2'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "hawc.apps.epiv2"
+    verbose_name = "Epi v2"
