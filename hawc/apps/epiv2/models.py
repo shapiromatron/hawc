3c95ebcd7a304bd8cd21f690816f135d905f4cc4
Matthew Munns <mmunns16@gmail.com>
2022-01-10 12:00:23 -0500

    allow blank and null for more fields

--- hawc/apps/epiv2/models.py
+++ hawc/apps/epiv2/models.py
@@ -41,9 +41,11 @@ class MeasurementType(models.Model):
 class StudyPopulation(models.Model):
     study = models.ForeignKey(Study, on_delete=models.CASCADE, related_name="study_populations_v2")
     study_design = models.CharField(
-        max_length=128, choices=constants.StudyDesign.choices, blank=True
+        max_length=128, choices=constants.StudyDesign.choices, blank=True, null=True,
     )
-    source = models.CharField(max_length=128, choices=constants.Source.choices)
+    source = models.CharField(
+        max_length=128, choices=constants.Source.choices, blank=True, null=True,
+    )
     age_profile = models.ManyToManyField(
         AgeProfile,
         blank=True,
@@ -56,9 +58,9 @@ class StudyPopulation(models.Model):
         verbose_name="Population Summary",
         help_text="Breifly describe the study population (e.g., Women undergoing fertility treatment).",
     )
-    countries = models.ManyToManyField(Country, blank=True)
+    countries = models.ManyToManyField(Country, blank=True, null=True,)
     region = models.CharField(
-        max_length=128, blank=True, verbose_name="Other geographic information"
+        max_length=128, blank=True, null=True, verbose_name="Other geographic information"
     )
     participant_n = models.PositiveIntegerField(
         verbose_name="Overall study population N",
@@ -97,37 +99,50 @@ class Criteria(models.Model):
 
 
 class Exposure(models.Model):
+    name = models.CharField(
+        max_length=64,
+        help_text="A unique name for this exposure that will help you identify it later.",
+    )
     study_population = models.ForeignKey(
         StudyPopulation, on_delete=models.CASCADE, related_name="exposures"
     )
     measurement_type = models.ManyToManyField(
-        MeasurementType, verbose_name="Exposure measurement type"
+        MeasurementType, verbose_name="Exposure measurement type", blank=True, null=True,
     )
     biomonitoring_matrix = models.CharField(max_length=128,)
     measurement_timing = models.CharField(
         max_length=128,
+        blank=True,
+        null=True,
         verbose_name="Timing of exposure measurement",
         help_text='If timing is based on something other than age, specify the timing (e.g., start of employment at Factory A). If cross-sectional, enter "cross-sectional"',
     )
-    exposure_route = models.CharField(max_length=2, choices=constants.ExposureRoute.choices)
-    analytic_method = models.CharField(max_length=128,)
-    comments = models.CharField(max_length=128, blank=True)
+    exposure_route = models.CharField(
+        max_length=2, choices=constants.ExposureRoute.choices, blank=True, null=True,
+    )
+    analytic_method = models.CharField(max_length=128, blank=True, null=True,)
+    comments = models.CharField(max_length=128, blank=True, null=True,)
     created = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
 
-    # TODO: need a better way of displaying a unique (within the study) name
     def __str__(self):
-        return "Exposure #" + str(self.id)
+        return self.name
 
 
 class ExposureLevel(models.Model):
+    name = models.CharField(
+        max_length=64,
+        help_text="A unique name for this exposure level that will help you identify it later.",
+    )
     study_population = models.ForeignKey(
         StudyPopulation, on_delete=models.CASCADE, related_name="exposure_levels"
     )
     chemical = models.ForeignKey(Chemical, on_delete=models.CASCADE)
-    exposure_measurement = models.ForeignKey(Exposure, on_delete=models.CASCADE)
+    exposure_measurement = models.ForeignKey(
+        Exposure, on_delete=models.CASCADE, blank=True, null=True,
+    )
     sub_population = models.CharField(
-        max_length=128, verbose_name="Sub-population (if relevant)", blank=True
+        max_length=128, verbose_name="Sub-population (if relevant)", blank=True, null=True,
     )
     central_tendency = models.CharField(max_length=128,)
     central_tendency_type = models.CharField(
@@ -135,25 +150,37 @@ class ExposureLevel(models.Model):
         choices=constants.CentralTendencyType.choices,
         default=constants.CentralTendencyType.MEDIAN,
         verbose_name="Central tendency type (median preferred)",
+        blank=True,
+        null=True,
     )
-    minimum = models.FloatField()
-    maximum = models.FloatField()
-    percentile25 = models.FloatField()
-    percentile75 = models.FloatField()
+    minimum = models.FloatField(blank=True, null=True,)
+    maximum = models.FloatField(blank=True, null=True,)
+    percentile25 = models.FloatField(blank=True, null=True,)
+    percentile75 = models.FloatField(blank=True, null=True,)
     neg_exposure = models.FloatField(
-        verbose_name="Percent with negligible exposure", help_text="e.g., %% below the LOD"
+        verbose_name="Percent with negligible exposure",
+        help_text="e.g., %% below the LOD",
+        blank=True,
+        null=True,
     )
-    comments = models.CharField(max_length=128, verbose_name="Exposure level comments")
-    data_location = models.CharField(max_length=128, help_text="e.g., table number")
+    comments = models.CharField(
+        max_length=128, verbose_name="Exposure level comments", blank=True, null=True,
+    )
+    data_location = models.CharField(
+        max_length=128, help_text="e.g., table number", blank=True, null=True,
+    )
     created = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
 
-    # TODO: need a better way of displaying a unique (within the study) name
     def __str__(self):
-        return "Exposure Level #" + str(self.id)
+        return self.name
 
 
 class Outcome(models.Model):
+    name = models.CharField(
+        max_length=64,
+        help_text="A unique name for this health outcome that will help you identify it later.",
+    )
     study_population = models.ForeignKey(
         StudyPopulation, on_delete=models.CASCADE, related_name="outcomes"
     )
@@ -162,57 +189,75 @@ class Outcome(models.Model):
         max_length=128,
         choices=constants.HealthOutcomeSystem.choices,
         help_text="If multiple cancer types are present, report all types under Cancer.",
+        blank=True,
+        null=True,
     )
     created = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
 
-    # TODO: need a better way of displaying a unique (within the study) name
     def __str__(self):
-        return "Health Outcome #" + str(self.id)
+        return self.name
 
 
 class AdjustmentFactor(models.Model):
+    name = models.CharField(
+        max_length=64,
+        help_text="A unique name for this adjustment factor that will help you identify it later.",
+    )
     study_population = models.ForeignKey(
         StudyPopulation, on_delete=models.CASCADE, related_name="adjustment_factors"
     )
-    description = models.CharField(max_length=128,)
+    description = models.CharField(max_length=128, blank=True, null=True,)
     created = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
 
-    # TODO: need a better way of displaying a unique (within the study) name
     def __str__(self):
-        return "Adjustment Factor #" + str(self.id)
+        return self.name
 
 
 class DataExtraction(models.Model):
     study_population = models.ForeignKey(
         StudyPopulation, on_delete=models.CASCADE, related_name="data_extractions"
     )
-    outcome = models.ForeignKey(Outcome, on_delete=models.CASCADE)
-    exposure_level = models.ForeignKey(ExposureLevel, on_delete=models.SET_NULL, null=True)
-    n = models.PositiveIntegerField()
+    outcome = models.ForeignKey(Outcome, on_delete=models.CASCADE, blank=True, null=True,)
+    exposure_level = models.ForeignKey(
+        ExposureLevel, on_delete=models.SET_NULL, blank=True, null=True
+    )
+    n = models.PositiveIntegerField(blank=True, null=True,)
     effect_estimate_type = models.CharField(
-        max_length=128, choices=constants.EffectEstimateType.choices
+        max_length=128, choices=constants.EffectEstimateType.choices, blank=True, null=True,
     )
-    effect_estimate = models.CharField(max_length=128,)
-    effect_description = models.CharField(max_length=128,)
-    measurement_timing = models.CharField(max_length=128,)
+    effect_estimate = models.CharField(max_length=128, blank=True, null=True,)
+    effect_description = models.CharField(max_length=128, blank=True, null=True,)
+    measurement_timing = models.CharField(max_length=128, blank=True, null=True,)
     exposure_rank = models.PositiveSmallIntegerField(
-        help_text="Rank this comparison group by exposure (lowest exposure group = 1)"
+        blank=True,
+        null=True,
+        help_text="Rank this comparison group by exposure (lowest exposure group = 1)",
     )
-    ci_lcl = models.FloatField(verbose_name="Confidence Interval LCL", blank=True)
-    ci_ucl = models.FloatField(verbose_name="Confidence Interval UCL", blank=True)
-    sd_or_se = models.FloatField(verbose_name="Standard Deviation or Standard Error", blank=True)
-    pvalue = models.CharField(verbose_name="p-value", max_length=128, blank=True)
+    ci_lcl = models.FloatField(verbose_name="Confidence Interval LCL", blank=True, null=True,)
+    ci_ucl = models.FloatField(verbose_name="Confidence Interval UCL", blank=True, null=True,)
+    sd_or_se = models.FloatField(
+        verbose_name="Standard Deviation or Standard Error", blank=True, null=True,
+    )
+    pvalue = models.CharField(verbose_name="p-value", max_length=128, blank=True, null=True,)
     significant = models.BooleanField(
-        verbose_name="Statistically Significant", choices=constants.SIGNIFICANT_CHOICES
+        verbose_name="Statistically Significant",
+        choices=constants.SIGNIFICANT_CHOICES,
+        blank=True,
+        null=True,
     )
-    adjustment_factor = models.ForeignKey(AdjustmentFactor, on_delete=models.SET_NULL, null=True)
-    confidence = models.CharField(max_length=128, verbose_name="Study confidence")
-    # TODO: data location appears in Exposure Level also. Are both required?
-    data_location = models.CharField(max_length=128, help_text="e.g., table number")
-    statistical_method = models.CharField(max_length=128,)
-    comments = models.CharField(max_length=128, blank=True)
+    adjustment_factor = models.ForeignKey(
+        AdjustmentFactor, on_delete=models.SET_NULL, blank=True, null=True,
+    )
+    confidence = models.CharField(
+        max_length=128, verbose_name="Study confidence", blank=True, null=True,
+    )
+    data_location = models.CharField(
+        max_length=128, help_text="e.g., table number", blank=True, null=True,
+    )
+    statistical_method = models.CharField(max_length=128, blank=True, null=True,)
+    comments = models.CharField(max_length=128, blank=True, null=True,)
     created = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
 
