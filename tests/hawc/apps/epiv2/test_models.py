3f6796875fcb58760cece3ce80c8dc97420f46de
Matthew Munns <mmunns16@gmail.com>
2022-01-14 14:49:09 -0500

    Rename studypopulationv2 in yaml file as well

--- tests/hawc/apps/epiv2/test_models.py
+++ tests/hawc/apps/epiv2/test_models.py
@@ -8,3 +8,31 @@ def test_get_assessment(db_keys):
     for Model in [models.Design]:
         item = Model.objects.first()
         assert item.get_assessment().id == 1
+
+    for Model in [models.Chemical]:
+        item = Model.objects.first()
+        assert item.get_assessment().id == 1
+
+    for Model in [models.Criteria]:
+        item = Model.objects.first()
+        assert item.get_assessment().id == 1
+
+    for Model in [models.Exposure]:
+        item = Model.objects.first()
+        assert item.get_assessment().id == 1
+
+    for Model in [models.ExposureLevel]:
+        item = Model.objects.first()
+        assert item.get_assessment().id == 1
+
+    for Model in [models.Outcome]:
+        item = Model.objects.first()
+        assert item.get_assessment().id == 1
+
+    for Model in [models.AdjustmentFactor]:
+        item = Model.objects.first()
+        assert item.get_assessment().id == 1
+
+    for Model in [models.DataExtraction]:
+        item = Model.objects.first()
+        assert item.get_assessment().id == 1
