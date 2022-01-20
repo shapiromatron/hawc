bdcfed8833036e9efbe2dcab7eecae852a7729ca
munnsmunns <mmunns16@gmail.com>
2022-01-20 12:35:51 -0500

    fix typo

--- hawc/apps/assessment/migrations/0030_assessment_models.py
+++ hawc/apps/assessment/migrations/0030_assessment_models.py
@@ -57,7 +57,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ("assessment", "0029_communications"),
+        ("assessment", "0029_communication"),
     ]
 
     operations = [
