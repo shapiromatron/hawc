7a94af2457498620fc06bd338897b12d99f16ae1
Andy Shapiro <shapiromatron@gmail.com>
2022-01-24 22:14:49 -0500

    minor refactor and rewording; move option to separate option

--- hawc/apps/assessment/management/commands/chemicals.py
+++ hawc/apps/assessment/management/commands/chemicals.py
@@ -9,31 +9,34 @@ from hawc.apps.assessment.models import DSSTox
 from hawc.services.epa.dsstox import DssSubstance
 
 
-class Command(BaseCommand):
-    help = """Crawls all existing DSSTox instances and updates with fresh data"""
-
-    def add_arguments(self, parser):
-        parser.add_argument(
-            "--refresh", action="store_true", help="Refresh exisiting DSSTox",
-        )
-
-    @transaction.atomic
-    def handle(self, *args, **options):
-        if options["refresh"]:
-            dsstoxs = DSSTox.objects.all()
-            dsstox_update = []
-            for dsstox in dsstoxs.iterator():
-                updated_dsstox = DssSubstance.create_from_dtxsid(dsstox.dtxsid)
-                if dict_hash(dsstox.content) != dict_hash(updated_dsstox.content):
-                    dsstox.content = updated_dsstox.content
-                    dsstox_update.append(dsstox)
-            if dsstox_update:
-                DSSTox.objects.bulk_update(dsstox_update, ["content"])
-
-
-def dict_hash(dict: Dict[str, Any]) -> str:
+def _dict_hash(dict: Dict[str, Any]) -> str:
     """MD5 hash of a dictionary"""
     dhash = hashlib.md5()
     encoded = json.dumps(dict, sort_keys=True).encode()
     dhash.update(encoded)
     return dhash.hexdigest()
+
+
+class Command(BaseCommand):
+    help = """Chemical data operations"""
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--refresh", action="store_true", help="Refresh existing DSSTox",
+        )
+
+    def _refresh(self):
+        updates = []
+        for dsstox in DSSTox.objects.all().iterator():
+            new_dsstox = DssSubstance.create_from_dtxsid(dsstox.dtxsid)
+            existing_content = dsstox.content
+            if _dict_hash(existing_content) != _dict_hash(new_dsstox.content):
+                dsstox.content = new_dsstox.content
+                updates.append(dsstox)
+        if updates:
+            DSSTox.objects.bulk_update(updates, ["content"])
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        if options["refresh"]:
+            self._refresh()
