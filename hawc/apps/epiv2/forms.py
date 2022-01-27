c140856b6e31c5d3c15f6e554182adba9fdc3f01
munnsmunns <mmunns16@gmail.com>
2022-01-27 11:45:37 -0500

    Add helper to design form

--- hawc/apps/epiv2/forms.py
+++ hawc/apps/epiv2/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.urls import reverse
+
+from hawc.apps.common.forms import BaseFormHelper
 
 from . import models
 
@@ -14,3 +17,31 @@ class DesignForm(forms.ModelForm):
         model = models.Design
         exclude = ("study",)
         labels = {}
+
+    @property
+    def helper(self):
+        for fld in list(self.fields.keys()):
+            widget = self.fields[fld].widget
+            if type(widget) == forms.Textarea:
+                widget.attrs["rows"] = 3
+
+        if self.instance.id:
+            inputs = {
+                "legend_text": f"Update {self.instance}",
+                "help_text": self.UPDATE_HELP_TEXT,
+                "cancel_url": self.instance.get_absolute_url(),
+            }
+        else:
+            inputs = {
+                "legend_text": self.CREATE_LEGEND,
+                "help_text": self.CREATE_HELP_TEXT,
+                "cancel_url": self.instance.study.get_absolute_url(),
+            }
+
+        helper = BaseFormHelper(self, **inputs)
+
+        helper.add_row("study_design", 3, "col-md-4")
+        helper.add_row("age_description", 3, "col-md-4")
+        helper.add_row("summary", 2, "col-md-6")
+        helper.add_row("countries", 4, "col-md-3")
+        return helper
