f206af0f5df6dcb04590789b275c2bea51979512
munnsmunns <mmunns16@gmail.com>
2022-01-31 10:20:21 -0500

    Build start of epi v2 form views (#583)
    * Create initial form for Design model
    
    * Create templates for design detail and form
    
    * add epiv2 design detail, create,  update to urls
    
    * Add helper to design form
    
    * epi v2 is now linked to the study detail page
    
    * add init method to DesignForm to set parent study
    
    * lint
    
    * Add design delete view
    
    * test design views and permissions
    
    * fix failure tests to only use anon client
    
    * rename urls ex: des_create to design_create
    
    * rename epiv2 to epidemiology for user facing url
    
    * remove obj container
    
    * streamline templates
    
    Co-authored-by: Andy Shapiro <shapiromatron@gmail.com>

---
+++ hawc/apps/epiv2/forms.py
@@ -0,0 +1,52 @@
+from django import forms
+
+from hawc.apps.common.forms import BaseFormHelper
+
+from . import models
+
+
+class DesignForm(forms.ModelForm):
+    CREATE_LEGEND = "Create new study-population"
+
+    CREATE_HELP_TEXT = ""
+
+    UPDATE_HELP_TEXT = "Update an existing study-population."
+
+    class Meta:
+        model = models.Design
+        exclude = ("study",)
+        labels = {}
+
+    def __init__(self, *args, **kwargs):
+        study = kwargs.pop("parent", None)
+        super().__init__(*args, **kwargs)
+        if study:
+            self.instance.study = study
+
+    @property
+    def helper(self):
+        for fld in list(self.fields.keys()):
+            widget = self.fields[fld].widget
+            if type(widget) == forms.Textarea:
+                widget.attrs["rows"] = 3
+
+        if self.instance.id:
+            inputs = {
+                "legend_text": f"Update {self.instance}",
+                "help_text": self.UPDATE_HELP_TEXT,
+                "cancel_url": self.instance.get_absolute_url(),
+            }
+        else:
+            inputs = {
+                "legend_text": self.CREATE_LEGEND,
+                "help_text": self.CREATE_HELP_TEXT,
+                "cancel_url": self.instance.study.get_absolute_url(),
+            }
+
+        helper = BaseFormHelper(self, **inputs)
+
+        helper.add_row("study_design", 3, "col-md-4")
+        helper.add_row("age_description", 3, "col-md-4")
+        helper.add_row("summary", 2, "col-md-6")
+        helper.add_row("countries", 4, "col-md-3")
+        return helper
---
+++ tests/hawc/apps/epiv2/test_view_permissions.py
@@ -0,0 +1,142 @@
+import pytest
+from django.test.client import Client
+from django.urls import reverse
+from pytest_django.asserts import assertTemplateUsed
+
+from hawc.apps.epiv2.models import Design
+
+_successful_post = {
+    "study_design": "CC",
+    "source": "GP",
+    "age_profile": "1",
+    "age_description": "10-15 yrs",
+    "sex": "B",
+    "race": "Not specified but likely primarily Asian",
+    "summary": "Case-control study of asthma in children",
+    "study_name": "Genetic and Biomarkers study for Childhood Asthma",
+    "countries": "2",
+    "region": "northern Taiwan",
+    "years": "2009-2010",
+    "participant_n": "456",
+}
+
+
+@pytest.mark.django_db
+class TestCreatePermissions:
+    def test_success(self, db_keys):
+        # with authentication
+        url = reverse("epiv2:design_create", kwargs={"pk": db_keys.study_working})
+        c = Client()
+        assert c.login(username="admin@hawcproject.org", password="pw") is True
+        response = c.get(url)
+        assert response.status_code == 200
+
+        n_designs = Design.objects.count()
+        with assertTemplateUsed("epiv2/design_detail.html"):
+            response = c.post(url, data=_successful_post, follow=True,)
+            assert Design.objects.count() == n_designs + 1
+            n_designs += 1
+            assert response.status_code == 200
+
+    def test_failure(self, db_keys):
+        # without authentication
+        url = reverse("epiv2:design_create", kwargs={"pk": db_keys.study_working})
+        c = Client()
+        with assertTemplateUsed("403.html"):
+            c.get(url, follow=True)
+
+        with assertTemplateUsed("403.html"):
+            c.post(url, _successful_post, follow=True)
+
+
+@pytest.mark.django_db
+class TestDetailPermissions:
+    def test_detail(self, db_keys):
+        # these users should have permission
+        users = (
+            "admin@hawcproject.org",
+            "pm@hawcproject.org",
+            "team@hawcproject.org",
+            "reviewer@hawcproject.org",
+        )
+        for user in users:
+            c = Client()
+            assert c.login(email=user, password="pw") is True
+            response = c.get(reverse("epiv2:design_detail", kwargs={"pk": db_keys.epiv2_design}))
+            assert response.status_code == 200
+
+        # anon user should not view
+        c = Client()
+        url = reverse("epiv2:design_detail", kwargs={"pk": db_keys.epiv2_design})
+        response = c.get(url)
+        assert response.status_code == 403
+
+
+@pytest.mark.django_db
+class TestEditPermissions:
+    def test_success(self, db_keys):
+        clients = ("admin@hawcproject.org", "pm@hawcproject.org")
+        for client in clients:
+            c = Client()
+            assert c.login(email=client, password="pw") is True
+
+            # django template views
+            view = "epiv2:design_update"
+            pk = db_keys.epiv2_design
+            response = c.get(reverse(view, args=(pk,)))
+            assert response.status_code == 200
+
+            # check post updates `epiv2_design`
+            with assertTemplateUsed("epiv2/design_detail.html"):
+                response = c.post(
+                    reverse("epiv2:design_update", kwargs={"pk": db_keys.epiv2_design},),
+                    data=_successful_post,
+                    follow=True,
+                )
+                assert response.status_code == 200
+
+    def test_failure(self, db_keys):
+        c = Client()
+
+        view = "epiv2:design_update"
+        with assertTemplateUsed("403.html"):
+            response = c.get(reverse(view, kwargs={"pk": db_keys.epiv2_design},), follow=True)
+            assert response.status_code == 403
+
+        # check POST
+        with assertTemplateUsed("403.html"):
+            response = c.post(
+                reverse("epiv2:design_update", kwargs={"pk": db_keys.epiv2_design},),
+                {"name": "foo manchu"},
+            )
+            assert response.status_code == 403
+
+
+@pytest.mark.django_db
+class TestDeletePermissions:
+    def test_success(self, db_keys):
+        clients = ("admin@hawcproject.org", "pm@hawcproject.org")
+        for client in clients:
+            c = Client()
+            assert c.login(email=client, password="pw") is True
+
+            # django template views
+            view = "epiv2:design_delete"
+            response = c.get(reverse(view, kwargs={"pk": db_keys.epiv2_design}))
+            assert response.status_code == 200
+
+    def test_failure(self, db_keys):
+        c = Client()
+
+        view = "epiv2:design_delete"
+        with assertTemplateUsed("403.html"):
+            response = c.get(reverse(view, kwargs={"pk": db_keys.epiv2_design}), follow=True)
+            assert response.status_code == 403
+
+        # check POST
+        with assertTemplateUsed("403.html"):
+            response = c.post(
+                reverse("epiv2:design_update", kwargs={"pk": db_keys.epiv2_design}),
+                {"name": "foo manchu"},
+            )
+            assert response.status_code == 403
