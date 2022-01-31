595babe785c7849da7cc7d058a9af3d4b804dbe4
munnsmunns <mmunns16@gmail.com>
2022-01-31 14:07:09 -0500

    remove unnecessary code copied from epi version 1

--- hawc/apps/epiv2/urls.py
+++ hawc/apps/epiv2/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from . import views
+from . import api, views
+
+router = DefaultRouter()
+router.register(r"design", api.Design, basename="design")
 
 app_name = "epiv2"
 urlpatterns = [
+    path("api/", include((router.urls, "api"))),
+
+    # Design (similar to Study Population object from epi version 1)
     path("study/<int:pk>/design/create/", views.DesignCreate.as_view(), name="design_create",),
     path("design/<int:pk>/update/", views.DesignUpdate.as_view(), name="design_update",),
     path("design/<int:pk>/", views.DesignDetail.as_view(), name="design_detail",),
