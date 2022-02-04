46972f601f1a7173b374f62fbba80787406244b4
Andy Shapiro <shapiromatron@gmail.com>
2022-02-04 11:28:28 -0500

    improve species/strain admin

--- hawc/apps/assessment/admin.py
+++ hawc/apps/assessment/admin.py
@@ -177,11 +177,9 @@ class DoseUnitsAdmin(admin.ModelAdmin):
 
 @admin.register(models.Species)
 class SpeciesAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "name",
-    )
+    list_display = ("id", "name")
     list_display_links = ("name",)
+    search_fields = ("name",)
 
 
 @admin.register(models.Strain)
@@ -190,6 +188,7 @@ class StrainAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "species")
     list_display_links = ("name",)
     list_filter = ("species",)
+    search_fields = ("name",)
 
 
 @admin.register(models.EffectTag)
