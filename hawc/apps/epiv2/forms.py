93f55127a903b3974f52adc66f949e38ab8faa69
munnsmunns <mmunns16@gmail.com>
2022-02-11 10:55:25 -0500

    typo

--- hawc/apps/epiv2/forms.py
+++ hawc/apps/epiv2/forms.py
@@ -42,7 +42,7 @@ class DesignForm(forms.ModelForm):
             inputs = {
                 "legend_text": f"Update {self.instance}",
                 "help_text": self.UPDATE_HELP_TEXT,
-                "form_action": "",
+                "form_actions": "",
             }
             helper = BaseFormHelper(self, **inputs)
             helper.form_tag = False
