812b0c4f0438810e6aee2bc27834f6375b689e93
Andy Shapiro <shapiromatron@gmail.com>
2022-03-09 11:16:54 -0500

    Merge remote-tracking branch 'origin/epi_v2_child_forms' into epi_v2_child_forms
    # Conflicts:
    #	hawc/apps/assessment/templates/assessment/_attachment_list.html
    #	hawc/apps/assessment/urls.py
    #	hawc/apps/epiv2/migrations/0001_initial.py
    #	hawc/apps/study/templates/study/study_detail.html

---
+++ hawc/apps/common/templatetags/optional_table_row.py
@@ -0,0 +1,14 @@
+from typing import Any, Optional
+
+from django import template
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+
+@register.simple_tag
+def optional_table_row(name: str, value: Any) -> Optional[str]:
+    if value is not None and value != "":
+        return mark_safe(f"<tr><th>{name}</th><td>{escape(value)}</td></tr>")
+    return None
