2a41b6ff1d81271cead8210dc5679ee11d3c5d09
munnsmunns <mmunns16@gmail.com>
2022-03-10 09:52:39 -0500

    fix isort

--- hawc/apps/myuser/api.py
+++ hawc/apps/myuser/api.py
@@ -1,6 +1,6 @@
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.throttling import AnonRateThrottle
 from rest_framework.views import APIView
 
