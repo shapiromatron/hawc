ff3f411c4a6c7cc07ccfd3cc8dc4a0f120b71af5
munnsmunns <mmunns16@gmail.com>
2022-03-16 16:41:17 -0400

    lint

--- frontend/shared/startup.js
+++ frontend/shared/startup.js
@@ -10,21 +10,21 @@ import Quillify from "./utils/Quillify";
 $.fn.quillify = Quillify;
 
 // Django AJAX with CSRF protection.
-const getCookie = function (name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== "") {
-        var cookies = document.cookie.split(";");
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = $.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == name + "=") {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
+const getCookie = function(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie !== "") {
+            var cookies = document.cookie.split(";");
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = $.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == name + "=") {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
             }
         }
-    }
-    return cookieValue;
-},
+        return cookieValue;
+    },
     csrftoken = getCookie("csrftoken"),
     sessionid = getCookie("sessionid"),
     csrfSafeMethod = method => /^(GET|HEAD|OPTIONS|TRACE)$/.test(method);
@@ -32,7 +32,7 @@ const getCookie = function (name) {
 var intervalID;
 const INTERVAL_MS = 60000;
 
-const checkSession = function () {
+const checkSession = function() {
     // if session is about to expire, then display a popup to allow a session refresh
     const SESSION_EXPIRE_WARNING = 900000; // 15 minutes
     let exp_time = $('meta[name="session_expire_time"]').attr("content");
@@ -41,7 +41,7 @@ const checkSession = function () {
     if (exp_time - Date.now() < SESSION_EXPIRE_WARNING) {
         clearInterval(intervalID); // prevents multiple popups stacking on top of each other
         if (confirm("Your session will expire in 15 minutes. Click OK to stay logged in.")) {
-            $.post("/update-session/", { refresh: true }).done(response => {
+            $.post("/update-session/", {refresh: true}).done(response => {
                 $('meta[name="session_expire_time"]').attr("content", response.new_expiry_time);
             });
         }
@@ -49,8 +49,8 @@ const checkSession = function () {
     }
 };
 
-d3.selection.prototype.moveToFront = function () {
-    return this.each(function () {
+d3.selection.prototype.moveToFront = function() {
+    return this.each(function() {
         this.parentNode.appendChild(this);
     });
 };
