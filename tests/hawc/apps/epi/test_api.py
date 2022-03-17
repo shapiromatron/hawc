f8cc3b2f9fe0f0ac8432f9562095376015820a6f
BerkowitzMaxICF <90347602+BerkowitzMaxICF@users.noreply.github.com>
2022-03-17 17:17:05 -0400

    Icf 365 drf add nonstandard change logging (#610)
    * added nonstandard drf logging
    
    * removed unnecessary import
    
    * fixed code formatting
    
    * fixed import order
    
    * add transactions
    
    * added tests
    
    * add log check to bulk modify objects
    
    Co-authored-by: Berkowitz <55586@icf.com>
    Co-authored-by: Andy Shapiro <shapiromatron@gmail.com>

--- tests/hawc/apps/epi/test_api.py
+++ tests/hawc/apps/epi/test_api.py
@@ -7,18 +7,12 @@ from django.urls import reverse
 from django.urls.exceptions import NoReverseMatch
 from rest_framework.test import APIClient
 
-from hawc.apps.assessment.models import Assessment, DoseUnits, Log
+from hawc.apps.assessment.models import Assessment, DoseUnits
 from hawc.apps.epi import constants, models
 
+from ..test_utils import check_details_of_last_log_entry
+
 DATA_ROOT = Path(__file__).parents[3] / "data/api"
-
-
-def check_details_of_last_log_entry(obj_id: int, start_of_msg: str):
-    """
-    retrieve the latest log entry and check that the object_id/message look right.
-    """
-    log_entry = Log.objects.latest("id")
-    assert log_entry.object_id == int(obj_id) and log_entry.message.startswith(start_of_msg)
 
 
 @pytest.mark.django_db
