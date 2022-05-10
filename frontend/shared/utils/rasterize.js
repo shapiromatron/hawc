f865700126413ef2f941f22247687c6961c45882
Andy Shapiro <shapiromatron@gmail.com>
2022-05-10 08:54:33 -0400

    hack - redraw border

--- frontend/shared/utils/rasterize.js
+++ frontend/shared/utils/rasterize.js
@@ -26,6 +26,22 @@ const URL_TEMPLATES = "/rasterize/",
                 cb(svgStr);
             });
     },
+    drawBorder = (ctx, width, height) => {
+        /*
+        Draw a black border around the image, if one was present in the top-left pixel.
+        For some reason, the default image does include the top and left borders, but not the
+        bottom or right borders.
+        */
+        // get top-left pixel and check if RGB is dark; and if so, redraw the border
+        const data = ctx.getImageData(0, 0, 1, 1).data;
+        if (data[0] < 50 && data[1] < 50 && data[2] < 50) {
+            // if so, draw a rectangle. canvas stroke styles are middle-aligned, so to
+            // draw a stroke around the full border, offset by 2.
+            ctx.strokeStyle = "black";
+            ctx.lineWidth = 4;
+            ctx.strokeRect(2, 2, width - 2, height - 2);
+        }
+    },
     toSvg = svgElement => {
         getSvgString(svgElement, svgStr => {
             const blob = new Blob([svgStr], {type: "image/svg+xml"});
@@ -36,20 +52,23 @@ const URL_TEMPLATES = "/rasterize/",
         getSvgString(svgElement, svgStr => {
             const canvas = document.createElement("canvas"),
                 ctx = canvas.getContext("2d"),
-                bb = svgElement.getBoundingClientRect(),
+                bb = svgElement.getBBox(),
                 img = document.createElement("img"),
                 height = Math.ceil(bb.height * 5),
-                width = Math.ceil(bb.width * 5);
+                width = Math.ceil(bb.width * 5),
+                imgData = "data:image/svg+xml;base64," + btoa(unescape(encodeURIComponent(svgStr)));
 
-            canvas.height = height;
-            canvas.width = width;
+            // set canvas size equal to the image plus a few extra pixels for border
+            canvas.height = height + 3;
+            canvas.width = width + 3;
 
-            img.setAttribute(
-                "src",
-                "data:image/svg+xml;base64," + btoa(unescape(encodeURIComponent(svgStr)))
-            );
+            // (debugging only)
+            // $("<div>").attr("class", "p-2 m-2").css("background", "orange").appendTo("#main-content-container").append($(canvas));
+
+            img.setAttribute("src", imgData);
             img.onload = function() {
                 ctx.drawImage(img, 0, 0, width, height);
+                drawBorder(ctx, width, height);
                 canvas.toBlob(
                     blob => downloadBlob(blob, 'attachment; filename="download.png"'),
                     "image/png"
