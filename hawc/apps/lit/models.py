6ae8fd3a84c8d48698df757ce7f0d77bf0a45381
Andy Shapiro <shapiromatron@gmail.com>
2022-07-07 14:07:51 -0400

    update w/ query operations

--- hawc/apps/lit/models.py
+++ hawc/apps/lit/models.py
@@ -407,41 +407,25 @@ class Search(models.Model):
         2. are not associated in any other searches
         3. do not have any tags applied
         """
-        RefSearchM2M = Reference.searches.through
-        
-        logger.debug("Removing untagged references not associated with other searches")
         if results["removed"]:
-            removed_str = [str(id) for id in results["removed"]]
-            removed_ids = (
-                Reference.objects.filter(
-                    assessment=self.assessment, identifiers__unique_id__in=removed_str
-                )
-                .exclude(
-                    referencetags__gt=0
-                )
-                .values_list("pk")
-                .order_by("pk")
+            ids = [str(id) for id in results["removed"]]
+            # filter removed references with no tags
+            no_tags = list(
+                self.references.filter(identifiers__unique_id__in=ids)
+                .annotate(ntags=models.Count("tags"))
+                .filter(ntags=0)
+                .values_list("id", flat=True)
             )
-            ids_count = removed_ids.count()
-            if ids_count > 0:
-                refs_in_other_searches = (
-                    RefSearchM2M.objects.filter(
-                       reference_id__in=removed_ids,   
-                    )               
-                    .exclude(  
-                       search_id = self.pk
-                    )
-                    .values_list("reference_id")
-                    .order_by("reference_id")
-                    .distinct()
-                )
-                comparison = set(removed_ids).difference(set(refs_in_other_searches))
-
-                if comparison.sum() == set():
-                    pass
-                else:
-                    for id in comparison:
-                        Reference.objects.all().filter(id).delete()
+            # filter untagged references with only one search (this one)
+            no_searches = (
+                Reference.objects.filter(id__in=no_tags)
+                .annotate(nsearches=models.Count("searches"))
+                .filter(nsearches=1)
+            )
+            n = no_searches.count()
+            if n > 0:
+                logger.info(f"Removing {n} references from search {self.id}")
+                no_searches.delete()
 
     @property
     def date_last_run(self):
