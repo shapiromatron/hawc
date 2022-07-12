a17ce068ddccacc6ec427e488ca082aee2399a32
Andy Shapiro <shapiromatron@gmail.com>
2022-07-12 13:00:24 -0400

    fix blank text

--- frontend/riskofbias/robStudyForm/Completeness.js
+++ frontend/riskofbias/robStudyForm/Completeness.js
@@ -6,15 +6,21 @@ class Completeness extends Component {
         let display =
             this.props.number > 0 ? (
                 <div className="alert alert-danger">
-                    <p>{this.props.number} note(s) still need input.</p>
-                    <p>
-                        Notes should only be left blank if &quot;Not applicable&quot; judgment is
-                        selected.
-                    </p>
+                    <p className="mb-0">{this.props.number} note(s) still need input.</p>
+                    <ul>
+                        <li>
+                            Notes are required the &quot;Not reported&quot; judgment is selected.
+                        </li>
+                        <li>
+                            It is recommended, but not required, to add notes for all other
+                            judgments (except for &quot;Not applicable&quot; which is
+                            self-explanatory)
+                        </li>
+                    </ul>
                 </div>
             ) : (
                 <div className="alert alert-success">
-                    <p>All items complete! Ready to submit.</p>
+                    <p className="mb-0">All items complete! Ready to submit.</p>
                 </div>
             );
         return display;
