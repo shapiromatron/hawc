3b1c911779f4528bbb9870a44b17b9090c39ce4c
munnsmunns <mmunns16@gmail.com>
2022-07-12 14:45:41 -0400

    lint

--- hawc/apps/epiv2/migrations/0004_alter_adjustmentfactor_description.py
+++ hawc/apps/epiv2/migrations/0004_alter_adjustmentfactor_description.py
@@ -6,13 +6,16 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('epiv2', '0003_auto_20220708_0957'),
+        ("epiv2", "0003_auto_20220708_0957"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='adjustmentfactor',
-            name='description',
-            field=models.CharField(help_text='Enter the list of covariates in the model, separated by commas. These can be brief and ideally entered uniformly across studies when possible. Additional detail can be added in the comments or in study evaluation (e.g., enter "smoking" for consistency instead of "pack-years")', max_length=512),
+            model_name="adjustmentfactor",
+            name="description",
+            field=models.CharField(
+                help_text='Enter the list of covariates in the model, separated by commas. These can be brief and ideally entered uniformly across studies when possible. Additional detail can be added in the comments or in study evaluation (e.g., enter "smoking" for consistency instead of "pack-years")',
+                max_length=512,
+            ),
         ),
     ]
