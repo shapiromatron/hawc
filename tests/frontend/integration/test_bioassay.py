18ccb151c3ee6e8dc4419405180beff6611069ba
munnsmunns <mmunns16@gmail.com>
2022-07-12 14:02:14 -0400

    add bioassay test

--- tests/frontend/integration/test_bioassay.py
+++ tests/frontend/integration/test_bioassay.py
@@ -16,9 +16,7 @@ class TestBioassay(PlaywrightTestCase):
             page, f"{self.live_server_url}/ani/experiment/1/", "pm@hawcproject.org"
         )
         expect(page.locator("text=Multiple generations")).to_be_visible()
-        expect(
-            page.locator('css=#objContainer table >> has-text("2 year bioassay")')
-        ).to_have_count(1)
+        expect(page.locator('css=#objContainer table:has-text("2 year bioassay")')).to_have_count(1)
         expect(page.locator("css=#ag-table tbody tr")).to_have_count(1)
 
         # /animal-group/:id/
@@ -31,8 +29,8 @@ class TestBioassay(PlaywrightTestCase):
 
         # /endpoint/:id/
         page.goto(f"{self.live_server_url}/ani/endpoint/1/")
-        page.expect(page.locator("text=my outcome")).to_be_visible()
-        expect(page.locator("css=svg")).to_be_visible()
+        expect(page.locator("text=my outcome")).not_to_have_count(0)
+        expect(page.locator("css=svg")).not_to_have_count(0)
         expect(page.locator("css=.d3 .dr_dots .dose_points")).to_have_count(3)
-        expect(page.locator("#dr-tbl")).to_be_visible()
+        expect(page.locator("#dr-tbl")).not_to_have_count(0)
         expect(page.locator("#dr-tbl tr")).to_have_count(5)
