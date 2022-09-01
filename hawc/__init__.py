f028a3eecce8f0dbf7e9d43866a07f42be3baec9
Andy Shapiro <shapiromatron@gmail.com>
2022-09-01 08:47:15 -0400

    lint/format

--- hawc/__init__.py
+++ hawc/__init__.py
@@ -7,4 +7,5 @@ __version__ = "0.1"
 def manage():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hawc.main.settings.dev")
     from django.core.management import execute_from_command_line
+
     execute_from_command_line(sys.argv)
