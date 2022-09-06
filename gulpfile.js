27b14e1e0282a920060763e0a79856079095f5b5
casey1173 <caseyhansen1999@gmail.com>
2022-09-06 11:58:05 -0400

    updated header/footer

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+/* gulpfile.js */
+
+const uswds = require("@uswds/compile");
+
+/**
+ * USWDS version
+ * Set the version of USWDS you're using (2 or 3)
+ */
+
+uswds.settings.version = 3;
+
+/**
+ * Path settings
+ * Set as many as you need
+ */
+
+uswds.paths.dist.css = './hawc/static/css';
+uswds.paths.dist.sass = './hawc/static/sass';
+uswds.paths.dist.theme = './hawc/static/sass';
+uswds.paths.dist.img = './hawc/static/vendor/uswds/img';
+uswds.paths.dist.fonts = './hawc/static/vendor/uswds/fonts';
+uswds.paths.dist.css = './hawc/static/vendor/uswds/js';
+uswds.paths.dist.js = './hawc/static/vendor/uswds/css';
+uswds.paths.src.uswds = './node_modules/@uswds';
+uswds.paths.src.sass = './node_modules/@uswds/uswds/packages';
+uswds.paths.src.theme = './node_modules/@uswds/uswds/dist/theme';
+uswds.paths.src.fonts = './node_modules/@uswds/uswds/dist/fonts';
+uswds.paths.src.img = './node_modules/@uswds/uswds/dist/img';
+uswds.paths.src.js = './node_modules/@uswds/uswds/dist/js';
+uswds.paths.src.projectSass = './sass';
+
+/**
+ * Exports
+ * Add as many as you need
+ */
+
+exports.init = uswds.init;
+exports.compile = uswds.compile;
+exports.compileIcons = uswds.compileIcons;
+exports.compileSass = uswds.compileSass;
