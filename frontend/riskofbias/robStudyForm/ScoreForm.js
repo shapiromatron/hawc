207d6154a6bd922f6b9ba1949b093773eb8a2fec
Daniel Rabstejnek <rabstejnek@gmail.com>
2022-09-13 15:44:19 -0400

    Added red border for scores with validation errors on score form

--- frontend/riskofbias/robStudyForm/ScoreForm.js
+++ frontend/riskofbias/robStudyForm/ScoreForm.js
@@ -94,104 +94,109 @@ class ScoreForm extends Component {
             isOverride = score.is_default === false;
 
         return (
-            <div className="score-form container-fluid ">
+            <>
                 {isOverride ? <Spacer borderStyle="4px dashed #323a45" /> : null}
-                <div className="row">
-                    <div className="col-md-3">
-                        {editableMetricHasOverrides ? (
-                            <TextInput
-                                id={`${score.id}-label`}
-                                label="Label"
-                                name={`label-id-${score.id}`}
-                                onChange={e => {
-                                    store.updateScoreState(score, "label", e.target.value);
-                                }}
-                                value={score.label}
-                            />
-                        ) : null}
-                    </div>
-                    <div className="col-md-9">
-                        {showOverrideCreate ? (
-                            <button
-                                className="btn btn-primary float-right"
-                                type="button"
-                                onClick={() => {
-                                    store.createScoreOverride({
-                                        metric: score.metric_id,
-                                        riskofbias: score.riskofbias_id,
-                                    });
-                                }}>
-                                <i className="fa fa-plus"></i>&nbsp;Create new override
-                            </button>
-                        ) : null}
-
-                        {isOverride ? (
-                            <button
-                                className="btn btn-danger float-right"
-                                type="button"
-                                onClick={() => store.deleteScoreOverride(score.id)}>
-                                <i className="fa fa-trash"></i>&nbsp;Delete override
-                            </button>
-                        ) : null}
-
-                        {editableMetricHasOverrides ? (
-                            score.is_default ? (
-                                <b title="Unless otherwise specified, all content will use this value">
-                                    <i className="fa fa-check-square-o" />
-                                    &nbsp;Default judgment
-                                </b>
-                            ) : (
-                                <b title="Only selected override content will use this value">
-                                    <i className="fa fa-square-o" />
-                                    &nbsp;Override judgment
-                                </b>
-                            )
-                        ) : null}
-                    </div>
-                </div>
-                <div className="row">
-                    {showScoreInput ? (
+                <div
+                    className={`score-form container-fluid ${
+                        Object.keys(score.errors).length > 0 ? "border border-danger " : ""
+                    }`}>
+                    <div className="row mt-3">
                         <div className="col-md-3">
-                            <ScoreInput
-                                choices={scoreChoices}
-                                value={score.score}
-                                defaultValue={defaultScoreChoice}
-                                handleChange={value => {
-                                    store.updateScoreState(score, "score", parseInt(value));
+                            {editableMetricHasOverrides ? (
+                                <TextInput
+                                    id={`${score.id}-label`}
+                                    label="Label"
+                                    name={`label-id-${score.id}`}
+                                    onChange={e => {
+                                        store.updateScoreState(score, "label", e.target.value);
+                                    }}
+                                    value={score.label}
+                                />
+                            ) : null}
+                        </div>
+                        <div className="col-md-9">
+                            {showOverrideCreate ? (
+                                <button
+                                    className="btn btn-primary float-right"
+                                    type="button"
+                                    onClick={() => {
+                                        store.createScoreOverride({
+                                            metric: score.metric_id,
+                                            riskofbias: score.riskofbias_id,
+                                        });
+                                    }}>
+                                    <i className="fa fa-plus"></i>&nbsp;Create new override
+                                </button>
+                            ) : null}
+
+                            {isOverride ? (
+                                <button
+                                    className="btn btn-danger float-right"
+                                    type="button"
+                                    onClick={() => store.deleteScoreOverride(score.id)}>
+                                    <i className="fa fa-trash"></i>&nbsp;Delete override
+                                </button>
+                            ) : null}
+
+                            {editableMetricHasOverrides ? (
+                                score.is_default ? (
+                                    <b title="Unless otherwise specified, all content will use this value">
+                                        <i className="fa fa-check-square-o" />
+                                        &nbsp;Default judgment
+                                    </b>
+                                ) : (
+                                    <b title="Only selected override content will use this value">
+                                        <i className="fa fa-square-o" />
+                                        &nbsp;Override judgment
+                                    </b>
+                                )
+                            ) : null}
+                        </div>
+                    </div>
+                    <div className="row">
+                        {showScoreInput ? (
+                            <div className="col-md-3">
+                                <ScoreInput
+                                    choices={scoreChoices}
+                                    value={score.score}
+                                    defaultValue={defaultScoreChoice}
+                                    handleChange={value => {
+                                        store.updateScoreState(score, "score", parseInt(value));
+                                    }}
+                                    errors={score.errors.score}
+                                />
+                                <SelectInput
+                                    id={`${score.id}-direction`}
+                                    label="Bias direction"
+                                    choices={direction_choices}
+                                    multiple={false}
+                                    value={score.bias_direction}
+                                    handleSelect={value => {
+                                        store.updateScoreState(
+                                            score,
+                                            "bias_direction",
+                                            parseInt(value)
+                                        );
+                                    }}
+                                    errors={score.errors.bias_direction}
+                                />
+                            </div>
+                        ) : null}
+                        <div className="col-md-9">
+                            <QuillTextInput
+                                id={`${score.id}-notes`}
+                                className="score-editor"
+                                value={score.notes}
+                                onChange={htmlContent => {
+                                    store.updateScoreState(score, "notes", htmlContent);
                                 }}
-                                errors={score.errors.score}
-                            />
-                            <SelectInput
-                                id={`${score.id}-direction`}
-                                label="Bias direction"
-                                choices={direction_choices}
-                                multiple={false}
-                                value={score.bias_direction}
-                                handleSelect={value => {
-                                    store.updateScoreState(
-                                        score,
-                                        "bias_direction",
-                                        parseInt(value)
-                                    );
-                                }}
-                                errors={score.errors.bias_direction}
+                                errors={score.errors.notes}
                             />
                         </div>
-                    ) : null}
-                    <div className="col-md-9">
-                        <QuillTextInput
-                            id={`${score.id}-notes`}
-                            className="score-editor"
-                            value={score.notes}
-                            onChange={htmlContent => {
-                                store.updateScoreState(score, "notes", htmlContent);
-                            }}
-                            errors={score.errors.notes}
-                        />
                     </div>
+                    {score.is_default ? null : <ScoreOverrideForm score={score} />}
                 </div>
-                {score.is_default ? null : <ScoreOverrideForm score={score} />}
-            </div>
+            </>
         );
     }
 }
