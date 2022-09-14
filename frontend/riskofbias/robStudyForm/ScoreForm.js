e6d8158d090d9d4de5bc8c54baf72b3ddc95b5fe
Daniel Rabstejnek <rabstejnek@gmail.com>
2022-09-14 14:44:19 -0400

    Fix js linting error

--- frontend/riskofbias/robStudyForm/ScoreForm.js
+++ frontend/riskofbias/robStudyForm/ScoreForm.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
-import ReactQuill from "react-quill";
 import {observer, inject} from "mobx-react";
 
 import ScoreIcon from "riskofbias/robTable/components/ScoreIcon";
