bf7dbb45270e0a5ec9aa9524444a869b3a77f8f7
Andy Shapiro <shapiromatron@gmail.com>
2022-10-20 12:28:22 -0400

    add ability to download JPEG

--- frontend/shared/utils/rasterize.js
+++ frontend/shared/utils/rasterize.js
@@ -53,7 +53,7 @@ const URL_TEMPLATES = "/rasterize/",
             downloadBlob(blob, 'attachment; filename="download.svg"');
         });
     },
-    toPng = svgElement => {
+    toRaster = (svgElement, mime) => {
         getSvgString(svgElement, svgStr => {
             const canvas = document.createElement("canvas"),
                 ctx = canvas.getContext("2d"),
@@ -61,7 +61,8 @@ const URL_TEMPLATES = "/rasterize/",
                 img = document.createElement("img"),
                 height = Math.ceil(bb.height * 5),
                 width = Math.ceil(bb.width * 5),
-                imgData = "data:image/svg+xml;base64," + btoa(unescape(encodeURIComponent(svgStr)));
+                imgData = "data:image/svg+xml;base64," + btoa(unescape(encodeURIComponent(svgStr))),
+                extension = mime.slice(mime.match("/").index + 1);
 
             // set canvas size equal to the image plus a few extra pixels for border
             canvas.height = height + 3;
@@ -75,8 +76,9 @@ const URL_TEMPLATES = "/rasterize/",
                 ctx.drawImage(img, 0, 0, width, height);
                 drawBorder(ctx, width, height);
                 canvas.toBlob(
-                    blob => downloadBlob(blob, 'attachment; filename="download.png"'),
-                    "image/png"
+                    blob => downloadBlob(blob, `attachment; filename="download.${extension}"`),
+                    mime,
+                    0.95
                 );
             };
         });
@@ -87,7 +89,10 @@ const URL_TEMPLATES = "/rasterize/",
                 toSvg(svgElement);
                 break;
             case "png":
-                toPng(svgElement);
+                toRaster(svgElement, "image/png");
+                break;
+            case "jpeg":
+                toRaster(svgElement, "image/jpeg");
                 break;
             default:
                 throw `Invalid format ${format}`;
