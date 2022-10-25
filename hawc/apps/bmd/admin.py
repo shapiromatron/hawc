3d5a8b48a032416442381dd33b65418b04d2429f
Andy Shapiro <shapiromatron@gmail.com>
2022-10-25 13:41:42 -0400

    rewrite migration pattern

--- hawc/apps/bmd/admin.py
+++ hawc/apps/bmd/admin.py
@@ -6,7 +6,7 @@ from . import models
 
 @admin.register(models.AssessmentSettings)
 class AssessmentSettingsAdmin(AllListFieldAdmin):
-    pass
+    list_filter = ("version",)
 
 
 @admin.register(models.LogicField)
