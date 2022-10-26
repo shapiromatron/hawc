3842bc585e0d90ace84bcf4545e3d338e514fcf7
munnsmunns <mmunns16@gmail.com>
2022-10-26 17:22:43 -0400

    revert unused changes

--- hawc/apps/lit/views.py
+++ hawc/apps/lit/views.py
@@ -276,11 +276,6 @@ class TagReferences(WebappMixin, TeamMemberOrHigherMixin, FormView):
             ),
         )
 
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs["user"] = self.request.user
-        return kwargs
-
 
 class TagBySearch(TagReferences):
     """
