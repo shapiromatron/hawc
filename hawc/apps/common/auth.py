d517cbfd3617e39b466c39c5fd87f11d38a258cf
Andy Shapiro <shapiromatron@gmail.com>
2023-01-04 14:54:28 -0500

    only query db if a token value is provided

--- hawc/apps/common/auth.py
+++ hawc/apps/common/auth.py
@@ -6,20 +6,21 @@ from rest_framework.authtoken.models import Token
 class TokenBackend(ModelBackend):
     keyword = "Token"
 
-    def get_token_from_header(self, request):
+    def get_token_from_header(self, request) -> str:
         auth = get_authorization_header(request).split()
-        if not auth or auth[0].lower() != self.keyword.lower().encode() or len(auth) != 2:
-            return
-        try:
-            token = auth[1].decode()
-        except UnicodeError:
-            return
-
+        token = ""
+        if auth and auth[0].lower() == self.keyword.lower().encode() and len(auth) == 2:
+            try:
+                token = auth[1].decode()
+            except UnicodeError:
+                pass
         return token
 
     def authenticate(self, request, token=None, **kwargs):
         if request is not None and token is None:
             token = self.get_token_from_header(request)
+        if not token:
+            return None
         try:
             token_obj = Token.objects.select_related("user").get(key=token)
         except Token.DoesNotExist:
