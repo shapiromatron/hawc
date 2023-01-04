39bab0c2dd5753dec891de4ecc294fe0f23add21
Andy Shapiro <shapiromatron@gmail.com>
2023-01-04 17:59:55 -0500

    lint

--- hawc/apps/myuser/api.py
+++ hawc/apps/myuser/api.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import login
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
