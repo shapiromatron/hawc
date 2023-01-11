ce2bf93268549ef8c451212416774be5d23e26de
Daniel Rabstejnek <rabstejnek@gmail.com>
2023-01-11 11:03:45 -0500

    Update epimeta permissions

--- hawc/apps/epimeta/api.py
+++ hawc/apps/epimeta/api.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 
 from ..assessment.api import AssessmentLevelPermissions, AssessmentViewset
 from ..assessment.models import Assessment
+from ..common.constants import AssessmentViewsetPermissions
 from ..common.helper import re_digits
 from ..common.renderers import PandasRenderers
 from ..common.serializers import UnusedSerializer
@@ -11,25 +12,32 @@ from . import exports, models, serializers
 
 
 class EpiMetaAssessmentViewset(viewsets.GenericViewSet):
-    parent_model = Assessment
-    model = models.MetaResult
+    model = Assessment
+    queryset = Assessment.objects.all()
     permission_classes = (AssessmentLevelPermissions,)
+    action_perms = {}
     serializer_class = UnusedSerializer
     lookup_value_regex = re_digits
 
-    def get_queryset(self):
-        perms = self.get_obj_perms()
+    def get_meta_result_queryset(self):
+        perms = self.assessment.user_permissions(self.request.user)
         if not perms["edit"]:
-            return self.model.objects.published(self.assessment)
-        return self.model.objects.get_qs(self.assessment)
+            return models.MetaResult.objects.published(self.assessment)
+        return models.MetaResult.objects.get_qs(self.assessment)
 
-    @action(detail=True, url_path="export", renderer_classes=PandasRenderers)
+    @action(
+        detail=True,
+        url_path="export",
+        action_perms=AssessmentViewsetPermissions.CAN_VIEW_OBJECT,
+        renderer_classes=PandasRenderers,
+    )
     def export(self, request, pk):
         """
         Retrieve epidemiology metadata for assessment.
         """
+        self.get_object()
         exporter = exports.MetaResultFlatComplete(
-            self.get_queryset(), filename=f"{self.assessment}-epi-meta"
+            self.get_meta_result_queryset(), filename=f"{self.assessment}-epi-meta"
         )
         return Response(exporter.build_export())
 
