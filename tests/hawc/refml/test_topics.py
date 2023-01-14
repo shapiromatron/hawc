4e15d317473261243588c867937a05075a68daad
Andy Shapiro <shapiromatron@gmail.com>
2023-01-14 00:35:10 -0500

    fix shape

--- tests/hawc/refml/test_topics.py
+++ tests/hawc/refml/test_topics.py
@@ -70,7 +70,7 @@ class TestTopicModelTsne:
         ).to_frame()
         df, df_topics = topics.topic_model_tsne(input_df)
 
-        assert df.shape == (12, 3)
+        assert df.shape == (23, 3)
         assert set(df.columns.tolist()) == {"max_topic", "tsne_x", "tsne_y"}
 
         assert df_topics.shape == (10, 2)
