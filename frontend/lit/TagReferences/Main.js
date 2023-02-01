867cde2de7762d9620cb3aebc95c5ad3d6d9d062
casey1173 <caseyhansen1999@gmail.com>
2023-02-01 10:42:49 -0500

    js lint

--- frontend/lit/TagReferences/Main.js
+++ frontend/lit/TagReferences/Main.js
@@ -16,7 +16,9 @@ class ReferenceListItem extends Component {
     render() {
         const {store, reference, selectedReferencePk} = this.props,
             isSelected = reference.data.pk === selectedReferencePk,
-            divClass = `d-flex justify-content-between align-items-center reference ${isSelected ? "selected" : ""}`,
+            divClass = `d-flex justify-content-between align-items-center reference ${
+                isSelected ? "selected" : ""
+            }`,
             title = store.config.conflict_resolution ? "has consensus tag(s)" : "tagged";
 
         return (
@@ -24,10 +26,16 @@ class ReferenceListItem extends Component {
                 <p className="mb-0 pr-1">{reference.shortCitation()}</p>
                 <div className="mr-1 d-flex" style={{"flex-wrap": "nowrap"}}>
                     {reference.tags.length > 0 ? (
-                        <i className="fa fa-fw fa-tags small-tag-icon consensus mr-1" title={title} aria-hidden="true"></i>
+                        <i
+                            className="fa fa-fw fa-tags small-tag-icon consensus mr-1"
+                            title={title}
+                            aria-hidden="true"></i>
                     ) : null}
                     {reference.userTags.length > 0 ? (
-                        <i className="fa fa-fw fa-tags small-tag-icon user" title={"has tags from you"} aria-hidden="true"></i>
+                        <i
+                            className="fa fa-fw fa-tags small-tag-icon user"
+                            title={"has tags from you"}
+                            aria-hidden="true"></i>
                     ) : null}
                 </div>
             </div>
