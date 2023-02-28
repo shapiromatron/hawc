4b69bb5f51a7c3ebd4edb08b8307068b604b1db5
munnsmunns <mmunns16@gmail.com>
2023-02-28 17:14:17 -0500

    lint

--- hawc/apps/assessment/migrations/0035_auto_20230227_1555.py
+++ hawc/apps/assessment/migrations/0035_auto_20230227_1555.py
@@ -18,35 +18,49 @@ def get_species_name(apps, schema_editor):
         except Exception:
             continue
 
-    AssessmentValue.objects.bulk_update(updates, ['species_studied'])
+    AssessmentValue.objects.bulk_update(updates, ["species_studied"])
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('assessment', '0034_assessmentdetail_assessmentvalue'),
+        ("assessment", "0034_assessmentdetail_assessmentvalue"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='assessmentvalue',
-            name='non_adaf_value',
+            model_name="assessmentvalue",
+            name="non_adaf_value",
         ),
         migrations.AlterField(
-            model_name='assessmentvalue',
-            name='adaf',
-            field=models.BooleanField(default=False, help_text='When checked, the ADAF note will appear as a footnote for the value', verbose_name='Apply ADAF?'),
+            model_name="assessmentvalue",
+            name="adaf",
+            field=models.BooleanField(
+                default=False,
+                help_text="When checked, the ADAF note will appear as a footnote for the value",
+                verbose_name="Apply ADAF?",
+            ),
         ),
         migrations.AlterField(
-            model_name='assessmentvalue',
-            name='duration',
-            field=models.CharField(blank=True, help_text='Duration associated with the value (e.g., Chronic, Subchronic)', max_length=128, verbose_name='Value duration'),
+            model_name="assessmentvalue",
+            name="duration",
+            field=models.CharField(
+                blank=True,
+                help_text="Duration associated with the value (e.g., Chronic, Subchronic)",
+                max_length=128,
+                verbose_name="Value duration",
+            ),
         ),
         migrations.AlterField(
-            model_name='assessmentvalue',
-            name='species_studied',
-            field=models.TextField(blank=True, default='', help_text='Provide information about the animal(s) studied, including species and strain information', verbose_name='Species and strain studied'),
+            model_name="assessmentvalue",
+            name="species_studied",
+            field=models.TextField(
+                blank=True,
+                default="",
+                help_text="Provide information about the animal(s) studied, including species and strain information",
+                verbose_name="Species and strain studied",
+            ),
             preserve_default=False,
         ),
-        migrations.RunPython(get_species_name, migrations.RunPython.noop)
+        migrations.RunPython(get_species_name, migrations.RunPython.noop),
     ]
