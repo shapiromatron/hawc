af11d4ace5f06ac8d7e898395f6ec01d8c64dcc4
Andy Shapiro <shapiromatron@gmail.com>
2023-03-01 18:26:01 -0500

    fix height of tagtree

--- frontend/lit/components/TagTree.js
+++ frontend/lit/components/TagTree.js
@@ -43,7 +43,7 @@ class TagNode extends Component {
                         ) : null}
                     </div>
                     <div
-                        className={(showTagHoverAdd ? "tagHoverAdd" : "tagHoverSimple").concat(
+                        className={(showTagHoverAdd ? "tagHoverAdd" : "tagHover").concat(
                             tag === selectedTag ? " tagSelected" : ""
                         )}
                         style={{flex: 1}}>
@@ -93,9 +93,10 @@ class TagTree extends Component {
             untaggedCount,
             showTagHoverAdd,
             untaggedReferencesSelected,
+            style,
         } = this.props;
         return (
-            <div id="litTagtree" className="resize-y p-2 mt-2">
+            <div id="litTagtree" className="resize-y p-2 mt-2" style={style}>
                 {tagtree.rootNode.children.map((tag, i) => (
                     <TagNode
                         key={i}
@@ -108,9 +109,9 @@ class TagTree extends Component {
                 ))}
                 {untaggedHandleClick ? (
                     <p
-                        className={"nestedTag mt-2 tagHoverSimple".concat(
-                            untaggedReferencesSelected === true ? " tagSelected" : ""
-                        )}
+                        className={`nestedTag mt-2 tagHover ${
+                            untaggedReferencesSelected ? "tagSelected" : ""
+                        }`}
                         onClick={untaggedHandleClick}>
                         Untagged References:
                         <span className="ml-2 badge badge-dark">{untaggedCount}</span>
@@ -129,12 +130,14 @@ TagTree.propTypes = {
     untaggedHandleClick: PropTypes.func,
     showTagHoverAdd: PropTypes.bool,
     untaggedReferencesSelected: PropTypes.bool,
+    style: PropTypes.object,
 };
 TagTree.defaultProps = {
     showReferenceCount: false,
     handleTagClick: h.noop,
     showTagHoverAdd: false,
     untaggedReferencesSelected: false,
+    style: {},
 };
 
 export default TagTree;
