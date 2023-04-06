shapiromatron/hawc

commit efaa660119fdc436539ed6570ebee5d034f91b9e

refactor assessment permissions in django views and API (#732)
* remove custom mixins in favor of AssessmentPermissionsMixin

* wip todo stubs

* Flesh out DRF assessment permission class, fix server breaking errors

* Further seperated assessment api into seperate modules

* Fix bug with OPTIONS requests

* Clean up remaining merge conflicts

* Fix circular import in CI

* Update animal permissions

* Update assessment permissions

* Update bmd permissions

* Update epi permissions

* Update epimeta permissions

* Update epiv2 permissions

* Update invitro permissions

* Updated lit permissions

* Updated mgmt permissions

* Updated rob permissions

* Updated study permissions

* Updated summary permissions

* Fix tests; can_edit_object permissions should apply to reference tag import

* Check object permissions on rob_settings

* Moved DisabledPagination to common app

* Refactoring, added documentation

* fix error

* updates from review

* rename Viewset to ViewSet

* rename a few permission methods

* move methods around

* cleanup

* undo comment

* review views.py

* remove unused methods

* updatesr

* fix bug

---------

Co-authored-by: Daniel Rabstejnek <rabstejnek@gmail.com>

hawc/apps/mgmt/api.py
@@ -1,10 +1,10 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from ..assessment.api import AssessmentEditViewset, AssessmentLevelPermissions, DisabledPagination
-from ..assessment.models import Assessment
+from ..assessment.api import AssessmentEditViewset, AssessmentLevelPermissions
+from ..assessment.constants import AssessmentViewSetPermissions
+from ..common.api import DisabledPagination
 from . import models, serializers
 
 
@@ -12,17 +12,18 @@ class TaskViewSet(AssessmentEditViewset):
     http_method_names = ["get", "patch", "head", "options", "trace"]
     assessment_filter_args = "study__assessment"
     model = models.Task
+    list_actions = ["list", "assessment_assignments"]
     serializer_class = serializers.TaskSerializer
     permission_classes = (
-        AssessmentLevelPermissions,
         permissions.IsAuthenticated,
+        AssessmentLevelPermissions,
     )
     pagination_class = DisabledPagination
 
     def get_queryset(self):
         return super().get_queryset().select_related("owner", "study", "study__assessment")
 
-    @action(detail=False)
+    @action(detail=False, permission_classes=(permissions.IsAuthenticated,))
     def assignments(self, request):
         # Tasks assigned to user.
         qs = self.model.objects.owned_by(request.user).select_related(
@@ -31,13 +32,12 @@ class TaskViewSet(AssessmentEditViewset):
         serializer = serializers.TaskByAssessmentSerializer(qs, many=True)
         return Response(serializer.data)
 
-    @action(detail=True, methods=["get"])
-    def assessment_assignments(self, request, pk=None):
+    @action(detail=False, action_perms=AssessmentViewSetPermissions.CAN_VIEW_OBJECT)
+    def assessment_assignments(self, request):
         # Tasks assigned to user for a specific assessment
-        assessment = get_object_or_404(Assessment, pk=pk)
         qs = (
             self.model.objects.owned_by(request.user)
-            .filter(study__assessment=assessment)
+            .filter(study__assessment=self.assessment)
             .select_related("owner", "study", "study__reference_ptr", "study__assessment")
         )
         serializer = serializers.TaskByAssessmentSerializer(qs, many=True)

hawc/apps/mgmt/views.py
@@ -1,13 +1,13 @@
 from django.apps import apps
 from django.middleware.csrf import get_token
-from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views.generic import ListView
 
+from ..assessment.constants import AssessmentViewPermissions
 from ..assessment.models import Assessment
 from ..common.crumbs import Breadcrumb
 from ..common.helper import WebappConfig
-from ..common.views import BaseList, LoginRequiredMixin, TeamMemberOrHigherMixin, WebappMixin
+from ..common.views import BaseList, LoginRequiredMixin, WebappMixin
 from ..study.serializers import StudyAssessmentSerializer
 from . import models
 
@@ -64,7 +64,7 @@ class RobTaskMixin:
     def get_app_config(self, context) -> WebappConfig:
         assessment_id = self.assessment.id if hasattr(self, "assessment") else None
         task_url = (
-            reverse("mgmt:api:task-assessment-assignments", args=(assessment_id,))
+            reverse("mgmt:api:task-assessment-assignments")
             if assessment_id
             else reverse("mgmt:api:task-assignments")
         )
@@ -110,7 +110,9 @@ class UserAssessmentAssignments(RobTaskMixin, LoginRequiredMixin, BaseList):
 
     def get_queryset(self):
         return (
-            self.model.objects.owned_by(self.request.user)
+            super()
+            .get_queryset()
+            .owned_by(self.request.user)
             .filter(study__assessment=self.assessment)
             .select_related("owner", "study", "study__reference_ptr", "study__assessment")
         )
@@ -128,16 +130,14 @@ class UserAssessmentAssignments(RobTaskMixin, LoginRequiredMixin, BaseList):
 
 
 # Assessment-level task views
-class TaskDashboard(TeamMemberOrHigherMixin, BaseList):
+class TaskDashboard(BaseList):
     parent_model = Assessment
     model = models.Task
     template_name = "mgmt/assessment_dashboard.html"
-
-    def get_assessment(self, *args, **kwargs):
-        return get_object_or_404(Assessment, pk=kwargs["pk"])
+    assessment_permission = AssessmentViewPermissions.TEAM_MEMBER
 
     def get_queryset(self):
-        return self.model.objects.assessment_qs(self.assessment.id)
+        return super().get_queryset().filter(study__assessment_id=self.assessment.id)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)

hawc/apps/animal/api.py
@@ -10,69 +10,75 @@ from rest_framework.response import Response
 from ..assessment.api import (
     AssessmentLevelPermissions,
     AssessmentViewset,
-    DoseUnitsViewset,
-    get_assessment_from_query,
-    get_assessment_id_param,
-)
-from ..assessment.models import Assessment
-from ..common.api import (
     CleanupFieldsBaseViewSet,
-    LegacyAssessmentAdapterMixin,
-    user_can_edit_object,
+    DoseUnitsViewset,
 )
+from ..assessment.constants import AssessmentViewSetPermissions
 from ..common.helper import FlatExport, re_digits
 from ..common.renderers import PandasRenderers
 from ..common.serializers import HeatmapQuerySerializer, UnusedSerializer
-from ..common.views import AssessmentPermissionsMixin, create_object_log
+from ..common.views import create_object_log
 from . import exports, models, serializers
 from .actions.model_metadata import AnimalMetadata
 from .actions.term_check import term_check
 
 
-class AnimalAssessmentViewset(
-    AssessmentPermissionsMixin, LegacyAssessmentAdapterMixin, viewsets.GenericViewSet
-):
-    parent_model = Assessment
-    model = models.Endpoint
+class AnimalAssessmentViewset(viewsets.GenericViewSet):
+    model = models.Assessment
+    queryset = models.Assessment.objects.all()
     permission_classes = (AssessmentLevelPermissions,)
+    action_perms = {}
     serializer_class = UnusedSerializer
     lookup_value_regex = re_digits
 
-    def get_queryset(self):
-        perms = self.get_obj_perms()
+    def get_endpoint_queryset(self):
+        perms = self.assessment.user_permissions(self.request.user)
         if not perms["edit"]:
-            return self.model.objects.published(self.assessment)
-        return self.model.objects.get_qs(self.assessment)
+            return models.Endpoint.objects.published(self.assessment)
+        return models.Endpoint.objects.get_qs(self.assessment)
 
-    @action(detail=True, url_path="full-export", renderer_classes=PandasRenderers)
+    @action(
+        detail=True,
+        url_path="full-export",
+        action_perms=AssessmentViewSetPermissions.CAN_VIEW_OBJECT,
+        renderer_classes=PandasRenderers,
+    )
     def full_export(self, request, pk):
         """
         Retrieve complete animal data
         """
-        self.set_legacy_attr(pk)
-        self.permission_check_user_can_view()
+        self.assessment = self.get_object()
         exporter = exports.EndpointGroupFlatComplete(
-            self.get_queryset(),
+            self.get_endpoint_queryset(),
             filename=f"{self.assessment}-bioassay-complete",
             assessment=self.assessment,
         )
         return Response(exporter.build_export())
 
-    @action(detail=True, url_path="endpoint-export", renderer_classes=PandasRenderers)
+    @action(
+        detail=True,
+        url_path="endpoint-export",
+        action_perms=AssessmentViewSetPermissions.CAN_VIEW_OBJECT,
+        renderer_classes=PandasRenderers,
+    )
     def endpoint_export(self, request, pk):
         """
         Retrieve endpoint animal data
         """
-        self.set_legacy_attr(pk)
-        self.permission_check_user_can_view()
+        self.assessment = self.get_object()
         exporter = exports.EndpointSummary(
-            self.get_queryset(),
+            self.get_endpoint_queryset(),
             filename=f"{self.assessment}-bioassay-summary",
             assessment=self.assessment,
         )
         return Response(exporter.build_export())
 
-    @action(detail=True, url_path="study-heatmap", renderer_classes=PandasRenderers)
+    @action(
+        detail=True,
+        url_path="study-heatmap",
+        action_perms=AssessmentViewSetPermissions.CAN_VIEW_OBJECT,
+        renderer_classes=PandasRenderers,
+    )
     def study_heatmap(self, request, pk):
         """
         Return heatmap data for assessment, at the study-level (one row per study).
@@ -80,12 +86,11 @@ class AnimalAssessmentViewset(
         By default only shows data from published studies. If the query param `unpublished=true`
         is present then results from all studies are shown.
         """
-        self.set_legacy_attr(pk)
-        self.permission_check_user_can_view()
+        self.assessment = self.get_object()
         ser = HeatmapQuerySerializer(data=request.query_params)
         ser.is_valid(raise_exception=True)
         unpublished = ser.data["unpublished"]
-        if unpublished and not self.assessment.user_is_part_of_team(self.request.user):
+        if unpublished and not self.assessment.user_is_reviewer_or_higher(self.request.user):
             raise PermissionDenied("You must be part of the team to view unpublished data")
         key = f"assessment-{self.assessment.id}-bioassay-study-heatmap-pub-{unpublished}"
         df = cache.get(key)
@@ -95,7 +100,12 @@ class AnimalAssessmentViewset(
         export = FlatExport(df=df, filename=f"bio-study-heatmap-{self.assessment.id}")
         return Response(export)
 
-    @action(detail=True, url_path="endpoint-heatmap", renderer_classes=PandasRenderers)
+    @action(
+        detail=True,
+        url_path="endpoint-heatmap",
+        action_perms=AssessmentViewSetPermissions.CAN_VIEW_OBJECT,
+        renderer_classes=PandasRenderers,
+    )
     def endpoint_heatmap(self, request, pk):
         """
         Return heatmap data for assessment, at the endpoint level (one row per endpoint).
@@ -103,12 +113,11 @@ class AnimalAssessmentViewset(
         By default only shows data from published studies. If the query param `unpublished=true`
         is present then results from all studies are shown.
         """
-        self.set_legacy_attr(pk)
-        self.permission_check_user_can_view()
+        self.assessment = self.get_object()
         ser = HeatmapQuerySerializer(data=request.query_params)
         ser.is_valid(raise_exception=True)
         unpublished = ser.data["unpublished"]
-        if unpublished and not self.assessment.user_is_part_of_team(self.request.user):
+        if unpublished and not self.assessment.user_is_reviewer_or_higher(self.request.user):
             raise PermissionDenied("You must be part of the team to view unpublished data")
         key = f"assessment-{self.assessment.id}-bioassay-endpoint-heatmap-unpublished-{unpublished}"
         df = cache.get(key)
@@ -118,7 +127,12 @@ class AnimalAssessmentViewset(
         export = FlatExport(df=df, filename=f"bio-endpoint-heatmap-{self.assessment.id}")
         return Response(export)
 
-    @action(detail=True, url_path="endpoint-doses-heatmap", renderer_classes=PandasRenderers)
+    @action(
+        detail=True,
+        url_path="endpoint-doses-heatmap",
+        action_perms=AssessmentViewSetPermissions.CAN_VIEW_OBJECT,
+        renderer_classes=PandasRenderers,
+    )
     def endpoint_doses_heatmap(self, request, pk):
         """
         Return heatmap data with doses for assessment, at the {endpoint + dose unit} level.
@@ -126,12 +140,11 @@ class AnimalAssessmentViewset(
         By default only shows data from published studies. If the query param `unpublished=true`
         is present then results from all studies are shown.
         """
-        self.set_legacy_attr(pk)
-        self.permission_check_user_can_view()
+        self.assessment = self.get_object()
         ser = HeatmapQuerySerializer(data=request.query_params)
         ser.is_valid(raise_exception=True)
         unpublished = ser.data["unpublished"]
-        if unpublished and not self.assessment.user_is_part_of_team(self.request.user):
+        if unpublished and not self.assessment.user_is_reviewer_or_higher(self.request.user):
             raise PermissionDenied("You must be part of the team to view unpublished data")
         key = f"assessment-{self.assessment.id}-bioassay-endpoint-doses-heatmap-unpublished-{unpublished}"
         df = cache.get(key)
@@ -141,14 +154,17 @@ class AnimalAssessmentViewset(
         export = FlatExport(df=df, filename=f"bio-endpoint-doses-heatmap-{self.assessment.id}")
         return Response(export)
 
-    @action(detail=True, renderer_classes=PandasRenderers)
+    @action(
+        detail=True,
+        action_perms=AssessmentViewSetPermissions.CAN_VIEW_OBJECT,
+        renderer_classes=PandasRenderers,
+    )
     def endpoints(self, request, pk):
-        self.set_legacy_attr(pk)
-        self.permission_check_user_can_view()
+        self.assessment = self.get_object()
         ser = HeatmapQuerySerializer(data=request.query_params)
         ser.is_valid(raise_exception=True)
         unpublished = ser.data["unpublished"]
-        if unpublished and not self.assessment.user_is_part_of_team(self.request.user):
+        if unpublished and not self.assessment.user_is_reviewer_or_higher(self.request.user):
             raise PermissionDenied("You must be part of the team to view unpublished data")
         key = f"assessment-{self.assessment.id}-bioassay-endpoint-list"
         df = cache.get(key)
@@ -160,10 +176,14 @@ class AnimalAssessmentViewset(
         export = FlatExport(df=df, filename=f"bio-endpoint-list-{self.assessment.id}")
         return Response(export)
 
-    @action(detail=True, url_path="ehv-check", renderer_classes=PandasRenderers)
+    @action(
+        detail=True,
+        url_path="ehv-check",
+        action_perms=AssessmentViewSetPermissions.TEAM_MEMBER_OR_HIGHER,
+        renderer_classes=PandasRenderers,
+    )
     def ehv_check(self, request, pk):
-        self.set_legacy_attr(pk)
-        self.permission_check_user_can_edit()
+        _ = self.get_object()
         df = term_check(pk)
         export = FlatExport(df, f"term-report-{pk}")
         return Response(export)
@@ -173,6 +193,7 @@ class Experiment(mixins.CreateModelMixin, AssessmentViewset):
     assessment_filter_args = "study__assessment"
     model = models.Experiment
     serializer_class = serializers.ExperimentSerializer
+    permission_classes = (AssessmentLevelPermissions,)
 
     def get_queryset(self):
         return (
@@ -184,8 +205,6 @@ class Experiment(mixins.CreateModelMixin, AssessmentViewset):
 
     @transaction.atomic
     def perform_create(self, serializer):
-        # permissions check
-        user_can_edit_object(serializer.study, self.request.user, raise_exception=True)
         super().perform_create(serializer)
         create_object_log(
             "Created",
@@ -199,6 +218,7 @@ class AnimalGroup(mixins.CreateModelMixin, AssessmentViewset):
     assessment_filter_args = "experiment__study__assessment"
     model = models.AnimalGroup
     serializer_class = serializers.AnimalGroupSerializer
+    permission_classes = (AssessmentLevelPermissions,)
 
     @transaction.atomic
     def create(self, request, *args, **kwargs):
@@ -241,7 +261,8 @@ class Endpoint(mixins.CreateModelMixin, AssessmentViewset):
     assessment_filter_args = "assessment"
     model = models.Endpoint
     serializer_class = serializers.EndpointSerializer
-    list_actions = ["list", "effects", "rob_filter"]
+    list_actions = ["list", "effects", "rob_filter", "update_terms"]
+    permission_classes = (AssessmentLevelPermissions,)
 
     def get_queryset(self):
         return self.model.objects.optimized_qs()
@@ -256,19 +277,17 @@ class Endpoint(mixins.CreateModelMixin, AssessmentViewset):
             self.request.user.id,
         )
 
-    @action(detail=False)
+    @action(detail=False, action_perms=AssessmentViewSetPermissions.CAN_VIEW_OBJECT)
     def effects(self, request):
-        assessment_id = get_assessment_id_param(self.request)
-        effects = models.Endpoint.objects.get_effects(assessment_id)
+        effects = models.Endpoint.objects.get_effects(self.assessment.id)
         return Response(effects)
 
-    @action(detail=False)
+    @action(detail=False, action_perms=AssessmentViewSetPermissions.CAN_VIEW_OBJECT)
     def rob_filter(self, request):
 
         params = request.query_params
-        assessment_id = get_assessment_id_param(request)
 
-        query = Q(assessment_id=assessment_id)
+        query = Q(assessment=self.assessment)
 
         effects = params.get("effect[]")
         if effects:
@@ -286,16 +305,14 @@ class Endpoint(mixins.CreateModelMixin, AssessmentViewset):
         serializer = self.get_serializer(qs, many=True)
         return Response(serializer.data)
 
-    @action(detail=False, methods=("post",))
+    @action(
+        detail=False, methods=("post",), action_perms=AssessmentViewSetPermissions.CAN_EDIT_OBJECT
+    )
     def update_terms(self, request):
-        # check assessment level permissions
-        assessment = get_assessment_from_query(request)
-        if not assessment.user_can_edit_object(request.user):
-            self.permission_denied(request)
         # update endpoint terms (all other validation done in manager)
-        updated_endpoints = self.model.objects.update_terms(request.data, assessment)
+        updated_endpoints = self.model.objects.update_terms(request.data, self.assessment)
         serializer = serializers.EndpointSerializer(updated_endpoints, many=True)
-        assessment.bust_cache()
+        self.assessment.bust_cache()
         return Response(serializer.data)
 
 

hawc/apps/animal/views.py
@@ -440,8 +440,8 @@ class EndpointListV2(BaseList):
 class EndpointTags(EndpointFilterList):
     # List of Endpoints associated with an assessment and tag
 
-    def get_base_queryset(self):
-        return self.model.objects.tag_qs(self.assessment.pk, self.kwargs["tag_slug"])
+    def get_queryset(self):
+        return super().get_queryset().tag_qs(self.assessment.pk, self.kwargs["tag_slug"])
 
 
 class EndpointRead(BaseDetail):

hawc/apps/assessment/views.py
@@ -41,8 +41,6 @@ from ..common.views import (
     CloseIfSuccessMixin,
     LoginRequiredMixin,
     MessageMixin,
-    ProjectManagerOrHigherMixin,
-    TeamMemberOrHigherMixin,
     TimeSpentOnPageMixin,
     beta_tester_required,
     create_object_log,
@@ -392,11 +390,11 @@ class AssessmentRead(BaseDetail):
     model = models.Assessment
 
     def get_queryset(self):
-        qs = super().get_queryset()
-        qs = qs.prefetch_related(
-            "project_manager", "team_members", "reviewers", "datasets", "dtxsids"
+        return (
+            super()
+            .get_queryset()
+            .prefetch_related("project_manager", "team_members", "reviewers", "datasets", "dtxsids")
         )
-        return qs
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -419,18 +417,21 @@ class AssessmentUpdate(BaseUpdate):
     success_message = "Assessment updated."
     model = models.Assessment
     form_class = forms.AssessmentForm
+    assessment_permission = constants.AssessmentViewPermissions.PROJECT_MANAGER
 
 
 class AssessmentModulesUpdate(AssessmentUpdate):
     success_message = "Assessment modules updated."
     form_class = forms.AssessmentModulesForm
     template_name = "assessment/assessment_module_form.html"
+    assessment_permission = constants.AssessmentViewPermissions.PROJECT_MANAGER
 
 
 class AssessmentDelete(BaseDelete):
     model = models.Assessment
     success_url = reverse_lazy("portal")
     success_message = "Assessment deleted."
+    assessment_permission = constants.AssessmentViewPermissions.PROJECT_MANAGER
 
 
 class AssessmentClearCache(MessageMixin, View):
@@ -600,19 +601,13 @@ class BaseEndpointList(BaseList):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
         eps = self.model.endpoint.related.related_model.objects.get_qs(self.assessment.id).count()
-
         os = self.model.outcome.related.related_model.objects.get_qs(self.assessment.id).count()
-
         mrs = apps.get_model("epimeta", "metaresult").objects.get_qs(self.assessment.id).count()
-
         iveps = self.model.ivendpoint.related.related_model.objects.get_qs(
             self.assessment.id
         ).count()
-
         alleps = eps + os + mrs + iveps
-
         context.update(
             {
                 "ivendpoints": iveps,
@@ -622,11 +617,10 @@ class BaseEndpointList(BaseList):
                 "total_endpoints": alleps,
             }
         )
-
         return context
 
 
-class CleanExtractedData(TeamMemberOrHigherMixin, BaseEndpointList):
+class CleanExtractedData(BaseEndpointList):
     """
     To add a model to clean,
      - add TEXT_CLEANUP_FIELDS = {...fields} to the model
@@ -640,9 +634,7 @@ class CleanExtractedData(TeamMemberOrHigherMixin, BaseEndpointList):
 
     breadcrumb_active_name = "Clean extracted data"
     template_name = "assessment/clean_extracted_data.html"
-
-    def get_assessment(self, request, *args, **kwargs):
-        return get_object_or_404(self.parent_model, pk=kwargs["pk"])
+    assessment_permission = constants.AssessmentViewPermissions.TEAM_MEMBER
 
     def get_app_config(self, context) -> WebappConfig:
         return WebappConfig(
@@ -696,13 +688,11 @@ class RasterizeCss(View):
         return JsonResponse({"template": get_styles_svg_definition()})
 
 
-class CleanStudyRoB(ProjectManagerOrHigherMixin, BaseDetail):
+class CleanStudyRoB(BaseDetail):
     template_name = "assessment/clean_study_rob_scores.html"
     model = models.Assessment
     breadcrumb_active_name = "Clean reviews"
-
-    def get_assessment(self, request, *args, **kwargs):
-        return get_object_or_404(self.model, pk=kwargs["pk"])
+    assessment_permission = constants.AssessmentViewPermissions.PROJECT_MANAGER
 
     def get_app_config(self, context) -> WebappConfig:
         return WebappConfig(
@@ -811,20 +801,20 @@ class LogObjectList(ListView):
         return context
 
 
-class AssessmentLogList(TeamMemberOrHigherMixin, BaseList):
+class AssessmentLogList(BaseList):
     parent_model = models.Assessment
     model = models.Log
     breadcrumb_active_name = "Logs"
     template_name = "assessment/assessment_log_list.html"
     paginate_by = 25
-
-    def get_assessment(self, request, *args, **kwargs):
-        return get_object_or_404(models.Assessment, pk=kwargs["pk"])
+    assessment_permission = constants.AssessmentViewPermissions.TEAM_MEMBER
 
     def get_queryset(self):
-        qs = super().get_queryset()
-        qs = qs.filter(assessment=self.assessment).select_related(
-            "assessment", "content_type", "user"
+        qs = (
+            super()
+            .get_queryset()
+            .filter(assessment=self.assessment)
+            .select_related("assessment", "content_type", "user")
         )
         self.form = forms.LogFilterForm(self.request.GET, assessment=self.assessment)
         if self.form.is_valid():

hawc/apps/bmd/api.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from ..assessment.api import AssessmentViewset
+from ..assessment.constants import AssessmentViewSetPermissions
 from . import models, serializers, tasks
 
 
@@ -18,7 +19,9 @@ class Session(AssessmentViewset):
         else:
             return serializers.SessionSerializer
 
-    @action(detail=True, methods=["post"])
+    @action(
+        detail=True, methods=["post"], action_perms=AssessmentViewSetPermissions.CAN_EDIT_OBJECT
+    )
     def execute(self, request, pk=None):
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data)
@@ -27,13 +30,15 @@ class Session(AssessmentViewset):
         tasks.execute.delay(instance.id)
         return Response({"started": True})
 
-    @action(detail=True, methods=["get"])
+    @action(detail=True, methods=["get"], action_perms=AssessmentViewSetPermissions.CAN_VIEW_OBJECT)
     def execute_status(self, request, pk=None):
         # ping until execution is complete
         session = self.get_object()
         return Response({"finished": session.is_finished})
 
-    @action(detail=True, methods=("post",))
+    @action(
+        detail=True, methods=("post",), action_perms=AssessmentViewSetPermissions.CAN_EDIT_OBJECT
+    )
     def selected_model(self, request, pk=None):
         session = self.get_object()
         serializer = self.get_serializer(data=request.data, context={"session": session})

commit cdda8bc768d177f00b8f9ed159fe5596166410ba

fix another regression from #732

hawc/apps/mgmt/api.py
@@ -26,9 +26,9 @@ class TaskViewSet(AssessmentEditViewset):
     @action(detail=False, permission_classes=(permissions.IsAuthenticated,))
     def assignments(self, request):
         # Tasks assigned to user.
-        qs = self.model.objects.owned_by(request.user).select_related(
+        qs = self.model.objects.select_related(
             "owner", "study", "study__reference_ptr", "study__assessment"
-        )
+        ).owned_by(request.user)
         serializer = serializers.TaskByAssessmentSerializer(qs, many=True)
         return Response(serializer.data)
 
@@ -36,8 +36,8 @@ class TaskViewSet(AssessmentEditViewset):
     def assessment_assignments(self, request):
         # Tasks assigned to user for a specific assessment
         qs = (
-            self.model.objects.owned_by(request.user)
-            .filter(study__assessment=self.assessment)
+            self.model.objects.filter(study__assessment=self.assessment)
+            .owned_by(request.user)
             .select_related("owner", "study", "study__reference_ptr", "study__assessment")
         )
         serializer = serializers.TaskByAssessmentSerializer(qs, many=True)

hawc/apps/mgmt/managers.py
@@ -1,5 +1,7 @@
 import logging
 
+from django.db.models import QuerySet
+
 from ..common.models import BaseManager
 from ..study.models import Study
 from . import constants
@@ -7,11 +9,16 @@ from . import constants
 logger = logging.getLogger(__name__)
 
 
+class TaskQuerySet(QuerySet):
+    def owned_by(self, user):
+        return self.filter(owner=user)
+
+
 class TaskManager(BaseManager):
     assessment_relation = "study__assessment"
 
-    def owned_by(self, user):
-        return self.filter(owner=user)
+    def get_queryset(self):
+        return TaskQuerySet(self.model, using=self._db)
 
     def create_assessment_tasks(self, assessment):
         """

hawc/apps/mgmt/views.py
@@ -92,7 +92,7 @@ class UserAssignments(RobTaskMixin, WebappMixin, LoginRequiredMixin, ListView):
     template_name = "mgmt/user_assignments.html"
 
     def get_queryset(self):
-        return self.model.objects.owned_by(self.request.user)
+        return self.model.objects.all().owned_by(self.request.user)
 
     def get_rob_queryset(self, RiskOfBias):
         return RiskOfBias.objects.filter(author=self.request.user, active=True)
@@ -112,8 +112,8 @@ class UserAssessmentAssignments(RobTaskMixin, LoginRequiredMixin, BaseList):
         return (
             super()
             .get_queryset()
-            .owned_by(self.request.user)
             .filter(study__assessment=self.assessment)
+            .owned_by(self.request.user)
             .select_related("owner", "study", "study__reference_ptr", "study__assessment")
         )
 