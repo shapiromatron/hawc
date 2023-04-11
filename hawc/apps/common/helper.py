afea5e2f01b6e4787c911c1ce324133e5ca30ded
Andy Shapiro <shapiromatron@gmail.com>
2023-04-10 21:38:36 -0400

    update full text URL endpoint

--- hawc/apps/common/helper.py
+++ hawc/apps/common/helper.py
@@ -76,10 +76,6 @@ def strip_entities(value):
     return re.sub(r"&(?:\w+|#\d+);", "", force_str(value))
 
 
-def listToUl(list_):
-    return f"<ul>{''.join(['<li>{0}</li>'.format(d) for d in list_])}</ul>"
-
-
 def tryParseInt(
     value: Any, default: int = None, min_value: int = -inf, max_value: int = inf
 ) -> Optional[int]:
