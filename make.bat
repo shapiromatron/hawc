0def8d37de9b4f09e531186b365758e6d94c0500
Andy Shapiro <shapiromatron@gmail.com>
2023-04-18 16:33:14 -0400

    show fixes when linting

--- make.bat
+++ make.bat
@@ -75,7 +75,7 @@ npm --prefix .\frontend run lint
 goto :eof
 
 :format
-black . && ruff . --fix
+black . && ruff . --fix --show-fixes
 npm --prefix .\frontend run format
 goto :eof
 
@@ -84,7 +84,7 @@ black . --check && ruff .
 goto :eof
 
 :format-py
-black . && ruff . --fix
+black . && ruff . --fix --show-fixes
 goto :eof
 
 :lint-js
