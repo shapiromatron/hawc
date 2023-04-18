651d4de7ab42d3d2fb05c208ee8b81d8168e8cba
munnsmunns <mmunns16@gmail.com>
2023-04-18 12:16:41 -0400

    refactor and add docstring

--- client/hawc_client/summary.py
+++ client/hawc_client/summary.py
@@ -54,60 +54,65 @@ class SummaryClient(BaseClient):
         response_json = self.session.get(url).json()
         return pd.DataFrame(response_json)
 
-    def download_visuals(self, assessment_id: int, dest_str: str, file_type: str = "docx"):
-        def _save_to_pngs(data: pd.DataFrame, page: Page, dest: pathlib.Path):
+    def download_visuals(
+        self, assessment_id: int, dest: str = ".", file_type: str = "docx", headless: bool = True
+    ):
+        """
+        Retrieves all visuals for the given assessment and saves them in the given path.
+        Session must be authenticated with a token to access private assessment visuals.
+
+        Args:
+            assessment_id (int): Assessment ID
+            dest (str): Folder where images will be saved. Defaults to current working directory.
+            file_type (str): 'docx' will save all images to a Word document in the dest folder.
+                'png' will save all images separately as pngs in the dest folder. Defaults to 'docx'
+            headless (bool): If False, will display the browser while downloading images. Defaults
+                to True.
+
+            Returns:
+                None
+        """
+
+        def _prepare_download(page: Page, row):
+            page.goto(row.full_url)
+
+            if row.visual_type == "embedded external website":
+                # assume embedded external website is tableau
+                download_button = page.frame_locator("iframe").locator(
+                    'div[role="button"]:has-text("Download")'
+                )
+                download_confirm = page.frame_locator("iframe").locator('button:has-text("Image")')
+            else:
+                download_button = page.locator("button", has=page.locator("i.fa-download"))
+                download_confirm = page.locator("text=Download as a PNG")
+
+            download_button.click()
+
+            return download_confirm
+
+        def _save_to_pngs(data: pd.DataFrame, page: Page, dest_path: pathlib.Path):
             for row in data[["slug", "full_url", "visual_type"]].itertuples():
                 try:
-                    page.goto(row.full_url)
-
-                    if row.visual_type == "embedded external website":
-                        # assume embedded external website is tableau
-                        download_button = page.frame_locator("iframe").locator(
-                            'div[role="button"]:has-text("Download")'
-                        )
-                        download_confirm = page.frame_locator("iframe").locator(
-                            'button:has-text("Image")'
-                        )
-                    else:
-                        download_button = page.locator("button", has=page.locator("i.fa-download"))
-                        download_confirm = page.locator("text=Download as a PNG")
-
-                    download_button.click()
-
+                    download_confirm = _prepare_download(page, row)
                     # Start waiting for the download
                     with page.expect_download() as download_info:
                         download_confirm.click()
                         download = download_info.value
                         # Save downloaded file
-                        download.save_as(str(dest.joinpath(f"{row.slug}.png")))
+                        download.save_as(str(dest_path.joinpath(f"{row.slug}.png")))
                 except Exception as e:
                     # create a txt file with error info
-                    with open(dest.joinpath(f"{row.slug}-error.txt"), "w") as f:
+                    with open(dest_path.joinpath(f"{row.slug}-error.txt"), "w") as f:
                         f.write(f"There was an error getting the visual named {row.slug}.\n{e}")
 
-        def _save_to_docx(data: pd.DataFrame, page: Page, dest: pathlib.Path):
+        def _save_to_docx(data: pd.DataFrame, page: Page, dest_path: pathlib.Path):
             doc = Document()
             for row in data[["slug", "full_url", "visual_type"]].itertuples():
                 # path for tmp image file
-                path = dest.joinpath(f"tmp/{row.slug}.png")
+                path = dest_path.joinpath(f"tmp/{row.slug}.png")
 
                 try:
-                    page.goto(row.full_url)
-
-                    if row.visual_type == "embedded external website":
-                        # assume embedded external website is tableau
-                        download_button = page.frame_locator("iframe").locator(
-                            'div[role="button"]:has-text("Download")'
-                        )
-                        download_confirm = page.frame_locator("iframe").locator(
-                            'button:has-text("Image")'
-                        )
-                    else:
-                        download_button = page.locator("button", has=page.locator("i.fa-download"))
-                        download_confirm = page.locator("text=Download as a PNG")
-
-                    download_button.click()
-
+                    download_confirm = _prepare_download(page, row)
                     # Start waiting for the download
                     with page.expect_download() as download_info:
                         download_confirm.click()
@@ -125,37 +130,38 @@ class SummaryClient(BaseClient):
                 # remove tmp file if created
                 path.unlink(missing_ok=True)
 
-            doc.save(dest.joinpath(f"assessment-{assessment_id}-visuals.docx"))
+            doc.save(dest_path.joinpath(f"assessment-{assessment_id}-visuals.docx"))
             # remove tmp folder
-            dest.joinpath("tmp").rmdir()
+            dest_path.joinpath("tmp").rmdir()
 
         valid_file_types = ["png", "docx"]
 
         if file_type not in valid_file_types:
             raise ValueError(f"Valid file types are {valid_file_types}")
 
-        dest = pathlib.Path(dest_str)
+        dest_path = pathlib.Path(dest)
         visuals = self.visual_list(assessment_id)
         dp = self.datapivot_list(assessment_id)
         data = pd.concat([visuals, dp])
-        base_url = self.session.root_url
+        root_url = self.session.root_url
 
         if data.shape[0] == 0:
             raise ValueError("No visuals found for given assessment id")
 
-        data.loc[:, "full_url"] = base_url + data.url
+        data.loc[:, "full_url"] = root_url + data.url
 
         with sync_playwright() as p:
-            browser = p.chromium.launch(headless=False)
+            browser = p.chromium.launch(headless=headless)
             page = browser.new_page()
+
+            # Give playwright authorization to view private pages
             token = self.session._session.headers.get("Authorization")
             if token:
                 page.set_extra_http_headers({"Authorization": str(token)})
-                page.goto(f"{base_url}/user/api/validate-token/?login=1")
-                page.set_extra_http_headers({})
+                page.goto(f"{root_url}/user/api/validate-token/?login=1")
 
             if file_type == "docx":
-                _save_to_docx(data, page, dest)
+                _save_to_docx(data, page, dest_path)
             elif file_type == "png":
-                _save_to_pngs(data, page, dest)
+                _save_to_pngs(data, page, dest_path)
             browser.close()
