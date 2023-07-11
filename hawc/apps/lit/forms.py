9470c6e5b9d42f7823444746045b6b42bb66a006
Andy Shapiro <shapiromatron@gmail.com>
2023-07-11 11:54:47 -0400

    update help text on form

--- hawc/apps/lit/forms.py
+++ hawc/apps/lit/forms.py
@@ -152,7 +152,10 @@ class ImportForm(SearchForm):
         if self.instance.id:
             inputs = {
                 "legend_text": f"Update {self.instance}",
-                "help_text": "Update an existing literature search",
+                "help_text": """
+                    Update an existing literature import. After a literature import
+                    has been created, you can no longer edit the source or IDs to import.
+                """,
                 "cancel_url": self.instance.get_absolute_url(),
             }
         else:
