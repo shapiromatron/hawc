641e1fe12c4a17ef48bd287605c63569c4f8355a
Danny Peterson <dannypeterson28@gmail.com>
2023-07-12 13:23:17 -0400

    fixed tests + made symbols easier to see

--- hawc/apps/riskofbias/templatetags/riskofbias.py
+++ hawc/apps/riskofbias/templatetags/riskofbias.py
@@ -5,9 +5,13 @@ from ..constants import SCORE_CHOICES_MAP, SCORE_SHADES, SCORE_SYMBOLS
 
 register = template.Library()
 
+DARK_SHADES = {'#CC3333', "#00441b"}
 
 @register.simple_tag()
 def score_option(id: int) -> str:
+    background = SCORE_SHADES[id]
+    color = 'white' if background in DARK_SHADES else 'auto'
     return mark_safe(
-        f'<li class="my-2"><span class="d-inline-block py-1 mr-2 text-center" style="width: 50px; background: {SCORE_SHADES[id]}">{SCORE_SYMBOLS[id]}</span>{SCORE_CHOICES_MAP[id]}</li>'
+        f'<li class="my-2"><span class="d-inline-block py-1 mr-2 text-center" style="width: 50px; color: {color}; background: {background}">{SCORE_SYMBOLS[id]}</span>{SCORE_CHOICES_MAP[id]}</li>'
     )
+
