d6a91aaea70b7e342bb7ce9bdb41f8eadb8b868f
Andy Shapiro <shapiromatron@gmail.com>
2023-07-25 15:53:34 -0400

    update from review

--- hawc/apps/assessment/views.py
+++ hawc/apps/assessment/views.py
@@ -695,22 +695,16 @@ class BaseEndpointList(BaseList):
         iveps = self.model.ivendpoint.related.related_model.objects.get_qs(
             self.assessment.id
         ).count()
-        epiv2_designs = apps.get_model("epiv2", "Design").objects.get_qs(self.assessment.id).count()
         epiv2_outcomes = (
             apps.get_model("epiv2", "Outcome").objects.get_qs(self.assessment.id).count()
         )
-        epiv2_data_extractions = (
-            apps.get_model("epiv2", "DataExtraction").objects.get_qs(self.assessment.id).count()
-        )
-        alleps = eps + os + mrs + iveps + epiv2_data_extractions
+        alleps = eps + os + mrs + iveps + epiv2_outcomes
         context.update(
             {
                 "ivendpoints": iveps,
                 "endpoints": eps,
                 "outcomes": os,
-                "epiv2_designs": epiv2_designs,
                 "epiv2_outcomes": epiv2_outcomes,
-                "epiv2_data_extractions": epiv2_data_extractions,
                 "meta_results": mrs,
                 "total_endpoints": alleps,
             }
