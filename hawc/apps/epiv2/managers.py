7fbfae61bd494881ba0cf1e21a084def0c1bdb8a
Danny Peterson <dannypeterson28@gmail.com>
2023-07-31 11:05:00 -0400

    added complete_df to each qs in managers.py

--- hawc/apps/epiv2/managers.py
+++ hawc/apps/epiv2/managers.py
@@ -37,6 +37,32 @@ class DesignQuerySet(QuerySet):
             ),
         )
 
+    def complete_df(self) -> pd.DataFrame:
+        mapping = {
+            "design-id": "design__pk",
+            "design-url": "design__get_absolute_url",
+            "design-summary": "design__summary",
+            "design-study_name": "design__study_name",
+            "design-study_design": "design__study_design",
+            "design-source": "design__source",
+            "design-age_profile": "design__age_profile",
+            "design-age_description": "design__age_description",
+            "design-sex": "design__sex",
+            "design-race": "design__race",
+            "design-participant_n": "design__participant_n",
+            "design-years_enrolled": "design__years_enrolled",
+            "design-years_followup": "design__years_followup",
+            "design-countries": "design__countries",
+            "design-region": "design__region",
+            "design-criteria": "design__criteria",
+            "design-susceptibility": "design__susceptibility",
+            "design-comments": "design__comments",
+            "design-created": "design__created",
+            "design-last_updated": "design__last_updated",
+        }
+        qs = None
+        return pd.DataFrame(data=qs, columns=list(mapping.keys()))
+
 
 class DesignManager(BaseManager):
     assessment_relation = "study__assessment"
@@ -126,7 +152,16 @@ class DesignManager(BaseManager):
 
 
 class ChemicalQuerySet(QuerySet):
-    pass
+    def complete_df(self) -> pd.DataFrame:
+        mapping = {
+            "chemical-pk": "chemical__pk",
+            "chemical-name": "chemical__name",
+            "chemical-DTSXID": "chemical__dsstox__dtxsid",
+            "chemical-created": "chemical__created",
+            "chemical-last_updated": "chemical__last_updated",
+        }
+        qs = None
+        return pd.DataFrame(data=qs, columns=list(mapping.keys()))
 
 
 class ChemicalManager(BaseManager):
@@ -137,7 +172,22 @@ class ChemicalManager(BaseManager):
 
 
 class ExposureQuerySet(QuerySet):
-    pass
+    def complete_df(self) -> pd.DataFrame:
+        mapping = {
+            "exposure-pk": "exposure__pk",
+            "exposure-name": "exposure__name",
+            "exposure-measurement_type": "exposure__measurement_type",
+            "exposure-biomonitoring_matrix": "exposure__biomonitoring_matrix",
+            "exposure-biomonitoring_source": "exposure__biomonitoring_source",
+            "exposure-measurement_timing": "exposure__measurement_timing",
+            "exposure-exposure_route": "exposure__exposure_route",
+            "exposure-measurement_method": "exposure__measurement_method",
+            "exposure-comments": "exposure__comments",
+            "exposure-created": "exposure__created",
+            "exposure-last_updated": "exposure__last_updated",
+        }
+        qs = None
+        return pd.DataFrame(data=qs, columns=list(mapping.keys()))
 
 
 class ExposureManager(BaseManager):
@@ -148,7 +198,29 @@ class ExposureManager(BaseManager):
 
 
 class ExposureLevelQuerySet(QuerySet):
-    pass
+    def complete_df(self) -> pd.DataFrame:
+        mapping = {
+            "exposure_level-pk": "exposure_level__pk",
+            "exposure_level-name": "exposure_level__name",
+            "exposure_level-sub_population": "exposure_level__sub_population",
+            "exposure_level-median": "exposure_level__median",
+            "exposure_level-mean": "exposure_level__mean",
+            "exposure_level-variance": "exposure_level__variance",
+            "exposure_level-variance_type": "exposure_level__variance_type",
+            "exposure_level-units": "exposure_level__units",
+            "exposure_level-ci_lcl": "exposure_level__ci_lcl",
+            "exposure_level-percentile_25": "exposure_level__percentile_25",
+            "exposure_level-percentile_75": "exposure_level__percentile_75",
+            "exposure_level-ci_ucl": "exposure_level__ci_ucl",
+            "exposure_level-ci_type": "exposure_level__ci_type",
+            "exposure_level-negligible_exposure": "exposure_level__negligible_exposure",
+            "exposure_level-data_location": "exposure_level__data_location",
+            "exposure_level-comments": "exposure_level__comments",
+            "exposure_level-created": "exposure_level__created",
+            "exposure_level-last_updated": "exposure_level__last_updated",
+        }
+        qs = None
+        return pd.DataFrame(data=qs, columns=list(mapping.keys()))
 
 
 class ExposureLevelManager(BaseManager):
@@ -159,7 +231,18 @@ class ExposureLevelManager(BaseManager):
 
 
 class OutcomeQuerySet(QuerySet):
-    pass
+    def complete_df(self) -> pd.DataFrame:
+        mapping = {
+            "outcome-pk": "outcome__pk",
+            "outcome-system": "outcome__system",
+            "outcome-effect": "outcome__effect",
+            "outcome-effect_detail": "outcome__effect_detail",
+            "outcome-comments": "outcome__comments",
+            "outcome-created": "outcome__created",
+            "outcome-last_updated": "outcome__last_updated",
+        }
+        qs = None
+        return pd.DataFrame(data=qs, columns=list(mapping.keys()))
 
 
 class OutcomeManager(BaseManager):
@@ -170,7 +253,17 @@ class OutcomeManager(BaseManager):
 
 
 class AdjustmentFactorQuerySet(QuerySet):
-    pass
+    def complete_df(self) -> pd.DataFrame:
+        mapping = {
+            "adjustment_factor-pk": "adjustment_factor__pk",
+            "adjustment_factor-name": "adjustment_factor__name",
+            "adjustment_factor-description": "adjustment_factor__description",
+            "adjustment_factor-comments": "adjustment_factor__comments",
+            "adjustment_factor-created": "adjustment_factor__created",
+            "adjustment_factor-last_updated": "adjustment_factor__last_updated",
+        }
+        qs = None
+        return pd.DataFrame(data=qs, columns=list(mapping.keys()))
 
 
 class AdjustmentFactorManager(BaseManager):
@@ -192,6 +285,37 @@ class DataExtractionQuerySet(QuerySet):
             "outcome",
             "factors",
         ).prefetch_related("design__countries")
+
+    def complete_df(self) -> pd.DataFrame:
+        mapping = {
+            "data_extraction-pk": "data_extraction__pk",
+            "data_extraction-sub_population": "data_extraction__sub_population",
+            "data_extraction-outcome_measurement_timing": "data_extraction__outcome_measurement_timing",
+            "data_extraction-effect_estimate_type": "data_extraction__effect_estimate_type",
+            "data_extraction-effect_estimate": "data_extraction__effect_estimate",
+            "data_extraction-ci_lcl": "data_extraction__ci_lcl",
+            "data_extraction-ci_ucl": "data_extraction__ci_ucl",
+            "data_extraction-ci_type": "data_extraction__ci_type",
+            "data_extraction-units": "data_extraction__units",
+            "data_extraction-variance_type": "data_extraction__variance_type",
+            "data_extraction-variance": "data_extraction__variance",
+            "data_extraction-n": "data_extraction__n",
+            "data_extraction-p_value": "data_extraction__p_value",
+            "data_extraction-significant": "data_extraction__significant",
+            "data_extraction-group": "data_extraction__group",
+            "data_extraction-exposure_rank": "data_extraction__exposure_rank",
+            "data_extraction-exposure_transform": "data_extraction__exposure_transform",
+            "data_extraction-outcome_transform": "data_extraction__outcome_transform",
+            "data_extraction-confidence": "data_extraction__confidence",
+            "data_extraction-data_location": "data_extraction__data_location",
+            "data_extraction-effect_description": "data_extraction__effect_description",
+            "data_extraction-statistical_method": "data_extraction__statistical_method",
+            "data_extraction-comments": "data_extraction__comments",
+            "data_extraction-created": "data_extraction__created",
+            "data_extraction-last_updated": "data_extraction__last_updated",
+        }
+        qs = None
+        return pd.DataFrame(data=qs, columns=list(mapping.keys()))
 
 
 class DataExtractionManager(BaseManager):
