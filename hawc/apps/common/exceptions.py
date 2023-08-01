151aaf33629f0f3aadb4a69b021048fe9fd3cdf7
Andy Shapiro <shapiromatron@gmail.com>
2023-07-31 23:36:30 -0400

    use built-in django exception instead of custom

--- hawc/apps/common/exceptions.py
+++
@@ -1,6 +0,0 @@
-class ClassConfigurationException(Exception):
-    """
-    Raised when a programmer has misconfigured something; should not occur in normal application lifecycle.
-    """
-
-    pass
