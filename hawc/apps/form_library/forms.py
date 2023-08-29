249b7515bc2679ddebd918aaf2f05233bdb6f49b
munnsmunns <mmunns16@gmail.com>
2023-08-29 15:00:38 -0400

    improve form layout

--- hawc/apps/form_library/forms.py
+++ hawc/apps/form_library/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from django.urls import reverse
 
 from hawc.apps.common.dynamic_forms.schemas import Schema
-from hawc.apps.common.forms import PydanticValidator
+from hawc.apps.common.forms import BaseFormHelper, PydanticValidator
 
 from .models import CustomDataExtraction
 
@@ -15,3 +16,14 @@ class CustomDataExtractionForm(forms.ModelForm):
     class Meta:
         model = CustomDataExtraction
         exclude = ("created", "last_updated")
+
+    @property
+    def helper(self):
+        cancel_url = reverse("portal")  # TODO: replace temp cancel url
+        helper = BaseFormHelper(
+            self,
+            legend_text="Create a custom data extraction form",
+            cancel_url=cancel_url,
+            submit_text="Create Form",
+        )
+        return helper
