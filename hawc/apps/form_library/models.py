34aab55238c251d64f499427669b9c521769a361
munnsmunns <mmunns16@gmail.com>
2023-08-31 10:57:08 -0400

    add more fields from review

--- hawc/apps/form_library/models.py
+++ hawc/apps/form_library/models.py
@@ -1,15 +1,28 @@
 import reversion
 from django.db import models
 
+from ..myuser.models import HAWCUser
+
 
 # TODO: is this a good name? i changed it from dynamic form because it makes the naming scheme weird
 # for the actual form class (DynamicFormForm)
 class CustomDataExtraction(models.Model):
-    name = models.CharField()
-    description = models.TextField()
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
     schema = models.JSONField()
     created = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
+    creator = models.ForeignKey(
+        HAWCUser, on_delete=models.SET_NULL, null=True, related_name="created_forms"
+    )
+    editors = models.ManyToManyField(HAWCUser, blank=True, related_name="editable_forms")
+    parent_form = models.ForeignKey(
+        "form_library.CustomDataExtraction",
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="child_forms",
+    )
 
 
 reversion.register(CustomDataExtraction)
