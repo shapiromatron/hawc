300736ca05c2d8a3ff696a951cbf6535999491e2
munnsmunns <mmunns16@gmail.com>
2023-09-12 12:18:46 -0400

    fix urls

--- hawc/apps/form_library/urls.py
+++ hawc/apps/form_library/urls.py
@@ -1,13 +1,18 @@
+from django.conf import settings
 from django.urls import path
 
 from . import views
 
 app_name = "form_library"
-urlpatterns = [
-    # Create a user defined form
-    path(
-        "create/",
-        views.CreateUDFView.as_view(),
-        name="form_create",
-    ),
-]
+urlpatterns = (
+    [
+        # Create a user defined form
+        path(
+            "create/",
+            views.CreateUDFView.as_view(),
+            name="form_create",
+        ),
+    ]
+    if settings.HAWC_FEATURES.ENABLE_DYNAMIC_FORMS
+    else []
+)
