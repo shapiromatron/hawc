de319a86d6f66aeb2e0a6064cc5d6a3deee1dd63
Andy Shapiro <shapiromatron@gmail.com>
2023-09-15 17:44:29 -0400

    add show/hide toggles

--- hawc/apps/summary/prefilters.py
+++ hawc/apps/summary/prefilters.py
@@ -27,7 +27,12 @@ class PrefilterForm(forms.Form):
         return helper
 
 
-class BioassayEndpointPrefilter(BaseFilterSet):
+class PrefilterBase(BaseFilterSet):
+    def noop(self, queryset, name, value):
+        return queryset
+
+
+class BioassayEndpointPrefilter(PrefilterBase):
     # studies
     published_only = df.BooleanFilter(
         method="filter_published_only",
@@ -35,47 +40,89 @@ class BioassayEndpointPrefilter(BaseFilterSet):
         label="Published studies only",
         help_text='Only present data from studies which have been marked as "published" in HAWC.',
     )
+    cb_studies = df.BooleanFilter(
+        method="noop",
+        widget=CheckboxInput(attrs={"data-pf": "studies"}),
+        label="Prefilter by study",
+        help_text="Prefilter endpoints to include only selected studies.",
+    )
     studies = df.MultipleChoiceFilter(
         field_name="animal_group__experiment__study",
         label="Studies to include",
         help_text="Select one or more studies to include in the plot.",
     )
     # bioassay
+    cb_systems = df.BooleanFilter(
+        method="noop",
+        widget=CheckboxInput(attrs={"data-pf": "systems"}),
+        label="Prefilter by system",
+        help_text="Prefilter endpoints to include only selected systems.",
+    )
     systems = df.MultipleChoiceFilter(
         field_name="system",
         label="Systems to include",
         help_text="Select one or more systems to include in the plot.",
+    )
+    cb_organs = df.BooleanFilter(
+        method="noop",
+        widget=CheckboxInput(attrs={"data-pf": "organs"}),
+        label="Prefilter by organ",
+        help_text="Prefilter endpoints to include only selected organs.",
     )
     organs = df.MultipleChoiceFilter(
         field_name="organ",
         label="Organs to include",
         help_text="Select one or more organs to include in the plot.",
     )
+    cb_effects = df.BooleanFilter(
+        method="noop",
+        widget=CheckboxInput(attrs={"data-pf": "effects"}),
+        label="Prefilter by effect",
+        help_text="Prefilter endpoints to include only selected effects.",
+    )
     effects = df.MultipleChoiceFilter(
         field_name="effect",
         label="Effects to include",
         help_text="Select one or more effects to include in the plot.",
+    )
+    cb_effect_subtypes = df.BooleanFilter(
+        method="noop",
+        widget=CheckboxInput(attrs={"data-pf": "effect_subtypes"}),
+        label="Prefilter by effect subtype",
+        help_text="Prefilter endpoints to include only selected effect subtypes.",
     )
     effect_subtypes = df.MultipleChoiceFilter(
         field_name="effect_subtype",
         label="Effect Sub-Types to include",
         help_text="Select one or more effect sub-types to include in the plot.",
     )
+    cb_effect_tags = df.BooleanFilter(
+        method="noop",
+        widget=CheckboxInput(attrs={"data-pf": "effect_tags"}),
+        label="Prefilter by effect tag",
+        help_text="Prefilter endpoints to include only selected effect tags.",
+    )
     effect_tags = df.MultipleChoiceFilter(
         field_name="effects",
         label="Tags to include",
-        help_text="Select one or more effect-tags to include in the plot.",
+        help_text="Select one or more effect tags to include in the plot.",
     )
 
     class Meta:
         model = Endpoint
         fields = [
             "published_only",
+            "cb_studies",
             "studies",
+            "cb_systems",
             "systems",
+            "cb_organs",
             "organs",
+            "cb_effects",
             "effects",
+            "cb_effect_subtypes",
             "effect_subtypes",
+            "cb_effect_tags",
             "effect_tags",
         ]
         form = PrefilterForm
@@ -105,7 +152,7 @@ class BioassayEndpointPrefilter(BaseFilterSet):
         form.fields["effect_tags"].choices = EffectTag.objects.get_choices(self.assessment.pk)
 
 
-class EpiV1ResultPrefilter(BaseFilterSet):
+class EpiV1ResultPrefilter(PrefilterBase):
     # studies
     published_only = df.BooleanFilter(
         method="filter_published_only",
@@ -113,35 +160,63 @@ class EpiV1ResultPrefilter(BaseFilterSet):
         label="Published studies only",
         help_text='Only present data from studies which have been marked as "published" in HAWC.',
     )
+    cb_studies = df.BooleanFilter(
+        method="noop",
+        widget=CheckboxInput(attrs={"data-pf": "studies"}),
+        label="Prefilter by study",
+        help_text="Prefilter endpoints to include only selected studies.",
+    )
     studies = df.MultipleChoiceFilter(
         field_name="study_population__study",
         label="Studies to include",
         help_text="Select one or more studies to include in the plot.",
     )
     # epi
+    cb_systems = df.BooleanFilter(
+        method="noop",
+        widget=CheckboxInput(attrs={"data-pf": "systems"}),
+        label="Prefilter by system",
+        help_text="Prefilter endpoints to include only selected systems.",
+    )
     systems = df.MultipleChoiceFilter(
         field_name="system",
         label="Systems to include",
         help_text="Select one or more systems to include in the plot.",
+    )
+    cb_effects = df.BooleanFilter(
+        method="noop",
+        widget=CheckboxInput(attrs={"data-pf": "effects"}),
+        label="Prefilter by effect",
+        help_text="Prefilter endpoints to include only selected effects.",
     )
     effects = df.MultipleChoiceFilter(
         field_name="effect",
         label="Effects to include",
         help_text="Select one or more effects to include in the plot.",
     )
+    cb_effect_tags = df.BooleanFilter(
+        method="noop",
+        widget=CheckboxInput(attrs={"data-pf": "effect_tags"}),
+        label="Prefilter by effect tag",
+        help_text="Prefilter endpoints to include only selected effect tags.",
+    )
     effect_tags = df.MultipleChoiceFilter(
         field_name="effects",
         label="Tags to include",
-        help_text="Select one or more effect-tags to include in the plot.",
+        help_text="Select one or more effect tags to include in the plot.",
     )
 
     class Meta:
         model = Outcome
         fields = [
             "published_only",
+            "cb_studies",
             "studies",
+            "cb_systems",
             "systems",
+            "cb_effects",
             "effects",
+            "cb_effect_tags",
             "effect_tags",
         ]
         form = PrefilterForm
@@ -165,13 +240,19 @@ class EpiV1ResultPrefilter(BaseFilterSet):
         form.fields["effect_tags"].choices = EffectTag.objects.get_choices(self.assessment.pk)
 
 
-class EpiV2ResultPrefilter(BaseFilterSet):
+class EpiV2ResultPrefilter(PrefilterBase):
     # studies
     published_only = df.BooleanFilter(
         method="filter_published_only",
         widget=CheckboxInput(),
         label="Published studies only",
         help_text='Only present data from studies which have been marked as "published" in HAWC.',
+    )
+    cb_studies = df.BooleanFilter(
+        method="noop",
+        widget=CheckboxInput(attrs={"data-pf": "studies"}),
+        label="Prefilter by study",
+        help_text="Prefilter endpoints to include only selected studies.",
     )
     studies = df.MultipleChoiceFilter(
         field_name="design__study",
@@ -183,6 +264,7 @@ class EpiV2ResultPrefilter(BaseFilterSet):
         model = DataExtraction
         fields = [
             "published_only",
+            "cb_studies",
             "studies",
         ]
         form = PrefilterForm
@@ -203,13 +285,19 @@ class EpiV2ResultPrefilter(BaseFilterSet):
         form.fields["studies"].choices = Study.objects.get_choices(self.assessment.pk)
 
 
-class EpiMetaResultPrefilter(BaseFilterSet):
+class EpiMetaResultPrefilter(PrefilterBase):
     # studies
     published_only = df.BooleanFilter(
         method="filter_published_only",
         widget=CheckboxInput(),
         label="Published studies only",
         help_text='Only present data from studies which have been marked as "published" in HAWC.',
+    )
+    cb_studies = df.BooleanFilter(
+        method="noop",
+        widget=CheckboxInput(attrs={"data-pf": "studies"}),
+        label="Prefilter by study",
+        help_text="Prefilter endpoints to include only selected studies.",
     )
     studies = df.MultipleChoiceFilter(
         field_name="protocol__study",
@@ -221,6 +309,7 @@ class EpiMetaResultPrefilter(BaseFilterSet):
         model = MetaResult
         fields = [
             "published_only",
+            "cb_studies",
             "studies",
         ]
         form = PrefilterForm
@@ -241,7 +330,7 @@ class EpiMetaResultPrefilter(BaseFilterSet):
         form.fields["studies"].choices = Study.objects.get_choices(self.assessment.pk)
 
 
-class InvitroOutcomePrefilter(BaseFilterSet):
+class InvitroOutcomePrefilter(PrefilterBase):
     # studies
     published_only = df.BooleanFilter(
         method="filter_published_only",
@@ -249,41 +338,76 @@ class InvitroOutcomePrefilter(BaseFilterSet):
         label="Published studies only",
         help_text='Only present data from studies which have been marked as "published" in HAWC.',
     )
+    cb_studies = df.BooleanFilter(
+        method="noop",
+        widget=CheckboxInput(attrs={"data-pf": "studies"}),
+        label="Prefilter by study",
+        help_text="Prefilter endpoints to include only selected studies.",
+    )
     studies = df.MultipleChoiceFilter(
         field_name="experiment__study",
         label="Studies to include",
         help_text="Select one or more studies to include in the plot.",
     )
     # invitro
+    cb_effects = df.BooleanFilter(
+        method="noop",
+        widget=CheckboxInput(attrs={"data-pf": "effects"}),
+        label="Prefilter by effect",
+        help_text="Prefilter endpoints to include only selected effects.",
+    )
     effects = df.MultipleChoiceFilter(
         field_name="effect",
         label="Effects to include",
         help_text="Select one or more effects to include in the plot.",
+    )
+    cb_categories = df.BooleanFilter(
+        method="noop",
+        widget=CheckboxInput(attrs={"data-pf": "categories"}),
+        label="Prefilter by category",
+        help_text="Prefilter endpoints to include only selected categories.",
     )
     categories = df.MultipleChoiceFilter(
         field_name="category",
         label="Categories to include",
         help_text="Select one or more categories to include in the plot.",
     )
+    cb_chemicals = df.BooleanFilter(
+        method="noop",
+        widget=CheckboxInput(attrs={"data-pf": "chemicals"}),
+        label="Prefilter by chemical",
+        help_text="Prefilter endpoints to include only selected chemicals.",
+    )
     chemicals = df.MultipleChoiceFilter(
         field_name="chemical__name",
         label="Chemicals to include",
         help_text="Select one or more chemicals to include in the plot.",
     )
+    cb_effect_tags = df.BooleanFilter(
+        method="noop",
+        widget=CheckboxInput(attrs={"data-pf": "effect_tags"}),
+        label="Prefilter by effect tag",
+        help_text="Prefilter endpoints to include only selected effect tags.",
+    )
     effect_tags = df.MultipleChoiceFilter(
         field_name="effects",
         label="Tags to include",
-        help_text="Select one or more effect-tags to include in the plot.",
+        help_text="Select one or more effect tags to include in the plot.",
     )
 
     class Meta:
         model = IVEndpoint
         fields = [
             "published_only",
+            "cb_studies",
             "studies",
+            "cb_effects",
             "effects",
+            "cb_categories",
             "categories",
+            "cb_chemicals",
             "chemicals",
+            "cb_effect_tags",
             "effect_tags",
         ]
         form = PrefilterForm
@@ -323,6 +447,12 @@ class EcoResultPrefilter(BaseFilterSet):
         label="Published studies only",
         help_text='Only present data from studies which have been marked as "published" in HAWC.',
     )
+    cb_studies = df.BooleanFilter(
+        method="noop",
+        widget=CheckboxInput(attrs={"data-pf": "studies"}),
+        label="Prefilter by study",
+        help_text="Prefilter endpoints to include only selected studies.",
+    )
     studies = df.MultipleChoiceFilter(
         field_name="design__study",
         label="Studies to include",
@@ -333,6 +463,7 @@ class EcoResultPrefilter(BaseFilterSet):
         model = Result
         fields = [
             "published_only",
+            "cb_studies",
             "studies",
         ]
         form = PrefilterForm
