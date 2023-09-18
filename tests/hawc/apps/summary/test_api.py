17cc4cc4d0cc1747e4ea4b406730bbdaaf3feeb6
casey1173 <caseyhansen1999@gmail.com>
2023-09-18 10:51:59 -0400

    update visual test

--- tests/hawc/apps/summary/test_api.py
+++ tests/hawc/apps/summary/test_api.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 from django.utils.http import urlencode
 from rest_framework.test import APIClient
 
-from hawc.apps.summary import constants, models
+from hawc.apps.summary import models
 
 from ..test_utils import check_api_json_data
 
@@ -81,7 +81,7 @@ class TestVisual:
     Make sure our API gives expected results for all visual types
     """
 
-    def _test_visual_crud_api(self, rewrite_data_files: bool, slug: str, assessment_id):
+    def _test_visual_crud_api(self, data, rewrite_data_files: bool, slug: str):
         user_anon = APIClient()
         user_reviewer = APIClient()
         assert user_reviewer.login(username="reviewer@hawcproject.org", password="pw") is True
@@ -93,27 +93,15 @@ class TestVisual:
         response = user_anon.get(url)
         assert response.status_code == 200
 
-        data = response.json()
-        data_str = json.dumps(data, indent=2, sort_keys=True)
+        resp_data = response.json()
+        data_str = json.dumps(resp_data, indent=2, sort_keys=True)
         data_str = re.sub(r"obj_ct=\d+", "obj_ct=9999", data_str)
 
         key = f"api-visual-{slug}.json"
         check_api_json_data(json.loads(data_str), key, rewrite_data_files)
 
-        visual_types = dict(constants.VisualType.choices)
-
-        # use read data for CUD operations
-        data["title"] = data["title"] + "-2"
-        data["assessment"] = assessment_id
-        data["slug"] = data["slug"] + "-2"
-        data["visual_type"] = {
-            key for key, value in visual_types.items() if data["visual_type"] in value
-        }.pop()
-        data["settings"] = json.dumps(data["settings"])
-        data["studies"] = []
-        data["endpoints"] = []
-
         # create visual
+        url = reverse("summary:api:visual-list")
         for user in [user_anon, user_reviewer]:
             response = user.post(url, data, format="json")
             assert response.status_code == 403
@@ -141,49 +129,116 @@ class TestVisual:
         assert response.status_code == 204
 
     def test_bioassay_aggregation(self, rewrite_data_files: bool, db_keys):
-        self._test_visual_crud_api(
-            rewrite_data_files, "bioassay-aggregation", db_keys.assessment_working
-        )
+        data = {
+            "title": "bioassay-aggregation-2",
+            "slug": "bioassay-aggregation-2",
+            "visual_type": 0,
+            "settings": "{}",
+            "assessment": db_keys.assessment_working,
+            "prefilters": "{}",
+            "caption": "<p>caption</p>",
+            "published": True,
+            "sort_order": "short_citation",
+            "endpoints": [],
+            "studies": [],
+        }
+        self._test_visual_crud_api(data, rewrite_data_files, "bioassay-aggregation")
 
     def test_rob_heatmap(self, rewrite_data_files: bool, db_keys):
-        self._test_visual_crud_api(rewrite_data_files, "rob-heatmap", db_keys.assessment_working)
+        data = {
+            "title": "rob-heatmap-2",
+            "slug": "rob-heatmap-2",
+            "visual_type": 2,
+            "settings": '{"title":"","xAxisLabel":"","yAxisLabel":"","padding_top":20,"cell_size":40,"padding_right":300,"padding_bottom":35,"padding_left":20,"x_field":"study","study_label_field":"short_citation","included_metrics":[14,15],"excluded_score_ids":[],"show_legend":true,"show_na_legend":true,"show_nr_legend":true,"legend_x":231,"legend_y":30}',
+            "assessment": db_keys.assessment_working,
+            "prefilters": "{}",
+            "caption": "<p>caption</p>",
+            "published": True,
+            "sort_order": "short_citation",
+            "endpoints": [],
+            "studies": [],
+        }
+        self._test_visual_crud_api(data, rewrite_data_files, "rob-heatmap")
 
     def test_crossview(self, rewrite_data_files: bool, db_keys):
-        self._test_visual_crud_api(rewrite_data_files, "crossview", db_keys.assessment_working)
+        data = {
+            "title": "crossview-2",
+            "slug": "crossview-2",
+            "visual_type": 1,
+            "settings": '{"title":"Title","xAxisLabel":"Dose (<add units>)","yAxisLabel":"% change from control (continuous), % incidence (dichotomous)","width":1100,"height":600,"inner_width":940,"inner_height":520,"padding_left":75,"padding_top":45,"dose_isLog":true,"dose_range":"","response_range":"","title_x":0,"title_y":0,"xlabel_x":0,"xlabel_y":0,"ylabel_x":0,"ylabel_y":0,"filters":[{"name":"study","headerName":"Study","allValues":true,"values":null,"columns":1,"x":null,"y":null}],"reflines_dose":[{"value":null,"title":"","style":"base"}],"refranges_dose":[{"lower":null,"upper":null,"title":"","style":"base"}],"reflines_response":[{"value":null,"title":"","style":"base"}],"refranges_response":[{"lower":null,"upper":null,"title":"","style":"base"}],"labels":[{"caption":"","style":"base","max_width":null,"x":null,"y":null}],"colorBase":"#cccccc","colorHover":"#ff4040","colorSelected":"#6495ed","colorFilters":[],"colorFilterLegend":true,"colorFilterLegendLabel":"Color filters","colorFilterLegendX":0,"colorFilterLegendY":0,"endpointFilters":[],"endpointFilterLogic":"and"}',
+            "assessment": db_keys.assessment_working,
+            "prefilters": '{"animal_group__experiment__study__published": true}',
+            "caption": "<p>example</p>",
+            "published": True,
+            "sort_order": "short_citation",
+            "endpoints": [],
+            "studies": [],
+        }
+        self._test_visual_crud_api(data, rewrite_data_files, "crossview")
 
     def test_barchart(self, rewrite_data_files: bool, db_keys):
-        self._test_visual_crud_api(rewrite_data_files, "barchart", db_keys.assessment_working)
+        data = {
+            "title": "barchart-2",
+            "slug": "barchart-2",
+            "visual_type": 3,
+            "settings": '{"title":"Title","xAxisLabel":"Percent of studies","yAxisLabel":"","plot_width":400,"row_height":30,"padding_top":40,"padding_right":300,"padding_bottom":40,"padding_left":70,"show_values":true,"included_metrics":[14,15],"show_legend":true,"show_na_legend":true,"legend_x":574,"legend_y":10}',
+            "assessment": db_keys.assessment_working,
+            "prefilters": "{}",
+            "caption": "<p>caption</p>",
+            "published": True,
+            "sort_order": "short_citation",
+            "endpoints": [],
+            "studies": [],
+        }
+        self._test_visual_crud_api(data, rewrite_data_files, "barchart")
 
     def test_tagtree(self, rewrite_data_files: bool, db_keys):
-        self._test_visual_crud_api(rewrite_data_files, "tagtree", db_keys.assessment_working)
+        data = {
+            "title": "tagtree-2",
+            "slug": "tagtree-2",
+            "visual_type": 4,
+            "settings": '{"root_node": 11, "required_tags": [], "pruned_tags": [], "hide_empty_tag_nodes": false, "height": 500, "width": 1280, "show_legend": true, "show_counts": true}',
+            "assessment": db_keys.assessment_working,
+            "prefilters": "{}",
+            "caption": "<p>caption</p>",
+            "published": True,
+            "sort_order": "short_citation",
+            "endpoints": [],
+            "studies": [],
+        }
+        self._test_visual_crud_api(data, rewrite_data_files, "tagtree")
 
     def test_embedded_tableau(self, rewrite_data_files: bool, db_keys):
-        self._test_visual_crud_api(
-            rewrite_data_files, "embedded-tableau", db_keys.assessment_working
-        )
+        data = {
+            "title": "embedded-tableau-2",
+            "slug": "embedded-tableau-2",
+            "visual_type": 5,
+            "settings": '{"external_url": "https://public.tableau.com/views/Iris_15675445278420/Iris-Actual", "external_url_hostname": "https://public.tableau.com", "external_url_path": "/views/Iris_15675445278420/Iris-Actual", "external_url_query_args": [":showVizHome=no", ":embed=y"], "filters": []}',
+            "assessment": db_keys.assessment_working,
+            "prefilters": "{}",
+            "caption": "<p>iris (flowers)</p>",
+            "published": True,
+            "sort_order": "short_citation",
+            "endpoints": [],
+            "studies": [],
+        }
+        self._test_visual_crud_api(data, rewrite_data_files, "embedded-tableau")
 
     def test_exploratory_heatmap(self, rewrite_data_files: bool, db_keys):
-        self._test_visual_crud_api(
-            rewrite_data_files, "exploratory-heatmap", db_keys.assessment_working
-        )
-
-    def _test_visual_data_api(self, rewrite_data_files: bool, slug: str):
-        client = Client()
-        url = models.Visual.objects.get(slug=slug).get_data_url()
-        response = client.get(url)
-        assert response.status_code == 200
-
-        data = response.json()
-        data_str = json.dumps(data, indent=2, sort_keys=True)
-
-        key = f"api-visual-{slug}-data.json"
-        check_api_json_data(json.loads(data_str), key, rewrite_data_files)
-
-    def test_rob_heatmap_data(self, rewrite_data_files: bool):
-        self._test_visual_data_api(rewrite_data_files, "rob-heatmap")
-
-    def test_barchart_data(self, rewrite_data_files: bool):
-        self._test_visual_data_api(rewrite_data_files, "rob-barchart")
+        data = {
+            "title": "exploratory-heatmap-2",
+            "slug": "exploratory-heatmap-2",
+            "visual_type": 6,
+            "settings": '{"cell_height": 50, "cell_width": 50, "color_range": ["#ffffff", "#cc4700"], "compress_x": true, "compress_y": true, "data_url": "/ani/api/assessment/2/endpoint-heatmap/", "hawc_interactivity": true, "filter_widgets": [{"column": "species", "delimiter": "", "on_click_event": "---", "header": ""}, {"column": "strain", "delimiter": "", "on_click_event": "---", "header": ""}], "padding": {"top": 30, "left": 30, "bottom": 30, "right": 30}, "show_axis_border": true, "show_grid": true, "show_tooltip": true, "show_totals": true, "show_null": true, "autosize_cells": true, "autorotate_tick_labels": true, "table_fields": [{"column": "study citation", "delimiter": "", "on_click_event": "study", "header": ""}, {"column": "experiment name", "delimiter": "", "on_click_event": "experiment", "header": ""}, {"column": "animal group name", "delimiter": "", "on_click_event": "animal_group", "header": ""}, {"column": "endpoint name", "delimiter": "", "on_click_event": "endpoint_complete", "header": ""}, {"column": "---", "delimiter": "", "on_click_event": "---", "header": ""}], "title": {"text": "", "x": 0, "y": 0, "rotate": 0}, "x_fields": [{"column": "system", "wrap_text": 0, "delimiter": ""}], "x_label": {"text": "", "x": 0, "y": 0, "rotate": 0}, "x_tick_rotate": 0, "y_fields": [{"column": "study citation", "wrap_text": 0, "delimiter": ""}], "y_label": {"text": "", "x": 0, "y": 0, "rotate": 0}, "y_tick_rotate": -90, "x_axis_bottom": false, "filters": [], "filtersLogic": "and"}',
+            "assessment": db_keys.assessment_working,
+            "prefilters": "{}",
+            "caption": "<p>caption</p>",
+            "published": True,
+            "sort_order": "short_citation",
+            "endpoints": [],
+            "studies": [],
+        }
+        self._test_visual_crud_api(data, rewrite_data_files, "exploratory-heatmap")
 
 
 @pytest.mark.django_db
