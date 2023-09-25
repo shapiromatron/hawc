a131ead32943ce2e717e88c491c0a9810c8e4048
Matt Munns <mmunns16@gmail.com>
2023-09-25 14:48:28 -0400

    Create models/views for binding UDFs to models and tags (#899)
    * wip create tag/model binding views
    
    * wip add more views/template
    
    * add basic models and views
    
    * update form classes
    
    * create detail pages
    
    * filter queryset for content type choices
    
    * lint
    
    * add tests for list/create views
    
    * update view layouts
    
    * add built-in form preview
    
    * update permissions tests
    
    ---------
    
    Co-authored-by: Andy Shapiro <shapiromatron@gmail.com>

--- hawc/apps/udf/forms.py
+++ hawc/apps/udf/forms.py
@@ -1,6 +1,9 @@
 from crispy_forms import bootstrap as cfb
 from crispy_forms import layout as cfl
 from django import forms
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import F, Value
+from django.db.models.functions import Concat
 from django.urls import reverse, reverse_lazy
 
 from hawc.apps.common.autocomplete.forms import AutocompleteSelectMultipleWidget
@@ -8,7 +11,8 @@ from hawc.apps.common.dynamic_forms.schemas import Schema
 from hawc.apps.common.forms import BaseFormHelper, PydanticValidator, TextareaButton
 from hawc.apps.myuser.autocomplete import UserAutocomplete
 
-from .models import UserDefinedForm
+from ..assessment.models import Assessment
+from . import constants, models
 
 
 class UDFForm(forms.ModelForm):
@@ -34,7 +38,7 @@ class UDFForm(forms.ModelForm):
             self.instance.creator = user
 
     class Meta:
-        model = UserDefinedForm
+        model = models.UserDefinedForm
         fields = ("name", "description", "schema", "editors", "deprecated")
         widgets = {
             "editors": AutocompleteSelectMultipleWidget(UserAutocomplete),
@@ -70,3 +74,73 @@ class SchemaPreviewForm(forms.Form):
     """Form for previewing a Dynamic Form schema."""
 
     schema = forms.JSONField(validators=[PydanticValidator(Schema)])
+
+
+class ModelBindingForm(forms.ModelForm):
+    assessment = forms.ModelChoiceField(
+        queryset=Assessment.objects.all(), disabled=True, widget=forms.HiddenInput
+    )
+    content_type = forms.ModelChoiceField(
+        queryset=ContentType.objects.annotate(
+            full_name=Concat(F("app_label"), Value("."), F("model"))
+        ).filter(full_name__in=constants.SUPPORTED_MODELS)
+    )
+
+    def __init__(self, *args, **kwargs):
+        self.assessment = kwargs.pop("parent", None)
+        user = kwargs.pop("user", None)
+        super().__init__(*args, **kwargs)
+        if self.instance.id is None:
+            # include assessment for unique_together validation
+            self.fields["assessment"].initial = self.assessment
+            self.instance.assessment = self.assessment
+            self.instance.creator = user
+
+    class Meta:
+        model = models.ModelBinding
+        fields = ("assessment", "content_type", "form")
+
+    @property
+    def helper(self):
+        cancel_url = (
+            self.instance.get_absolute_url()
+            if self.instance.id
+            else self.instance.assessment.get_udf_list_url()
+        )
+        legend_text = "Update a model binding" if self.instance.id else "Create a model binding"
+        helper = BaseFormHelper(self, legend_text=legend_text, cancel_url=cancel_url)
+        return helper
+
+
+class TagBindingForm(forms.ModelForm):
+    assessment = forms.ModelChoiceField(
+        queryset=Assessment.objects.all(), disabled=True, widget=forms.HiddenInput
+    )
+
+    def __init__(self, *args, **kwargs):
+        self.assessment = kwargs.pop("parent", None)
+        user = kwargs.pop("user", None)
+        super().__init__(*args, **kwargs)
+        if self.instance.id is None:
+            # include assessment for unique_together validation
+            self.fields["assessment"].initial = self.assessment
+            self.instance.assessment = self.assessment
+            self.instance.creator = user
+        qs = models.ReferenceFilterTag.get_assessment_qs(self.instance.assessment_id)
+        self.fields["tag"].queryset = qs
+        self.fields["tag"].choices = [(el.id, el.get_nested_name()) for el in qs]
+
+    class Meta:
+        model = models.TagBinding
+        fields = ("assessment", "tag", "form")
+
+    @property
+    def helper(self):
+        cancel_url = (
+            self.instance.get_absolute_url()
+            if self.instance.id
+            else self.instance.assessment.get_udf_list_url()
+        )
+        legend_text = "Update a tag binding" if self.instance.id else "Create a tag binding"
+        helper = BaseFormHelper(self, legend_text=legend_text, cancel_url=cancel_url)
+        return helper
