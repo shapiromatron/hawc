8b534d03767d89fbad1c4966a5fdd69882b3d4fd
Daniel Rabstejnek <rabstejnek@gmail.com>
2023-10-11 14:09:10 -0400

    remove obsolete code

--- hawc/apps/epimeta/exports.py
+++ hawc/apps/epimeta/exports.py
@@ -173,7 +173,7 @@ class MetaResultFlatComplete(FlatFileExporter):
         return EpiMetaExporter().get_df(self.queryset)
 
 
-class MetaResultFlatDataPivot2(FlatFileExporter):
+class MetaResultFlatDataPivot(FlatFileExporter):
     """
     Return a subset of frequently-used data for generation of data-pivot
     visualizations.
@@ -213,70 +213,3 @@ class MetaResultFlatDataPivot2(FlatFileExporter):
             errors="raise",
         )
         return df
-
-
-class MetaResultFlatDataPivot(FlatFileExporter):
-    """
-    Return a subset of frequently-used data for generation of data-pivot
-    visualizations.
-
-    Note: data pivot does not currently include study confidence. Could be added if needed.
-    """
-
-    def _get_header_row(self):
-        return [
-            "study id",
-            "study name",
-            "study published",
-            "protocol id",
-            "protocol name",
-            "protocol type",
-            "total references",
-            "identified references",
-            "key",
-            "meta result id",
-            "meta result label",
-            "health outcome",
-            "exposure",
-            "result references",
-            "statistical metric",
-            "statistical metric abbreviation",
-            "N",
-            "estimate",
-            "lower CI",
-            "upper CI",
-            "CI units",
-            "heterogeneity",
-        ]
-
-    def _get_data_rows(self):
-        rows = []
-        for obj in self.queryset:
-            ser = obj.get_json(json_encode=False)
-            row = [
-                ser["protocol"]["study"]["id"],
-                ser["protocol"]["study"]["short_citation"],
-                ser["protocol"]["study"]["published"],
-                ser["protocol"]["id"],
-                ser["protocol"]["name"],
-                ser["protocol"]["protocol_type"],
-                ser["protocol"]["total_references"],
-                ser["protocol"]["total_studies_identified"],
-                ser["id"],  # repeat for data-pivot key
-                ser["id"],
-                ser["label"],
-                ser["health_outcome"],
-                ser["exposure_name"],
-                ser["number_studies"],
-                ser["metric"]["metric"],
-                ser["metric"]["abbreviation"],
-                ser["n"],
-                ser["estimate"],
-                ser["lower_ci"],
-                ser["upper_ci"],
-                ser["ci_units"],
-                ser["heterogeneity"],
-            ]
-            rows.append(row)
-
-        return rows
