ceb8e4e8fd1cede8e60f824ec926dea0ffb91f71
Danny Peterson <dannypeterson28@gmail.com>
2023-10-17 10:43:20 -0400

    added study and animal group analytics

--- hawc/apps/mgmt/analytics/growth.py
+++ hawc/apps/mgmt/analytics/growth.py
@@ -3,11 +3,15 @@ import math
 import numpy as np
 import pandas as pd
 import plotly.express as px
-from django.db.models import Count
+from django.db.models import Count, Q
 
+from hawc.apps.animal import constants
+from hawc.apps.animal.models import Endpoint, EndpointGroup, Experiment
 from hawc.apps.lit.models import Reference, Search
+from hawc.apps.study.models import Study
 
 
+# literature screening data
 def get_search_count(assessment_id):
     return Search.objects.filter(assessment_id=assessment_id).count()
 
@@ -138,9 +142,118 @@ def refs_tags_plot(assessment_id):
     return fig
 
 
+# studies data
+def study_classifications(assessment_id):
+    return {
+        "total": Study.objects.filter(assessment_id=assessment_id).count(),
+        "animal": Study.objects.filter(assessment_id=assessment_id)
+        .annotate(n_exp=Count("experiments"))
+        .filter(n_exp__gt=0)
+        .count(),
+        "epi": Study.objects.filter(assessment_id=assessment_id)
+        .annotate(n_sp=Count("study_populations"))
+        .filter(n_sp__gt=0)
+        .count(),
+        "epiv2": Study.objects.filter(assessment_id=assessment_id)
+        .annotate(n_des=Count("designs"))
+        .filter(n_des__gt=0)
+        .count(),
+        "rob": Study.objects.filter(assessment_id=assessment_id)
+        .annotate(n_rob=Count("riskofbiases"))
+        .filter(n_rob__gt=0)
+        .count(),
+        "published": Study.objects.filter(assessment_id=assessment_id)
+        .values("published")
+        .annotate(total=Count("published"))
+        .order_by("published")[True]["total"],
+        "unpublished": Study.objects.filter(assessment_id=assessment_id)
+        .values("published")
+        .annotate(total=Count("published"))
+        .order_by("published")[False]["total"],
+    }
+
+
+# animal data
+def animal_group_df(assessment_id):
+    qs = Endpoint.objects.filter(assessment_id=assessment_id).values_list(
+        "animal_group__experiment__study_id",
+        "animal_group__experiment__id",
+        "animal_group_id",
+        "id",
+    )
+    df = pd.DataFrame(data=qs, columns=["study", "experiment", "animal_group", "endpoint"])
+    return df
+
+
+def animal_counts(assessment_id):
+    df = animal_group_df(assessment_id)
+    return {
+        "studies": df.study.nunique(),
+        "experiments": df.experiment.nunique(),
+        "animal_groups": df.animal_group.nunique(),
+        "endpoints": df.endpoint.nunique(),
+    }
+
+
+def endpoints_extracted(assessment_id):
+    return (
+        Endpoint.objects.filter(assessment_id=assessment_id)
+        .annotate(ngroups=Count("groups"))
+        .filter(data_extracted=True, ngroups__gt=0)
+        .count()
+    )
+
+
+def endpoints_ehv(assessment_id):
+    return (
+        Endpoint.objects.filter(assessment_id=assessment_id)
+        .filter(
+            Q(name_term__isnull=False)
+            | Q(system_term__isnull=False)
+            | Q(organ_term__isnull=False)
+            | Q(effect_term__isnull=False)
+            | Q(effect_subtype_term__isnull=False)
+        )
+        .count()
+    )
+
+
+def n_dose_response_groups(assessment_id):
+    return EndpointGroup.objects.filter(endpoint__assessment_id=assessment_id).count()
+
+
+def experiment_type_df(assessment_id):
+    types = (
+        Experiment.objects.filter(study__assessment_id=assessment_id)
+        .values_list("type")
+        .annotate(total=Count("type"))
+        .order_by("type")
+    )
+    data = []
+    for type, n in list(types):
+        data.append((constants.ExperimentType(type).label, n))
+    df = pd.DataFrame(data=data, columns=["type", "count"]).sort_values("count")
+    return df
+
+
+def experiment_type_plot(assessment_id):
+    df = experiment_type_df(assessment_id)
+    fig = px.bar(
+        data_frame=df,
+        y="type",
+        x="count",
+        orientation="h",
+        text_auto=True,
+        height=500,
+        labels={"type": "Experiment type", "count": "# Experiments"},
+    )
+    return fig
+
+
 def get_context_data(self, **kwargs):
     id = self.assessment.id
     context = dict()
+    # literature screening
     context["n"] = get_search_count(id)
     context["search_types"] = get_search_types(id)
     context["total_n_refs"] = total_n_refs(id)
@@ -148,4 +261,12 @@ def get_context_data(self, **kwargs):
     context["refs_by_year_plot"] = refs_by_year_plot(id)
     context["ref_tags_breakdown"] = ref_tags_breakdown(id)
     context["refs_tags_plot"] = refs_tags_plot(id)
+    # studies
+    context["study_types"] = study_classifications(id)
+    # animal group
+    context["animal_counts"] = animal_counts(id)
+    context["n_endpoints_extracted"] = endpoints_extracted(id)
+    context["n_endpoints_ehv"] = endpoints_ehv(id)
+    context["n_dose_res_groups"] = n_dose_response_groups(id)
+    context["experiment_type_plot"] = experiment_type_plot(id)
     return context
