8bf70e632f6be7f4e1c254bc6336ef52b1e81f62
Danny Peterson <dannypeterson28@gmail.com>
2023-11-22 14:10:43 -0500

    combined after_edit and after_publish hooks

--- hawc/apps/docs/wagtail_hooks.py
+++ hawc/apps/docs/wagtail_hooks.py
@@ -3,13 +3,6 @@ from wagtail.models import PageViewRestriction
 
 
 @hooks.register("after_edit_page")
-def make_private_after_save(request, page):
-    if page.depth <= 2:
-        PageViewRestriction.objects.update_or_create(
-            page=page, defaults=dict(restriction_type=PageViewRestriction.LOGIN)
-        )
-
-
 @hooks.register("after_publish_page")
 def make_private_after_publish(request, page):
     if page.depth <= 2:
