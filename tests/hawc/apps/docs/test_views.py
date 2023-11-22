b8fe6c82711dc0bca7cf4e5c4405fcaf7a940b38
munnsmunns <mmunns16@gmail.com>
2023-11-22 11:08:39 -0500

    update wagtail tests

--- tests/hawc/apps/docs/test_views.py
+++ tests/hawc/apps/docs/test_views.py
@@ -1,15 +1,21 @@
 import pytest
 from django.urls import reverse
 
-from ..test_utils import check_200, check_302, get_client
+from hawc.apps.docs import models
+
+from ..test_utils import check_200, check_302, check_404, get_client
 
 
 @pytest.mark.django_db
 class TestDocsViews:
     def test_permissions(self, db_keys):
+        draft = models.DocsPage.objects.get(title="Draft Page")
+        public = models.DocsPage.objects.get(title="Public Page")
+
         urls = [
             ("login", reverse("wagtail_serve", args=("",))),
-            ("login", reverse("wagtaildocs_serve", args=("",))),
+            ("login", public.get_url()),
+            ("404", draft.get_url()),
             ("admin", reverse("wagtailadmin_home")),
         ]
         anon = get_client()
@@ -24,6 +30,9 @@ class TestDocsViews:
                 # admin required
                 check_302(reviewer, url, reverse("wagtailadmin_login"))
                 check_200(admin, url)
+            elif role == "404":
+                # always returns 404 (not published yet)
+                check_404(admin, url)
 
             else:
                 raise ValueError("Unknown role.")
