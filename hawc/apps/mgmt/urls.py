0c4ad25428b22756747939d72edbd1c25e42a7aa
Andy Shapiro <shapiromatron@gmail.com>
2023-11-28 09:34:28 -0500

    remove feature flag

--- hawc/apps/mgmt/urls.py
+++ hawc/apps/mgmt/urls.py
@@ -36,13 +36,9 @@ urlpatterns = [
         {"action": "update"},
         name="task-update",
     ),
+    path(
+        "assessment/<int:pk>/analytics/",
+        views.AssessmentAnalytics.as_view(),
+        name="assessment-analytics",
+    ),
 ]
-
-if settings.HAWC_FEATURES.ENABLE_ANALYTICS:
-    urlpatterns += [
-        path(
-            "assessment/<int:pk>/analytics/",
-            views.AssessmentAnalytics.as_view(),
-            name="assessment-analytics",
-        ),
-    ]
