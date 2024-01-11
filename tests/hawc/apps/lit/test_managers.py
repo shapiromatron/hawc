40c7e6436d7c182db4cc59cd359270bfb927857f
Casey Hansen <42587248+caseyhans@users.noreply.github.com>
2024-01-11 15:10:01 -0500

    Lit bulk merge tag conflicts (#932)
    * add tag deletion/addition filters
    
    * spelling error
    
    * fix filtering
    
    * updates
    
    * bulk merge tag conflicts
    
    * updates
    
    * remove merge_tags changes
    
    * use deleted_tags field
    
    * test
    
    * update modals
    
    * update migrations, add duplicate removal script
    
    * use .distinct and .difference
    
    * updates
    
    * add cache
    
    * deletion empty case fix
    
    * add permissions check
    
    * add test
    
    * update css
    
    * page refresh on modal close after merge
    
    * add tag comparisons
    
    * stupid easy fix for the bug that drove me crazy
    
    * updates
    
    * revert modal changes
    
    * add assessment id check before merge
    
    * simplify css
    
    * apply constraint cleanup for both models
    
    * DRY permissions check
    
    * remove api endpoint; support form view for now
    
    * use our django modal template
    
    * disable cache_key field
    
    * for consistency, make all "Change logs" -> "View change logs"
    
    * revert this change
    
    * use set comparisons instead of numpy
    
    * use boolean check instead of equality in template
    
    * add docstring
    
    * use kw names in func calls
    
    * use TZ aware datetime
    
    * write a flatten helper method
    
    * minor docstring updates
    
    * remove if statement
    
    * expand and center modals
    
    * fix loading modal
    
    * increase form field size
    
    * show tag parents in merge results
    
    * fix div
    
    ---------
    
    Co-authored-by: Andy Shapiro <shapiromatron@gmail.com>

--- tests/hawc/apps/lit/test_managers.py
+++ tests/hawc/apps/lit/test_managers.py
@@ -16,3 +16,55 @@ class TestReferenceTagsManager:
         df = models.ReferenceTags.objects.as_dataframe(db_keys.assessment_working)
         assert df.shape == (1, 2)
         assert df.to_csv(index=False, lineterminator="\n") == "reference_id,tag_id\n1,2\n"
+
+
+class TestReferenceManager:
+    @pytest.mark.django_db
+    def test_bulk_merge_conflicts(self, db_keys):
+        assessment = db_keys.assessment_conflict_resolution
+        tag_ids = [34]  # Animal Study
+        references_check = models.Reference.objects.filter(
+            assessment=assessment, user_tags__tags__in=tag_ids, user_tags__is_resolved=False
+        )
+        all_assessment_refs = models.Reference.objects.filter(assessment=assessment)
+
+        tagged_animal_before = models.Reference.objects.filter(
+            assessment=assessment, tags__in=tag_ids
+        ).count()
+
+        references_before = {}
+        for ref in references_check:
+            references_before[ref.id] = {}
+            references_before[ref.id]["tags"] = set(ref.tags.all().values_list("id", flat=True))
+            references_before[ref.id]["users_resolved"] = set(
+                user_tag.is_resolved for user_tag in ref.user_tags.all()
+            )
+
+        merge_result = all_assessment_refs.merge_tag_conflicts(
+            tag_ids, db_keys.pm_user_id, include_without_conflicts=False, preview=True
+        )  # preview mode enabled
+        assert merge_result["message"] == "Preview mode enabled."
+
+        # no change with preview mode
+        for ref in references_check:
+            tags_after = set(ref.tags.all().values_list("id", flat=True))
+            assert references_before[ref.id]["tags"] == tags_after
+            resolved_after = set([user_tag.is_resolved for user_tag in ref.user_tags.all()])
+            assert references_before[ref.id]["users_resolved"] == resolved_after
+
+        merge_result = all_assessment_refs.merge_tag_conflicts(
+            tag_ids, db_keys.pm_user_id, include_without_conflicts=False, preview=False
+        )  # merge mode
+        assert "references updated" in merge_result["message"]
+
+        # changes after merge mode
+        for ref in references_check:
+            tags_after = set(ref.tags.all().values_list("id", flat=True))
+            assert references_before[ref.id]["tags"] != tags_after
+            resolved_after = set([user_tag.is_resolved for user_tag in ref.user_tags.all()])
+            assert references_before[ref.id]["users_resolved"] != resolved_after
+
+        assert (
+            models.Reference.objects.filter(assessment=assessment, tags__in=tag_ids).count()
+            > tagged_animal_before
+        )
