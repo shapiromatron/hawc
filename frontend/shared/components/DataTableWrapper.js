1e9e9a0c290d0922e257fff0ceecbc44cd620286
Daniel Rabstejnek <rabstejnek@gmail.com>
2024-02-01 13:39:43 -0500

    improve datatable render for large datasets

--- frontend/shared/components/DataTableWrapper.js
+++ frontend/shared/components/DataTableWrapper.js
@@ -1,19 +1,37 @@
 import $ from "jquery";
 import PropTypes from "prop-types";
-import React, {createRef, useEffect} from "react";
+import React, {createRef, useEffect, useState} from "react";
+import Loading from "shared/components/Loading";
 
-const DataTableWrapper = ({children}) => {
-    const ref = createRef();
+const DataTableWrapper = ({children, className, data, columns}) => {
+    const ref = createRef(),
+        [loading, setLoading] = useState(true);
     $.DataTable = require("datatables.net");
     useEffect(() => {
-        const table = $(ref.current.firstElementChild).DataTable({order: [], pageLength: 10});
+        // only include data/columns if they were provided
+        const table = $(ref.current.firstElementChild).DataTable({
+            autoWidth: false,
+            order: [],
+            pageLength: 10,
+            ...(data ? {data} : {}),
+            ...(columns ? {columns} : {}),
+        });
+        setLoading(false);
         return () => table.destroy();
     });
-    return <div ref={ref}>{children}</div>;
+    return (
+        <>
+            {loading ? <Loading /> : null}
+            <div ref={ref}>{children ? children : <table className={className}></table>}</div>
+        </>
+    );
 };
 
 DataTableWrapper.propTypes = {
     children: PropTypes.node,
+    className: PropTypes.string,
+    data: PropTypes.array,
+    columns: PropTypes.array,
 };
 
 export default DataTableWrapper;
