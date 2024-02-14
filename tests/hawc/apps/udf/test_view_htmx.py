1c0881a81d044965e23e60c814edab12890cd819
casey1173 <caseyhansen1999@gmail.com>
2024-02-14 13:35:36 -0500

    fix and add tests

---
+++ tests/hawc/apps/udf/test_view_htmx.py
@@ -0,0 +1,117 @@
+import pytest
+from django.test import Client
+from django.urls import reverse
+from pytest_django.asserts import assertTemplateUsed
+
+from hawc.apps.lit.models import ReferenceFilterTag
+from hawc.apps.udf import constants, models
+
+
+@pytest.mark.django_db
+class TestBindings:
+    def test_tagbindings(self, db_keys):
+        assessment_id = db_keys.assessment_working
+        client = Client(headers={"hx-request": "true"})
+        assert client.login(email="pm@hawcproject.org", password="pw") is True
+
+        initial_tagbinding_count = models.TagBinding.objects.count()
+
+        udf_id = models.UserDefinedForm.objects.first().id
+        tags = ReferenceFilterTag.get_assessment_qs(assessment_id)
+
+        # tag binding create
+        url = reverse("udf:binding_create", args=[assessment_id, constants.BindingType.TAG.value])
+        inputs = {
+            "form": udf_id,
+            "tag": tags[1].id,
+        }
+        resp = client.post(url, data=inputs)
+        assertTemplateUsed(resp, "udf/fragments/_udf_item.html")
+        assert resp.status_code == 200
+        assert tags[1].name in str(resp.content)
+        tag_binding = resp.context["binding"]
+        assert models.TagBinding.objects.count() == initial_tagbinding_count + 1
+
+        # tag binding read
+        url = reverse("udf:binding_detail", args=[constants.BindingType.TAG.value, tag_binding.id])
+        resp = client.get(url)
+        assertTemplateUsed(resp, "udf/fragments/_udf_item.html")
+        assert resp.status_code == 200
+        assert "tag" in str(resp.content)
+
+        # tag binding update
+        url = reverse("udf:binding_update", args=[constants.BindingType.TAG.value, tag_binding.id])
+        inputs = {
+            "form": udf_id,
+            "tag": tags[2].id,
+        }
+        resp = client.post(url, data=inputs)
+        assertTemplateUsed(resp, "udf/fragments/_udf_item.html")
+        assert resp.status_code == 200
+        assert tags[2].name in str(resp.content)
+        assert models.TagBinding.objects.count() == initial_tagbinding_count + 1
+
+        # tag binding delete
+        url = reverse("udf:binding_delete", args=[constants.BindingType.TAG.value, tag_binding.id])
+        resp = client.get(url)
+        assert resp.status_code == 200
+        assert "Are you sure you want to delete?" in str(resp.content)
+        resp = client.post(url)
+        assert resp.status_code == 200
+        assert models.TagBinding.objects.count() == initial_tagbinding_count
+
+    def test_modelbindings(self, db_keys):
+        assessment_id = db_keys.assessment_working
+        client = Client(headers={"hx-request": "true"})
+        assert client.login(email="pm@hawcproject.org", password="pw") is True
+
+        initial_modelbinding_count = models.ModelBinding.objects.count()
+
+        udf_id = models.UserDefinedForm.objects.first().id
+
+        # model binding create
+        url = reverse("udf:binding_create", args=[assessment_id, constants.BindingType.MODEL.value])
+        inputs = {
+            "form": udf_id,
+            "content_type": 87,
+        }
+        resp = client.post(url, data=inputs)
+        assertTemplateUsed(resp, "udf/fragments/_udf_item.html")
+        assert resp.status_code == 200
+        assert "Study" in str(resp.content)
+        model_binding = resp.context["binding"]
+        assert models.ModelBinding.objects.count() == initial_modelbinding_count + 1
+
+        # model binding read
+        url = reverse(
+            "udf:binding_detail", args=[constants.BindingType.MODEL.value, model_binding.id]
+        )
+        resp = client.get(url)
+        assertTemplateUsed(resp, "udf/fragments/_udf_item.html")
+        assert resp.status_code == 200
+        assert "Study" in str(resp.content)
+
+        # model binding update
+        url = reverse(
+            "udf:binding_update", args=[constants.BindingType.MODEL.value, model_binding.id]
+        )
+        inputs = {
+            "form": udf_id,
+            "content_type": 91,
+        }
+        resp = client.post(url, data=inputs)
+        assertTemplateUsed(resp, "udf/fragments/_udf_item.html")
+        assert resp.status_code == 200
+        assert "endpoint" in str(resp.content)
+        assert models.ModelBinding.objects.count() == initial_modelbinding_count + 1
+
+        # model binding delete
+        url = reverse(
+            "udf:binding_delete", args=[constants.BindingType.MODEL.value, model_binding.id]
+        )
+        resp = client.get(url)
+        assert resp.status_code == 200
+        assert "Are you sure you want to delete?" in str(resp.content)
+        resp = client.post(url)
+        assert resp.status_code == 200
+        assert models.ModelBinding.objects.count() == initial_modelbinding_count
