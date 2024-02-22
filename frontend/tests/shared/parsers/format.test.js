85002d2223ab9cfc7d331e8bf90fe7de447a7b91
Daniel Rabstejnek <rabstejnek@gmail.com>
2024-02-22 15:51:14 -0500

    Updated tests

--- frontend/tests/shared/parsers/format.test.js
+++ frontend/tests/shared/parsers/format.test.js
@@ -1,7 +1,7 @@
 import assert from "assert";
 import Format from "shared/parsers/format";
 
-const obj = {one: 1, two: 2},
+const obj = {one: 1, two: "two"},
     getValue = prop => obj[prop],
     options = {getValue};
 
@@ -14,7 +14,7 @@ describe("shared/parsers/format", function() {
         });
         it("handles multiple substitutes", function() {
             let actual = Format.parse("${one} and ${two}", options),
-                expected = "1 and 2";
+                expected = "1 and two";
             assert.ok(actual == expected);
         });
         it("handles no substitutes", function() {
@@ -27,12 +27,39 @@ describe("shared/parsers/format", function() {
                 expected = "1 + 1 = 2";
             assert.ok(actual == expected);
         });
+        it("handles leading and trailing literals", function() {
+            let actual = Format.parse("1 + ${one} = 2", options),
+                expected = "1 + 1 = 2";
+            assert.ok(actual == expected);
+        });
+        it("handles escaped characters", function() {
+            let actual = Format.parse("\\${one}", options),
+                expected = "${one}";
+            assert.ok(actual == expected);
+        });
+        it("handles ternary true", function() {
+            let actual = Format.parse("match(one,1)?This is true:This is false", options),
+                expected = "This is true";
+            assert.ok(actual == expected);
+        });
+        it("handles ternary false", function() {
+            let actual = Format.parse("match(one,2)?This is true:This is false", options),
+                expected = "This is false";
+            assert.ok(actual == expected);
+        });
+        it("handles ternary integer match", function() {
+            let actual = Format.parse("match(one,1)?This is true:This is false", options),
+                expected = "This is true";
+            assert.ok(actual == expected);
+        });
+        it("handles ternary string match", function() {
+            let actual = Format.parse('match(two,"two")?This is true:This is false', options),
+                expected = "This is true";
+            assert.ok(actual == expected);
+        });
         it("unable to parse bad strings", function() {
             // substitutions need to be closed
-            assert.throws(() => Format.parse("${identifier"));
-            // the identifier must only consist of alphanumeric characters,
-            // spaces, hyphens, or underscores
-            assert.throws(() => Format.parse("${inv@lid}"));
+            assert.throws(() => Format.parse("${identifier", options));
         });
     });
 });
