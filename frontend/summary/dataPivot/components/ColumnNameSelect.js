321358345852e880296e9ab9e321282dbf74b394
Andy Shapiro <shapiromatron@gmail.com>
2024-03-14 15:07:18 -0400

    data pivot - calculated columns (#991)
    * refactor vertical alignment
    
    * add calc columns
    
    * continue?
    
    * Revert "refactor vertical alignment"
    
    This reverts commit 29ad230627e0d235c337e0e3e4ef3ae6d016cd67.
    
    * add back header
    
    * add select manager
    
    * remove unused field_index
    
    * add calculated cols
    
    * add computed fields
    
    * fix db fixture
    
    * add a check stub
    
    * Added parser for custom format strings
    
    * Ternary operations in format parser
    
    * Updated tests
    
    * Linter
    
    * Update calculated columns
    
    * Update npm script name
    
    * Add tests, fix parser
    
    * fix migration conflict
    
    * use latest peggy version
    
    * add "to_right" to fixture; should have added in #987
    
    * add round
    
    * fix test case
    
    * add some text to describe how to use
    
    * fix docs (facepalm)
    
    ---------
    
    Co-authored-by: Daniel Rabstejnek <rabstejnek@gmail.com>

---
+++ frontend/summary/dataPivot/components/ColumnNameSelect.js
@@ -0,0 +1,55 @@
+import _ from "lodash";
+
+import {NULL_CASE} from "../shared";
+
+class ColumnSelect {
+    constructor(mgr, showBlank, attrs) {
+        this.mgr = mgr;
+        this.showBlank = showBlank;
+        this.select = $('<select class="form-control">');
+        if (attrs) {
+            _.forEach(attrs, (v, k) => this.select.attr(k, v));
+        }
+        this.update();
+    }
+    update() {
+        const selectedValue = this.select.find("option:selected").val(),
+            opts = this.mgr.headers.map(v => `<option value="${v}">${v}</option>`);
+        if (this.showBlank) {
+            opts.unshift(`<option value="${NULL_CASE}">${NULL_CASE}</option>`);
+        }
+        this.select.html(opts.concat());
+        if (selectedValue) {
+            this.select.find(`option[value="${selectedValue}"]`).prop("selected", true);
+        }
+    }
+}
+class ColumnSelectManager {
+    constructor(data_pivot) {
+        this.data_pivot = data_pivot;
+        this.selects = [];
+        this.updateHeaders();
+    }
+    createSelect(showBlank, attrs) {
+        const select = new ColumnSelect(this, showBlank, attrs);
+        this.selects.push(select);
+        return select.select;
+    }
+    updateSelects() {
+        this.updateHeaders();
+        this.selects.forEach(select => select.update());
+    }
+    updateHeaders() {
+        const firstRow = this.data_pivot.raw_data[0],
+            headers = _.keys(firstRow);
+        headers.push(
+            ...this.data_pivot.settings.calculated_columns
+                .filter(d => d.name.length > 0)
+                .map(d => d.name)
+        );
+        this.headers = headers;
+        this.data_pivot.store.sortStore.updateFieldColumns(headers);
+    }
+}
+
+export default ColumnSelectManager;
