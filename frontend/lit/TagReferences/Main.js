5c30756605229644559457e7024d6513dbeed750
Andy Shapiro <shapiromatron@gmail.com>
2024-04-02 14:41:51 -0400

    move clear form code to helper method

--- frontend/lit/TagReferences/Main.js
+++ frontend/lit/TagReferences/Main.js
@@ -51,78 +51,19 @@ ReferenceListItem.propTypes = {
     store: PropTypes.object,
 };
 
-var ReferenceUDF = inject("store")(
+const ReferenceUDF = inject("store")(
     observer(({currentUDF, UDFValues, UDFError}) => {
-        useEffect(() => {
-            // clear the entire form of existing data
-            $("#udf-form :input").each(function() {
-                switch ($(this).prop("type")) {
-                    case "radio":
-                    case "checkbox":
-                        $(this).attr("checked", false);
-                        break;
-                    default:
-                        $(this).val("");
-                }
-            });
-            // add data for current tags/reference
-            _.forEach(UDFValues, function(value, name) {
-                _.forEach(value, function(val) {
-                    var input = $(`[name="${name}"]`);
-                    if (input.prop("multiple")) {
-                        input.children(`option[value="${val}"]`).prop("selected", true);
-                    } else {
-                        switch (input.prop("type")) {
-                            case "radio":
-                            case "checkbox":
-                                input.each(function() {
-                                    // multiple select checkbox
-                                    if ($(this).attr("value") == val) {
-                                        $(this).attr("checked", val);
-                                    }
-                                    // single checkbox
-                                    else if (
-                                        $(this).attr("value") == undefined &&
-                                        (val == "on" || val == true)
-                                    ) {
-                                        $(this).attr("checked", true);
-                                    }
-                                });
-                                break;
-                            default:
-                                // text/number fields
-                                input.val(val);
-                        }
-                    }
-                });
-            });
-            HAWCUtils.dynamicFormListeners();
-            $("#udf-form .invalid-feedback").remove();
-            $("#udf-form .is-invalid").removeClass("is-invalid");
-            $("#udf-form .bg-pink").removeClass("bg-pink");
-            _.forEach(_.fromPairs(UDFError), function(error, field) {
-                var input = $(`[name="${field}"]`);
-                input.addClass("is-invalid");
-                input
-                    .closest("div.form-group")
-                    .append(`<div class="invalid-feedback d-block">${error.join(" ")}</div>`);
-                input
-                    .closest('[id^="collapse-"]')
-                    .siblings('[id^="udf-header-"]')
-                    .addClass("bg-pink");
-            });
-        });
-
-        return currentUDF.length > 0 ? (
+        useEffect(() => HAWCUtils.resetDynamicForm("#udf-form", UDFValues, UDFError));
+        if (!currentUDF) {
+            return null;
+        }
+        return (
             <form id="udf-form">
                 <div dangerouslySetInnerHTML={{__html: currentUDF}} />
             </form>
-        ) : (
-            ""
         );
     })
 );
-
 ReferenceUDF.propTypes = {
     currentUDF: PropTypes.string.isRequired,
     UDFValues: PropTypes.object.isRequired,
