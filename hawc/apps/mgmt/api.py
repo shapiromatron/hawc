b1856983d4a144ba7d5715a73c946f0037348dbd
Danny Peterson <dannypeterson28@gmail.com>
2024-04-04 13:57:01 -0400

    added time series exports

--- hawc/apps/mgmt/api.py
+++ hawc/apps/mgmt/api.py
@@ -1,3 +1,5 @@
+import pandas as pd
+from rest_framework import exceptions
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -8,6 +10,9 @@ from ..common.helper import FlatExport
 from ..common.renderers import PandasRenderers
 from ..common.serializers import UnusedSerializer
 from ..lit.models import Reference
+from ..riskofbias.models import RiskOfBias
+from ..study.models import Study
+from ..summary.models import DataPivot, Visual
 from . import exports, models
 from .analytics import time_series, time_spent
 
@@ -51,13 +56,43 @@ class MgmtViewSet(BaseAssessmentViewSet):
 
     @action(
         detail=True,
-        url_path="time-series",
-        action_perms=AssessmentViewSetPermissions.PROJECT_MANAGER_OR_HIGHER,
+        url_path=r"time-series/(?P<type>[\w]+)",
+        action_perms=AssessmentViewSetPermissions.TEAM_MEMBER_OR_HIGHER,
         renderer_classes=PandasRenderers,
     )
-    def time_series(self, request, pk):
-        """Time series export."""
+    def time_series(self, request, pk, type: str):
+        """References over time series export."""
+
+        time_series_data = {
+            "reference": (Reference, "month", pk),
+            "study": (Study, "month", pk),
+            "rob": (RiskOfBias, "month", pk, "study__assessment_id"),
+            "visual": (Visual, "month", pk),
+            "data_pivot": (DataPivot, "month", pk),
+            "total_visuals": None,
+        }
+
+        if type not in time_series_data:
+            raise exceptions.ValidationError({"detail": "Invalid type"})
+
+        args = time_series_data[type]
+        if args is None:
+            df = (
+                pd.concat(
+                    [
+                        time_series.get_data(*time_series_data["visual"]),
+                        time_series.get_data(*time_series_data["data_pivot"]),
+                    ]
+                )
+                .groupby("date")
+                .sum()
+                .reset_index()
+            )
+        else:
+            df = time_series.get_data(*args)
+
         assessment: Assessment = self.get_object()
-        refs = time_series.get_data(Reference, "month", pk)
-        export = FlatExport(df=refs, filename=f"{assessment}-time-series")
+        df["date"] = df["date"].dt.strftime("%m/%d/%Y")
+        df = df.set_index("date").cumsum().reset_index()
+        export = FlatExport(df=df, filename=f"{assessment}-{type}-time-series")
         return Response(export)
