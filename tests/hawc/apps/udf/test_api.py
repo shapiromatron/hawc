b75b37305a16674e11aa252ad26dbe60bbb76860
casey1173 <caseyhansen1999@gmail.com>
2024-05-08 12:47:13 -0400

    add TagUDFContent export tests

--- tests/hawc/apps/udf/test_api.py
+++ tests/hawc/apps/udf/test_api.py
@@ -33,3 +33,31 @@ class TestUdfAssessmentViewSet:
         url = url.replace("study.study", "foo.foo")
         response = check_200(client, url)
         assert len(response.json()) == 0
+
+    def test_tag_export(self, rewrite_data_files):
+        url = reverse("udf:api:assessment-export-tags", args=(1,))
+        client = get_client(api=True)
+
+        # anon get 403
+        check_403(client, url)
+
+        # pm can get valid response
+        client = get_client("team", api=True)
+        response = check_200(client, url)
+        key = "api-udf-tag-export-1.json"
+        data = response.json()
+        assert len(data) == 1
+        check_api_json_data(data, key, rewrite_data_files)
+
+    def test_tag_export_filter(self):
+        url = reverse("udf:api:assessment-export-tags", args=(1,))
+        client = get_client("team", api=True)
+
+        # check content_type filter
+        url += "?tag=2"
+        response = check_200(client, url)
+        assert len(response.json()) == 1
+
+        url = url.replace("?tag=2", "?tag=234")
+        response = check_200(client, url)
+        assert len(response.json()) == 0
