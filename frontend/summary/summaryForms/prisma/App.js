db765441990e7e1f6a4916cd25aa6bf8115a55cd
Matt Munns <mmunns16@gmail.com>
2024-06-13 09:57:51 -0400

    Add PRISMA visual type view and JS stub (#1020)
    * add constant and adjust feature flag
    
    * Add prisma to list of visual types and templates
    
    * add js stub
    
    * fix js errors
    
    * indent html
    
    * update migration
    
    * make it functional instead of throwing an exception
    
    * use react-based JS
    
    * restore a few things
    
    ---------
    
    Co-authored-by: Andy Shapiro <shapiromatron@gmail.com>

---
+++ frontend/summary/summaryForms/prisma/App.js
@@ -0,0 +1,47 @@
+import {inject, observer} from "mobx-react";
+import PropTypes from "prop-types";
+import React, {Component} from "react";
+import {Tab, TabList, TabPanel, Tabs} from "react-tabs";
+import FormActions from "shared/components/FormActions";
+
+import OverallPanel from "./OverallPanel";
+import PreviewPanel from "./PreviewPanel";
+import SettingsPanel from "./SettingsPanel";
+
+@inject("store")
+@observer
+class App extends Component {
+    render() {
+        const {cancel_url} = this.props.store.base.config,
+            {handleSubmit, handleTabChange} = this.props.store.base,
+            handleTabSelection = (newIndex, lastIndex) => {
+                handleTabChange(newIndex, lastIndex);
+            };
+
+        return (
+            <div>
+                <Tabs onSelect={handleTabSelection}>
+                    <TabList>
+                        <Tab>Overall</Tab>
+                        <Tab>Setting</Tab>
+                        <Tab>Preview</Tab>
+                    </TabList>
+                    <TabPanel>
+                        <OverallPanel />
+                    </TabPanel>
+                    <TabPanel>
+                        <SettingsPanel />
+                    </TabPanel>
+                    <TabPanel>
+                        <PreviewPanel />
+                    </TabPanel>
+                </Tabs>
+                <FormActions handleSubmit={handleSubmit} cancel={cancel_url} />
+            </div>
+        );
+    }
+}
+App.propTypes = {
+    store: PropTypes.object,
+};
+export default App;
---
+++ frontend/summary/summaryForms/prisma/index.js
@@ -0,0 +1,23 @@
+import {Provider} from "mobx-react";
+import React from "react";
+import ReactDOM from "react-dom";
+
+import $ from "$";
+
+import {createPrismaStore} from "../stores";
+import App from "./App";
+
+const prismaFormAppStartup = function(el, config, djangoForm) {
+    const store = createPrismaStore(config);
+    store.base.setInitialData();
+    store.base.setDjangoForm(djangoForm);
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+        el
+    );
+    $(el).fadeIn();
+};
+
+export default prismaFormAppStartup;
