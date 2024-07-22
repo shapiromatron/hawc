aacc46b642c16ba90c5d6319dd3842a5fac20c1a
Matthew Munns <mmunns16@gmail.com>
2024-07-22 15:44:37 -0400

    some default data to start with

---
+++ frontend/summary/summaryForms/prisma/PrismaDefaultSettings.js
@@ -0,0 +1,33 @@
+export default {
+
+    sections: [
+        {
+            name: "Records",
+            width: 10,
+            height: 6,
+            border_width: 2,
+            rx: 50,
+            ry: 5,
+            bg_color: "White",
+            border_color: "Black",
+            font_color: "Black",
+            text_style: "Left justified"
+        }
+    ],
+    boxes: [{
+        name: "References identified",
+        width: 6,
+        height: 4,
+        border_width: 5,
+        rx: 20,
+        ry: 20,
+        bg_color: "White",
+        border_color: "Black",
+        font_color: "Black",
+        text_style: "Left justified",
+        section: "Records"
+    }],
+    bulleted_list: [],
+    cards: [],
+    arrows: [],
+};
