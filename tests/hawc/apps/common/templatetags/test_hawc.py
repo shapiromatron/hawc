de5f8d4e6b69f2436ab453d228658a0f2cee5f97
Matthew Munns <mmunns16@gmail.com>
2024-08-26 12:44:26 -0400

    add test

--- tests/hawc/apps/common/templatetags/test_hawc.py
+++ tests/hawc/apps/common/templatetags/test_hawc.py
@@ -1,4 +1,4 @@
-from hawc.apps.common.templatetags.hawc import hastext
+from hawc.apps.common.templatetags.hawc import e_notation, hastext
 
 
 class TestHasText:
@@ -7,3 +7,11 @@ class TestHasText:
             assert hastext(text) is False
         for text in [0, False, "a", "&nbsp;", "<p>a</p>"]:
             assert hastext(text) is True
+
+
+class TestENotation:
+    def test_e_notation(self):
+        assert e_notation(1) == 1
+        assert e_notation(0.0009) == " 9.00e-04"
+        assert e_notation(0.0000001) == " 1e-07"
+        assert e_notation(100000) == 100000
