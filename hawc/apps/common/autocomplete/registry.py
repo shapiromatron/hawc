ef68a0e3e0c1f628c115e051a6b4a1c58c3f7e4e
Matthew Munns <mmunns16@gmail.com>
2024-08-27 10:32:41 -0400

    catch validation and field errors in autocomplete

--- hawc/apps/common/autocomplete/registry.py
+++ hawc/apps/common/autocomplete/registry.py
@@ -1,4 +1,4 @@
-from django.core.exceptions import BadRequest
+from django.core.exceptions import BadRequest, FieldError, ValidationError
 from django.http import Http404
 from django.utils.encoding import force_str
 from django.utils.module_loading import autodiscover_modules
@@ -50,7 +50,7 @@ def get_autocomplete(request, autocomplete_name):
         raise Http404(f"Autocomplete {autocomplete_name} not found")
     try:
         return autocomplete_cls.as_view()(request)
-    except ValueError as err:
+    except (ValueError, ValidationError, FieldError) as err:
         raise BadRequest(str(err))
 
 
