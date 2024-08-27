3eb59f322b13bd79adff0ef371306a598f1f4261
Matthew Munns <mmunns16@gmail.com>
2024-08-27 10:43:10 -0400

    add test

--- tests/hawc/apps/common/test_autocomplete.py
+++ tests/hawc/apps/common/test_autocomplete.py
@@ -9,3 +9,16 @@ def test_null_query():
     client = Client()
     resp = client.get(url + "?q=\0")
     assert resp.status_code == 200
+
+
+@pytest.mark.django_db
+def test_400_errors():
+    # check that bad requests are caught properly
+    url = "/autocomplete/study-studyautocomplete/?assessment_id=1&bioassay=bKRc26YM:%20h4vqyNto"
+    client = Client()
+    resp = client.get(url)
+    assert resp.status_code == 400
+
+    url = "/autocomplete/animal-endpointautocomplete/?animal_group__experiment__study__assessment_id=499&create=false&field=%3C!--&q=e"
+    resp = client.get(url)
+    assert resp.status_code == 400
