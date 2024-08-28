efdbd36146533d43e2d88fc334078aa93d8c0012
Casey Hansen <42587248+caseyhans@users.noreply.github.com>
2024-08-28 13:34:03 -0400

    fix create_superuser command (#1084)
    * fix create_superuser command
    
    * set superuser as staff
    
    * reduce db saves
    
    * add tests
    
    ---------
    
    Co-authored-by: Andy Shapiro <shapiromatron@gmail.com>
    Co-authored-by: Andy Shapiro <shapiro.andy@epa.gov>

--- hawc/apps/myuser/managers.py
+++ hawc/apps/myuser/managers.py
@@ -20,7 +20,7 @@ class HAWCMgr(BaseUserManager):
     # from https://docs.djangoproject.com/en/1.5/topics/auth/customizing/
     # also from UserManager(BaseUserManager)
 
-    def create_user(self, email, password=None, external_id=None, **extra_fields):
+    def _create_user(self, email, password=None, external_id=None, **extra_fields):
         if not email:
             raise ValueError("Users must have an email address")
         if settings.AUTH_PROVIDERS == {AuthProvider.django} and not password:
@@ -37,9 +37,7 @@ class HAWCMgr(BaseUserManager):
         user = self.model(
             email=self.normalize_email(email),
             external_id=external_id,
-            is_staff=False,
             is_active=True,
-            is_superuser=False,
             last_login=now,
             date_joined=now,
             **extra_fields,
@@ -48,6 +46,15 @@ class HAWCMgr(BaseUserManager):
         user.save()
 
         return user
+
+    def create_user(self, email, password=None, external_id=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, external_id, **extra_fields)
+
+    def create_superuser(self, email, password=None, external_id=None, **extra_fields):
+        extra_fields.update(is_staff=True, is_superuser=True)
+        return self._create_user(email, password, external_id, **extra_fields)
 
     def active(self):
         return self.filter(is_active=True)
