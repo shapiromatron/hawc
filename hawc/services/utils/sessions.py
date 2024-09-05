955970a9116e6ad2cd924b648a1bcdf19b8b9008
Andy Shapiro <shapiromatron@gmail.com>
2024-09-05 00:12:40 -0400

    update header defaults

--- hawc/services/utils/sessions.py
+++ hawc/services/utils/sessions.py
@@ -3,11 +3,8 @@ from django.conf import settings
 
 
 def get_session(headers: dict | None = None):
-    overrides = headers if headers is not None else {}
-    headers = {
-        "User-Agent": settings.USER_AGENT,
-        "Content-Type": "application/json",
-    } | overrides
+    _headers = {"User-Agent": settings.USER_AGENT, "Content-Type": "application/json"}
+    _headers.update(headers or {})
     session = requests.Session()
-    session.headers.update(headers)
+    session.headers.update(**_headers)
     return session
