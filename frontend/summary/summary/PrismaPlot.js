fb7a748480c86da312a1b83c639160200fba554d
Matthew Munns <mmunns16@gmail.com>
2024-09-24 17:39:53 -0400

    more formatting and add initial boxes

--- frontend/summary/summary/PrismaPlot.js
+++ frontend/summary/summary/PrismaPlot.js
@@ -14,8 +14,9 @@ class PrismaPlot {
     }
 
     build_section(obj) {
-        const g = this.vis.append("g");
-        const rect = g.append("rect")
+        // TODO: set position of g for each row
+        const section = this.vis.append("g");
+        section.append("rect")
             .attr("x", obj.rx)
             .attr("y", obj.ry)
             .attr("width", obj.width)
@@ -23,15 +24,8 @@ class PrismaPlot {
             .style("fill", obj.bg_color)
             .style("border-width", obj.border_width)
             .attr("stroke", obj.border_color);
-        // const titleBox = g.append("rect")
-        //     .attr("x", obj.rx)
-        //     .attr("y", obj.ry)
-        //     .attr("width", obj.width/2)
-        //     .attr("height", obj.height/2)
-        //     .attr("fill", obj.bg_color)
-        //     .style("border-width", obj.border_width)
-        //     .attr("stroke", obj.border_color);
-        g.append("text")
+        // TODO: apply obj.text_style
+        section.append("text")
             .attr("x", obj.rx + 5)
             .attr("y", obj.ry + 15)
             .text(obj.name)
@@ -40,13 +34,39 @@ class PrismaPlot {
             .style("border", "solid")
             .style("border-width", 2)
             .style("font-color", obj.font_color);
+        this.dataset.boxes.map(box => {
+            if (box.section == obj.key) {
+                this.build_box(box, section);
+            }
+        });
     }
 
     apply_text_style(obj, style) {
         // check style string for options like bold and left justified
     }
 
-    build_box(obj, parent) {}
+    build_box(obj, parent) {
+        const box = parent.append("g");
+        box.append("rect")
+            .attr("x", obj.rx)
+            .attr("y", obj.ry)
+            .attr("width", obj.width)
+            .attr("height", obj.height)
+            .style("fill", obj.bg_color)
+            .style("border-width", obj.border_width)
+            .attr("stroke", obj.border_color);
+        box.append("text")
+            .attr("x", obj.rx)
+            .attr("y", obj.ry)
+            .attr("width", obj.width)
+            .attr("height", obj.height)
+            .text(obj.name)
+            .attr("class", "prisma_box_text")
+            .style("background-color", "white")
+            .style("border", "solid")
+            .style("border-width", 2)
+            .style("font-color", obj.font_color);
+    }
 
     build_card(obj) {}
 
@@ -86,12 +106,27 @@ class PrismaPlot {
                     text_style: "Bold",
                 },
             ],
-            boxes: [],
+            boxes: [{
+                key: "box1key",
+                name: "Included References",
+                width: 180,
+                height: 90,
+                border_width: 0,
+                rx: 0,
+                ry: 0,
+                bg_color: "",
+                border_color: "",
+                font_color: "",
+                text_style: "",
+                section: "section1key",
+                count: 30,
+            }],
             bulleted_lists: [],
             cards: [],
             arrows: [],
         };
         // Declare the chart dimensions and margins.
+        // TODO: set row and column sizes or just depend on user input values?
         const width = 640;
         const height = 300 * this.dataset.sections.length;
         const marginTop = 20;
