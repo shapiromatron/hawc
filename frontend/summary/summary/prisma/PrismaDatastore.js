ecfc1f8a17b26ec1a8affd249ee5cf219b175ad8
Daniel Rabstejnek <rabstejnek@gmail.com>
2024-10-03 08:27:43 -0400

    Updated docs, cleanup

--- frontend/summary/summary/prisma/PrismaDatastore.js
+++ frontend/summary/summary/prisma/PrismaDatastore.js
@@ -44,92 +44,132 @@ class PrismaDatastore {
     createMaps() {
         this.maps = {};
 
+        // for each reference-tag pair we are adding the reference id
+        // to the set associated with the tag id in our tag map
         this.maps.tagMap = this.dataset.reference_tag_pairs.reduce(
-            (map, pair)=>map.set(pair.tag_id,map.has(pair.tag_id)?map.get(pair.tag_id).add(pair.reference_id):new Set([pair.reference_id])),
+            (map, pair) =>
+                map.set(
+                    pair.tag_id,
+                    map.has(pair.tag_id)
+                        ? map.get(pair.tag_id).add(pair.reference_id)
+                        : new Set([pair.reference_id])
+                ),
             new Map()
-        )
-
+        );
+        // for each reference-search pair we are adding the reference id
+        // to the set associated with the search id in our search map
         this.maps.searchMap = this.dataset.reference_search_pairs.reduce(
-            (map, pair)=>map.set(pair.search_id,map.has(pair.search_id)?map.get(pair.search_id).add(pair.reference_id):new Set([pair.reference_id])),
+            (map, pair) =>
+                map.set(
+                    pair.search_id,
+                    map.has(pair.search_id)
+                        ? map.get(pair.search_id).add(pair.reference_id)
+                        : new Set([pair.reference_id])
+                ),
             new Map()
-        )
-
+        );
     }
 
     updateTagMap() {
         let index = 0,
             recursivelyUpdate = () => {
+                // this function recursively updates the map by
+                // going to leaf nodes and working its way back.
                 let tag = this.dataset.tags[index++],
                     nextTag = this.dataset.tags[index];
-                while(nextTag && nextTag.depth == tag.depth + 1) {
-                    recursivelyUpdate()
-                    this.maps.tagMap.set(tag.id,(this.maps.tagMap.get(tag.id)||new Set()).union(this.maps.tagMap.get(nextTag.id)||new Set()))
-                    nextTag = this.dataset.tags[index]
+                // while the next tag is a child of current tag...
+                while (nextTag && nextTag.depth == tag.depth + 1) {
+                    // update the child map through recursion
+                    recursivelyUpdate();
+                    // add the updated child map to the parent map
+                    this.maps.tagMap.set(
+                        tag.id,
+                        (this.maps.tagMap.get(tag.id) || new Set()).union(
+                            this.maps.tagMap.get(nextTag.id) || new Set()
+                        )
+                    );
+                    // move on to the next child candidate
+                    nextTag = this.dataset.tags[index];
                 }
-            }
-        while(index < this.dataset.tags.length){
+            };
+        while (index < this.dataset.tags.length) {
+            // recursively update each subtree with root parent
             recursivelyUpdate();
         }
     }
 
-    tag_lookup(tag){
-        let [type,id] = tag.split("_")
-        if(type == "tag") {
-            return this.maps.tagMap.get(Number(id)) || new Set()
-        }
-        else if(type == "search") {
-            return this.maps.searchMap.get(Number(id)) || new Set()
-        }
-        else {
-            return new Set()
+    tag_lookup(tag) {
+        // the "tag" identifiers have the form "{type}_{id}",
+        // where "type" can be tag or search
+        // this function splits the identifier and uses the metadata
+        // to retrieve the set of reference ids associated with it
+        // from the relevant map
+        let [type, id] = tag.split("_");
+        if (type == "tag") {
+            // possibility of set operations so always return set
+            return this.maps.tagMap.get(Number(id)) || new Set();
+        } else if (type == "search") {
+            // possibility of set operations so always return set
+            return this.maps.searchMap.get(Number(id)) || new Set();
+        } else {
+            return new Set();
         }
     }
 
-    unique_sum(tags){
+    unique_sum(tags) {
+        // perform union operations on all the reference ids
+        // associated with each tag in tags, thus performing
+        // a "unique_sum"
         let refs = new Set();
-        for(const tag of tags){
-            refs = refs.union(this.tag_lookup(tag))
+        for (const tag of tags) {
+            refs = refs.union(this.tag_lookup(tag));
         }
-        return refs
+        return refs;
     }
 
-    section_lookup(label){
-        return this.data.find(s=>s.label == label)
+    section_lookup(label) {
+        // find section by label
+        return this.sections.find(s => s.label == label);
     }
 
-    block_sum(blocks){
+    block_sum(blocks) {
+        // compile a set of reference ids given
+        // blocks to include and blocks to exclude.
         let total = new Set();
-        for(const block of blocks){
-            if(block.type == "include") {
+        for (const block of blocks) {
+            if (block.type == "include") {
                 total = total.union(block.refs);
-            }
-            else if(block.type == "exclude") {
+            } else if (block.type == "exclude") {
                 total = total.difference(block.refs);
             }
         }
-        return total
+        return total;
     }
 
     setRefs() {
-        for(const section of this.sections) {
-            for(const block of section.blocks) {
+        for (const section of this.sections) {
+            for (const block of section.blocks) {
+                // set the reference ids and reference id count
+                // on each block in each section
                 let block_tags = [];
-                if(block.sub_blocks) {
-                    for(const sub_block of block.sub_blocks ) {
+                if (block.sub_blocks) {
+                    // if there are sub blocks include them in
+                    // "unique_sum" calculation
+                    for (const sub_block of block.sub_blocks) {
                         sub_block.refs = this.unique_sum(sub_block.tags);
                         sub_block.value = sub_block.refs.size;
-                        block_tags.push(...sub_block.tags||[]);
+                        block_tags.push(...(sub_block.tags || []));
                     }
                 }
-                if(block.count == null) {
+                if (block.count == null) {
                     continue;
-                }
-                else if(block.count == "unique_sum") {
-                    block_tags.push(...block.tags||[]);
+                } else if (block.count == "unique_sum") {
+                    // perform "unique_sum" count
+                    block_tags.push(...(block.tags || []));
                     block.refs = this.unique_sum(block_tags);
                     block.value = block.refs.size;
-                }
-                else {
+                } else {
+                    // perform count based on blocks in other section
                     let prev_section = this.section_lookup(block.count);
                     block.refs = this.block_sum(prev_section.blocks);
                     block.value = block.refs.size;
@@ -137,18 +177,6 @@ class PrismaDatastore {
             }
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
 export default PrismaDatastore;
