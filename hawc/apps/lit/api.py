1b81b2a164c2fc129ae066ecb2927605dd8864b1
Matthew Munns <mmunns16@gmail.com>
2024-11-07 17:31:13 -0500

    Remove unnecessary error statement

--- hawc/apps/lit/api.py
+++ hawc/apps/lit/api.py
@@ -62,8 +62,6 @@ class LiteratureAssessmentViewSet(BaseAssessmentViewSet):
             create_object_log(
                 "Updated (tagtree replace)", assessment, assessment.id, self.request.user.id
             )
-        else:
-            raise ValueError()
         return Response(serializer.data)
 
     @action(
