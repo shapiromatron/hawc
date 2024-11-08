9545000d8979d184cfd0202bd27422be6740d4e5
Matthew Munns <mmunns16@gmail.com>
2024-11-08 10:06:23 -0500

    change to drf validation error

--- hawc/apps/lit/api.py
+++ hawc/apps/lit/api.py
@@ -62,6 +62,8 @@ class LiteratureAssessmentViewSet(BaseAssessmentViewSet):
             create_object_log(
                 "Updated (tagtree replace)", assessment, assessment.id, self.request.user.id
             )
+        else:
+            raise ValidationError(f"Request method {self.request.method} is not allowed.")
         return Response(serializer.data)
 
     @action(
