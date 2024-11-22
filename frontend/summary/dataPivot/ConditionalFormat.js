1db1b7413598385636be24747ff33c89a8c4053a
Andy Shapiro <shapiromatron@gmail.com>
2024-11-22 10:21:18 -0500

    change label for null conditional

--- frontend/summary/dataPivot/ConditionalFormat.js
+++ frontend/summary/dataPivot/ConditionalFormat.js
@@ -292,7 +292,9 @@ class _DataPivot_settings_conditional {
                 let mapping = buildStyleMap(values, true);
                 vals.unique_tokens.forEach(v => {
                     var select = dp.style_manager
-                        .add_select(parent.settings.type, mapping.get(v) || NULL_CASE, true)
+                        .add_select(parent.settings.type, mapping.get(v) || NULL_CASE, true, {
+                            null_label: " --- no change ---",
+                        })
                         .data("key", v);
                     self.discrete_styles.push(select);
                     add_input_row(discrete, `Style for <kbd>${v}</kbd>:`, select);
