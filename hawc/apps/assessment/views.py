shapiromatron/hawc

commit 59b3662d349ae36fa827d0adf646474cb2251305

one fewer db query?

hawc/apps/assessment/views.py
@@ -978,7 +978,7 @@ class LabelViewSet(HtmxViewSet):
     list_fragment = "assessment/fragments/label_list.html"
 
     def get_queryset(self, request):
-        return models.Label.get_assessment_qs(request.item.assessment.pk)
+        return models.Label.get_assessment_qs(request.item.assessment_id)
 
     @action(permission=can_view, htmx_only=False)
     def read(self, request: HttpRequest, *args, **kwargs):

commit 72e433c14356ac567344ed3d5f8e5415874d4223

fix error I introduced in 59b3662d349ae36fa827d0adf646474cb2251305 (womp womp)

hawc/apps/assessment/views.py
@@ -978,7 +978,7 @@ class LabelViewSet(HtmxViewSet):
     list_fragment = "assessment/fragments/label_list.html"
 
     def get_queryset(self, request):
-        return models.Label.get_assessment_qs(request.item.assessment.id)
+        return models.Label.get_assessment_qs(request.item.assessment.pk)
 
     @action(permission=can_view, htmx_only=False)
     def read(self, request: HttpRequest, *args, **kwargs):