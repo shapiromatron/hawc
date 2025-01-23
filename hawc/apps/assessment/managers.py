238294588da79807f8a199d18b4c14863f074093
Andy Shapiro <shapiromatron@gmail.com>
2025-01-22 22:54:19 -0500

    use correct label related queries

--- hawc/apps/assessment/managers.py
+++ hawc/apps/assessment/managers.py
@@ -337,7 +337,7 @@ class LabeledItemQuerySet(QuerySet):
         )
 
     def filter_title(self, query: str):
-        filter = Q(summary_table__title__icontains=query) | Q(visual__title__icontains=query)
+        filter = Q(summary_tables__title__icontains=query) | Q(visuals__title__icontains=query)
         return self.filter(filter)
 
     def matching_all_labels(self, labels: list):
