1d77fabcc06c31f965cc7707dff47cdf1eebb47a
63080 <63080_icfcorp>
2025-02-10 14:59:41 -0500

    Add experiment observations tab to sidebar

--- hawc/apps/animalv2/autocomplete.py
+++ hawc/apps/animalv2/autocomplete.py
@@ -6,3 +6,10 @@ from . import models
 class ChemicalAutocomplete(BaseAutocomplete):
     model = models.Chemical
     search_fields = ["name"]
+
+
+@register
+class ExperimentAutocomplete(BaseAutocomplete):
+    model = models.Experiment
+    search_fields = ["name"]
+    filter_fields = ["study_id", "study__assessment_id"]
