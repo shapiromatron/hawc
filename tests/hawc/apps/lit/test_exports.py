85c59c9d5d761229083ae65bbd88f69af9441d4f
Andy Shapiro <shapiro.andy@epa.gov>
2025-05-06 08:53:40 -0400

    check for if an export is empty (#1173)

--- tests/hawc/apps/lit/test_exports.py
+++ tests/hawc/apps/lit/test_exports.py
@@ -12,3 +12,12 @@ class TestReferenceTagLongExport:
             assessment=Assessment.objects.get(id=2),
         )
         assert exporter.build_df().shape == (5, 10)
+
+    def test_no_data(self):
+        exporter = exports.ReferenceTagLongExport(
+            queryset=models.Reference.objects.filter(assessment_id=5),
+            assessment=Assessment.objects.get(id=5),
+        )
+        df = exporter.build_df()
+        assert df.shape == (1, 1)
+        assert df.Result[0] == "No data available."
