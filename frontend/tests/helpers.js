e99d89a9a913874d9d6538c1284698ed1c888f5d
Andy Shapiro <shapiromatron@gmail.com>
2025-06-05 19:27:30 -0400

    wrap startup commands in $(document).ready()

--- frontend/tests/helpers.js
+++ frontend/tests/helpers.js
@@ -13,7 +13,4 @@ const isClose = function(actual, expected, atol) {
 assert.isClose = isClose;
 assert.allClose = allClose;
 
-// This file only provides helpers for other test files and does not need to be run as a test suite itself.
-// Remove this file from test matching or rename to helpers-util.js if you want to avoid Vitest trying to run it as a test.
-
 export default assert;
