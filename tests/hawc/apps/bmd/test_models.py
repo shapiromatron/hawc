72a155371e3261ff420bc43d04a38409508f634f
Andy Shapiro <shapiromatron@gmail.com>
2025-08-31 09:28:22 -0400

    update to most recent pymbds version

--- tests/hawc/apps/bmd/test_models.py
+++ tests/hawc/apps/bmd/test_models.py
@@ -1,4 +1,5 @@
 import pytest
+from pybmds import __version__ as pybmds_version
 
 from hawc.apps.animal.models import Endpoint
 from hawc.apps.bmd.constants import SelectedModel
@@ -10,7 +11,7 @@ class TestBmdSession:
     def test_dichotomous(self):
         # create new session w/ inputs
         session = Session.create_new(Endpoint.objects.get(id=8))
-        assert session.version == "24.1"
+        assert session.version == pybmds_version
         settings = session.get_settings()
         assert settings.version == 2
         assert settings.dtype == "D"
@@ -32,7 +33,7 @@ class TestBmdSession:
     def test_continuous(self):
         # create new session w/ inputs
         session = Session.create_new(Endpoint.objects.get(id=3))
-        assert session.version == "24.1"
+        assert session.version == pybmds_version
         settings = session.get_settings()
         assert settings.version == 2
         assert settings.dtype == "C"
