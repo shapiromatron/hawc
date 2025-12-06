c05365a637174c1216ee28d4b8891b66e2184134
Andy Shapiro <shapiromatron@gmail.com>
2025-12-06 11:22:50 -0500

    fix

--- tests/hawc/apps/eco/test_forms.py
+++ tests/hawc/apps/eco/test_forms.py
@@ -33,16 +33,16 @@ class TestResultForm:
     def test_clean(self, db_keys):
         design = models.Design.objects.get(id=db_keys.eco_design)
         result = models.Result.objects.filter(design=design).first()
+        assert result is not None
 
-        if result:
-            form = forms.ResultForm(instance=result)
-            initial_data = {f"result-{result.pk}-{k}": v for k, v in form.initial.items()}
+        form = forms.ResultForm(instance=result)
+        initial_data = {f"result-{result.pk}-{k}": v for k, v in form.initial.items()}
 
-            data = initial_data.copy()
-            data[f"result-{result.pk}-low_variability"] = 1.0
-            data[f"result-{result.pk}-variability"] = None
+        data = initial_data.copy()
+        data[f"result-{result.pk}-low_variability"] = 1.0
+        data[f"result-{result.pk}-variability"] = None
 
-            form = forms.ResultForm(data, instance=result)
-            assert form.is_valid() is False
-            assert "variability" in form.errors
-            assert forms.ResultForm.RESPONSE_VARIABILITY_REQ in form.errors["variability"]
+        form = forms.ResultForm(data, instance=result)
+        assert form.is_valid() is False
+        assert "variability" in form.errors
+        assert forms.ResultForm.RESPONSE_VARIABILITY_REQ in form.errors["variability"]
