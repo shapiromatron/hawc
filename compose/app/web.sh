95c963ecc2c2e897801169610b730e0709cb44c8
Andy Shapiro <shapiromatron@gmail.com>
2026-01-11 16:15:55 -0500

    updates from testing

--- compose/app/web.sh
+++ compose/app/web.sh
@@ -7,7 +7,8 @@ set -xe
     --host 0.0.0.0 \
     --port 5000 \
     --workers 3 \
-    --log-level info \
+    --workers-lifetime 3600 \
+    --respawn-interval 60 \
     --access-log \
     --respawn-failed-workers \
     hawc.main.wsgi:application
