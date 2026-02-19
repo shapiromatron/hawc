909be636c89c2f643eb2fd4711730d4f0e4a26b4
Andy Shapiro <shapiromatron@gmail.com>
2026-02-19 09:23:20 -0500

    add a sync_web command

--- compose/app/startup.py
+++ compose/app/startup.py
@@ -6,14 +6,52 @@ from enum import StrEnum, auto
 
 
 class Cmd(StrEnum):
-    cron = auto()
     sync = auto()
     web = auto()
+    sync_web = auto()
+    cron = auto()
     workers = auto()
 
 
 def run_manage(*args):
     subprocess.run(["manage", *args], check=True)  # noqa: S607
+
+
+def run_web():
+    os.execv(  # noqa: S606
+        "/usr/local/bin/granian",
+        [
+            "granian",
+            "--interface",
+            "wsgi",
+            "--host",
+            "0.0.0.0",  # noqa: S104
+            "--port",
+            "5000",
+            "--workers",
+            "3",
+            "--workers-lifetime",
+            "3600",
+            "--respawn-interval",
+            "60",
+            "--access-log",
+            "--respawn-failed-workers",
+            "hawc.main.wsgi:application",
+        ],
+    )
+
+
+def run_sync():
+    if os.environ.get("HAWC_LOAD_TEST_DB") == "1":
+        run_manage("load_test_db")
+    for cmd in [
+        ("clear_cache",),
+        ("clearsessions",),
+        ("collectstatic", "--noinput"),
+        ("migrate", "--noinput"),
+        ("recreate_views",),
+    ]:
+        run_manage(*cmd)
 
 
 def main():
@@ -33,38 +71,12 @@ def main():
                 ["celery", "--app=hawc.main.celery", "worker", "--loglevel=INFO"],
             )
         case Cmd.web:
-            os.execv(  # noqa: S606
-                "/usr/local/bin/granian",
-                [
-                    "granian",
-                    "--interface",
-                    "wsgi",
-                    "--host",
-                    "0.0.0.0",  # noqa: S104
-                    "--port",
-                    "5000",
-                    "--workers",
-                    "3",
-                    "--workers-lifetime",
-                    "3600",
-                    "--respawn-interval",
-                    "60",
-                    "--access-log",
-                    "--respawn-failed-workers",
-                    "hawc.main.wsgi:application",
-                ],
-            )
+            run_web()
         case Cmd.sync:
-            if os.environ.get("HAWC_LOAD_TEST_DB") == "1":
-                run_manage("load_test_db")
-            for cmd in [
-                ("clear_cache",),
-                ("clearsessions",),
-                ("collectstatic", "--noinput"),
-                ("migrate", "--noinput"),
-                ("recreate_views",),
-            ]:
-                run_manage(*cmd)
+            run_sync()
+        case Cmd.sync_web:
+            run_sync()
+            run_web()
 
 
 if __name__ == "__main__":
