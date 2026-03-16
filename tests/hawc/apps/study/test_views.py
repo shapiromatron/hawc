468d7fa549666fbee2cddab55739f000d1db0b8c
Andy Shapiro <shapiromatron@gmail.com>
2026-03-16 08:54:42 -0400

    reapply 1117

--- tests/hawc/apps/study/test_views.py
+++ tests/hawc/apps/study/test_views.py
@@ -1,9 +1,104 @@
 import pytest
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test.client import Client
 from django.urls import reverse
 from pytest_django.asserts import assertTemplateUsed
 
-from ..test_utils import check_200, get_client
+from hawc.apps.lit.models import Reference
+from hawc.apps.study.models import Attachment
+
+from ..test_utils import check_200, check_302, check_403, get_client
+
+
+@pytest.mark.django_db
+class TestStudyCreateFromReference:
+    def test_redirect(self):
+        client = get_client("team")
+        url = reverse("study:new_study", args=(1,))
+        check_302(client, url, reverse("study:detail", args=(1,)))
+
+    def test_success(self):
+        ref = Reference.objects.create(assessment_id=1)
+        client = get_client("team")
+
+        url = reverse("study:new_study", args=(ref.id,))
+        resp = client.post(url, {"short_citation": "test", "full_citation": "a", "coi_reported": 0})
+        assert resp.status_code == 302
+        assert resp.url == reverse("study:detail", args=(ref.id,))
+
+
+@pytest.mark.django_db
+class TestCloneViewSet:
+    def test_get(self):
+        team = get_client("team")
+        dst_assesment = 1
+        url = reverse("study:clone_from_assessment", args=(dst_assesment,))
+        resp = check_200(team, url)
+        assertTemplateUsed(resp, "study/study_clone.html")
+
+    def test_post_fetch(self):
+        team = get_client("team", htmx=True)
+        dst_assesment = 1
+        url = reverse("study:clone_from_assessment", args=(dst_assesment,))
+        resp = team.post(url, {"action": "fetch", "src_assessment": 2})
+        assert resp.status_code == 200
+        assertTemplateUsed(resp, "study/fragments/clone_fetch.html")
+
+    def test_bad_post_actions(self):
+        team = get_client("team", htmx=True)
+        dst_assesment = 1
+        url = reverse("study:clone_from_assessment", args=(dst_assesment,))
+
+        # bad src assessment
+        resp = team.post(url, {"action": "fetch", "src_assessment": 9999})
+        assert resp.status_code == 400
+
+        # bad actions
+        resp = team.post(url, {"src_assessment": 2})
+        assert resp.status_code == 400
+        resp = team.post(url, {"action": "zzz", "src_assessment": 2})
+        assert resp.status_code == 400
+
+    def _valid_clone_payload(self):
+        return {
+            "action": "clone",
+            "src_assessment": 2,
+            "study": [7],
+            "study_bioassay": [7],
+            "study_epi": [],
+            "study_rob": [7],
+            "include_rob": True,
+            "copy_mode": "final-to-final",
+            "metric-1": 14,
+            "metric-2": 15,
+        }
+
+    def test_post_clone(self):
+        team = get_client("team", htmx=True)
+        dst_assesment = 1
+        url = reverse("study:clone_from_assessment", args=(dst_assesment,))
+        resp = team.post(url, self._valid_clone_payload())
+        assert resp.status_code == 200
+        assertTemplateUsed(resp, "study/fragments/clone_success.html")
+
+    def test_post_clone_errors(self):
+        team = get_client("team", htmx=True)
+        dst_assesment = 1
+        url = reverse("study:clone_from_assessment", args=(dst_assesment,))
+
+        # bad src ID
+        data = self._valid_clone_payload()
+        data["metric-1"] = 9999
+        resp = team.post(url, data)
+        assert resp.status_code == 200
+        assertTemplateUsed(resp, "study/fragments/clone_failure.html")
+
+        # bad destination ID
+        data = self._valid_clone_payload()
+        data["metric-9999"] = data["metric-1"]
+        resp = team.post(url, data)
+        assert resp.status_code == 200
+        assertTemplateUsed(resp, "study/fragments/clone_failure.html")
 
 
 @pytest.mark.django_db
@@ -170,3 +265,38 @@ def test_get_200():
     ]
     for url in urls:
         check_200(client, url)
+
+
+@pytest.mark.django_db
+class TestAttachmentCrud:
+    def test_create_read_delete(self):
+        study_id = 1
+        anon = get_client()
+        client = get_client("team")
+        study_url = reverse("study:detail", args=(study_id,))
+
+        # create attachment
+        assert Attachment.objects.filter(study_id=study_id).exists() is False
+        url = reverse("study:attachment_create", args=(study_id,))
+        resp = client.post(url, {"attachment": SimpleUploadedFile("z.txt", b"test")})
+        assert resp.url == study_url
+        attachment = Attachment.objects.get(study_id=study_id)
+
+        # detail - check team member can view
+        url = attachment.get_absolute_url()
+        resp = client.get(url)
+        check_302(client, url, attachment.attachment.url)
+
+        # detail - check anonymous cannot view
+        check_403(anon, url)
+
+        # delete attachment
+        url = attachment.get_delete_url()
+        check_200(client, url)
+        resp = client.post(url)
+        assert resp.status_code == 302
+        assert resp.url == study_url
+        assert Attachment.objects.filter(study_id=study_id).exists() is False
+
+        # cleanup (delete attachment)
+        attachment.attachment.delete(save=False)
